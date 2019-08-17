edb836ef08b8308cd22a4f29f522f9da9ef49c22
brendanjmeade <brendanjmeade@gmail.com>
2019-08-16 20:36:27 -0400

    Finished pretty plotting for topo example.

--- src/ex_thrusttopo.jl
+++ src/ex_thrusttopo.jl
@@ -9,7 +9,7 @@ function ex_thrusttopo()
     elements = Elements()
 
     # Observation points for internal evaluation and visualization
-    npts= 100
+    npts= 200
     xobs = LinRange(-10e3, 10e3, npts)
     yobs = LinRange(-5e3, 5e3, npts)
     xobs, yobs = meshgrid(xobs, yobs)
@@ -17,7 +17,7 @@ function ex_thrusttopo()
     yobs = yobs[:]
 
     # Topographic free surface
-    x1, y1, x2, y2 = discretizedline(-10e3, 0, 10e3, 0, 20)
+    x1, y1, x2, y2 = discretizedline(-10e3, 0, 10e3, 0, 200)
     y1 = -1e3 * @.atan(x1 / 1e3)
     y2 = -1e3 * @.atan(x2 / 1e3)
     for i in 1:length(x1)
@@ -30,7 +30,7 @@ function ex_thrusttopo()
     standardize_elements!(elements)
 
     # Curved fault
-    x1, y1, x2, y2 = discretizedline(-7e3, 0e3, 0, 0, 10)
+    x1, y1, x2, y2 = discretizedline(-7e3, 0e3, 0, 0, 100)
     y1 = 3e3 * @.atan(x1 / 1e3)
     y2 = 3e3 * @.atan(x2 / 1e3)
     for i in 1:length(x1)
@@ -69,8 +69,6 @@ function ex_thrusttopo()
         dispfault += disp
         stressfault += stress
     end
-    # plotfields(elements, reshape(xobs, npts, npts), reshape(yobs, npts, npts),
-    #     dispfault, stressfault, "fault")
 
     # Free surface in full space
     dispfreesurface::Array{Float64} = zeros(length(xobs), 2)
@@ -84,49 +82,14 @@ function ex_thrusttopo()
         dispfreesurface += disp
         stressfreesurface += stress
     end
-    # plotfields(elements, reshape(xobs, npts, npts), reshape(yobs, npts, npts),
-    #     dispfreesurface, stressfreesurface, "free surface")
 
-    # Plot fault + free surface
-    # plotfields(elements, reshape(xobs, npts, npts), reshape(yobs, npts, npts),
-    #     dispfault + dispfreesurface, stressfault + stressfreesurface, "total")
-
-
-
-        # Pretty of displacements and stresses
-    # def common_plot_elements():
-    #     # Create a white fill over portion of the figure above the free surface
-    #     x_surface = np.unique(
-    #         [[_["x1"] for _ in elements_surface], [_["x2"] for _ in elements_surface]]
-    #     )
-    #     x_fill = np.append(x_surface, [10e3, -10e3, -10e3])
-    #     y_surface = np.unique(
-    #         [[_["y1"] for _ in elements_surface], [_["y2"] for _ in elements_surface]]
-    #     )
-    #     y_surface = np.flip(y_surface, 0)
-    #     y_fill = np.append(y_surface, [5e3, 5e3, np.min(y_surface)])
-    #     plt.fill(x_fill, y_fill, "w", zorder=30)
-    #
-    #     for element in elements_fault + elements_surface:
-    #         plt.plot(
-    #             [element["x1"], element["x2"]],
-    #             [element["y1"], element["y2"]],
-    #             "-k",
-    #             linewidth=1.0,
-    #             zorder=50,
-    #         )
-    #
-    #     x_lim = np.array([x_plot.min(), x_plot.max()])
-    #     y_lim = np.array([y_plot.min(), y_plot.max()])
-    #     plt.xticks([x_lim[0], 0, x_lim[1]])
-    #     plt.yticks([y_lim[0], 0, y_lim[1]])
-    #     plt.gca().set_aspect("equal")
-    #     plt.xlabel("$x$ (m)")
-    #     plt.ylabel("$y$ (m)")
-    #
-    #
+    # Pretty of displacements and stresses
+    freesurfaceidx = findall(x -> x == "freesurface", elements.name)
+    xfreesurface = unique([elements.x1[freesurfaceidx] ; elements.x2[freesurfaceidx]])
+    xfill = [xfreesurface ; [10e3 ; -10e3 ; -10e3]]
+    yfreesurface = unique([elements.y1[freesurfaceidx] ; elements.y2[freesurfaceidx]])
+    yfill = [yfreesurface ; [5e3 ; 5e3 ; minimum(yfreesurface)]]
     ufield = @.sqrt((dispfault + dispfreesurface)[:, 1].^2 + (dispfault + dispfreesurface)[:, 2].^2)
-
     σxx = (stressfreesurface + stressfault)[:, 1]
     σyy = (stressfreesurface + stressfault)[:, 2]
     σxy = (stressfreesurface + stressfault)[:, 3]
@@ -135,7 +98,7 @@ function ex_thrusttopo()
     J2 = (I1.^2) ./ 3.0 - I2  # 2nd invariant (deviatoric)
     σfield = @.log10(abs(J2))
 
-    ncontours = 5
+    ncontours = 10
     figure(figsize=(6, 8))
     subplot(2, 1, 1)
     contourf(reshape(xobs, npts, npts), reshape(yobs, npts, npts),
@@ -143,6 +106,7 @@ function ex_thrusttopo()
     colorbar(fraction=0.020, pad=0.05, extend="both", label=L"$||u||$ (m)")
     contour(reshape(xobs, npts, npts), reshape(yobs, npts, npts),
         reshape(ufield, npts, npts), ncontours, linewidths=0.5, colors="gray")
+    fill(xfill, yfill, "w", zorder=30)
     plotelements(elements)
     xticks([minimum(xobs), 0, maximum(xobs)])
     yticks([minimum(yobs), 0, maximum(yobs)])
@@ -154,9 +118,10 @@ function ex_thrusttopo()
     subplot(2, 1, 2)
     contourf(reshape(xobs, npts, npts), reshape(yobs, npts, npts),
         reshape(σfield, npts, npts), ncontours, cmap=get_cmap("hot_r"))
-    colorbar(fraction=0.020, pad=0.05, extend="both", label=L"$\log_{10}|\mathrm{J}_2|$ (Pa^2)")
+    colorbar(fraction=0.020, pad=0.05, extend="both", label=L"$\log_{10}|\mathrm{J}_2|$ (Pa$^2$)")
     contour(reshape(xobs, npts, npts), reshape(yobs, npts, npts),
         reshape(σfield, npts, npts), ncontours, linewidths=0.5, colors="gray")
+    fill(xfill, yfill, "w", zorder=30)
     plotelements(elements)
     xticks([minimum(xobs), 0, maximum(xobs)])
     yticks([minimum(yobs), 0, maximum(yobs)])
