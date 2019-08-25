1a3c49dc76108c4108c13b985b1cf5b253a3c11d
brendanjmeade <brendanjmeade@gmail.com>
2019-08-24 23:46:15 -0400

    ODE solve is called now but there is no evolution.  Something is very wrong.

--- src/ex_planarqdconst.jl
+++ src/ex_planarqdconst.jl
@@ -1,28 +1,41 @@
 using Revise
-using PyCall
-using PyPlot
+using DifferentialEquations
+# using PyCall
+# using PyPlot
+using Plots
 using Bem2d
 
 # Derivatives to feed to ODE integrator
-function derivs(t, vθ, ∂t, els, blockvelx, blockvely)
-    # Current shear stress rate on fault (slip -> traction)
-    dτ = ∂t * [vθ[1:3:end] ; vθ[2:3:end]]
-    dθ = 1 - θ * v / Dc # state evolution: aging law
-    dθ = 1 - θ * v / Dc # state evolution: slip law
-    dv = 1 / (η / els.σn[1:els.endidx] + els.a[1:els.endidx] / v) *
-        (dτ / els.σn[1:els.endidx] - els.b[1:els.endidx] * dθ / θ) # velocity evolution
+function calc_dvθ(dvθ, vθ, params, t)
+# function calc_dvθ(t, vθ, ∂t, els, blockvelx, blockvely, η, Dc)
+    ∂t, els, blockvelx, blockvely, η, Dc = params # Unpac parameters
+    θ = vθ[3:3:end]
+    v = @.sqrt(vθ[1:3:end].^2 + vθ[2:3:end].^2) # TODO: Flat fault only
+    dt = ∂t * [vθ[1:3:end] ; vθ[2:3:end]]
+    dτ = dt[1:2:end]
+
+    # State evolution: aging law
+    dθ = 1 .- θ .* v ./ Dc
+    # display(dθ)
+
+    # Acceleration evolution
+    dv = 1 ./ (η ./ els.σn[1:els.endidx] .+ els.a[1:els.endidx] ./ v) .*
+        (dτ ./ els.σn[1:els.endidx] .- els.b[1:els.endidx] .* dθ ./ θ)
+    display(dv)
     
-    derivs = zeros(3 * els.endidx)
-    derivs[1:3:end] .= -currentvels[1:2:end] + blockvelx
-    derivs[2:3:end] .= -currentvels[2:2:end] + blockvely
-    derivs[3:3:end] .= dθ
-    return derivs
+    dvθ = zeros(3 * els.endidx)
+    # dvθ[1:3:end] .= -dv .+ blockvelx # TODO: Flat fault only
+    # dvθ[2:3:end] .= blockvely # TODO: Flat fault only
+    dvθ[1:3:end] .= -dv # TODO: Flat fault only
+    dvθ[2:3:end] .= 0 # TODO: Flat fault only
+    dvθ[3:3:end] .= dθ
+    return dvθ
 end
 
 function ex_planarqdconst()
     # Constants and model parameters
-    spy = 365.25 * 24 * 60 * 60  # Seconds per year
-    timeinterval = spy * [0.0 1000.0]
+    siay = 365.25 * 24 * 60 * 60
+    tspan = (0.0, siay * 1000.0)
     μ = 3e10
     ν = 0.25
     ρ = 2700.0
@@ -33,7 +46,7 @@ function ex_planarqdconst()
 
     # Create fault elements
     els = Elements()
-    nfault = 50
+    nfault = 10
     nnodes = 1 * nfault
     L = 10000
     x1, y1, x2, y2 = discretizedline(-L, 0, L, 0, nfault)
@@ -48,49 +61,56 @@ function ex_planarqdconst()
     standardize_elements!(els)
 
     # Calculate slip to traction partials on the fault
-    println("Calculating ∂s")
+    println("Calculating velocity to traction matrix")
     srcidx = findall(x->x == "fault", els.name)
     obsidx = srcidx
     _, _, ∂t = ∂constslip(els, srcidx, obsidx, μ, ν)
 
-    # Try calculating derivatives that we want to integrate
+    # Set initial conditions
+    # Vp = 1e-9 
+    # [1e8, Vp/1000]
+    ics = zeros(3 * nnodes)
+    ics[1:3:end] = 1e-3 * blockvelx * ones(nnodes)
+    ics[2:3:end] = 1e-3 * blockvely * ones(nnodes)
+    ics[3:3:end] = 0.5 * ones(nnodes)
 
-    # Set initial conditions and time integrate
-    initconds = zeros(3 * nnodes)
-    initconds[1:3:end] = 1e-3 * blockvelx * ones(nnodes)
-    initconds[2:3:end] = 1e-3 * blockvely * ones(nnodes)
-    initconds[3:3:end] = 0.5 * ones(nnodes)
+    # Try calculating derivatives
+    # dvθ = calc_dvθ(1, ics, ∂t, els, blockvelx, blockvely, η, Dc)
+
+    # Time integrate
+    p = (∂t, els, blockvelx, blockvely, η, Dc)
+    prob = ODEProblem(calc_dvθ, ics, tspan, p)
+    sol = solve(prob)
+    plot(sol, vars = (1))
+    display(sol)
+    gui()
+
     return nothing
 end
 ex_planarqdconst()
 
-# function plottimeseries(solution, spy)
+# function plottimeseries(solution, siay)
 #     figure(figsize=(12, 9))
 #     subplot(3, 2, 1)
-#     plot(SOLUTION["t"] / SPY, SOLUTION["y"][:, 0::3], linewidth=0.5)
-#     ylabel(L"$u_x$ (m)")
-
+#         plot(SOLUTION["t"] / SPY, SOLUTION["y"][:, 0::3], linewidth=0.5)
+#         ylabel(L"$u_x$ (m)")
 #     subplot(3, 2, 2)
-#     plot(SOLUTION["y"][:, 0::3], linewidth=0.5)
-#     ylabel(L"$u_x$ (m)")
-
+#         plot(SOLUTION["y"][:, 0::3], linewidth=0.5)
+#         ylabel(L"$u_x$ (m)")
 #     subplot(3, 2, 3)
-#     plot(SOLUTION["t"] / SPY, SOLUTION["y"][:, 1::3], linewidth=0.5)
-#     ylabel(L"$u_y$ (m)")
-
+#         plot(SOLUTION["t"] / SPY, SOLUTION["y"][:, 1::3], linewidth=0.5)
+#         ylabel(L"$u_y$ (m)")
 #     subplot(3, 2, 4)
-#     plot(SOLUTION["y"][:, 1::3], linewidth=0.5)
-#     ylabel(L"$u_y$ (m)")
-
+#         plot(SOLUTION["y"][:, 1::3], linewidth=0.5)
+#         ylabel(L"$u_y$ (m)")
 #     subplot(3, 2, 5)
-#     plot(SOLUTION["t"] / SPY, SOLUTION["y"][:, 2::3], linewidth=0.5)
-#     xlabel("time (years)")
-#     ylabel(L"$\theta$")
-
+#         plot(SOLUTION["t"] / SPY, SOLUTION["y"][:, 2::3], linewidth=0.5)
+#         xlabel("time (years)")
+#         ylabel(L"$\theta$")
 #     subplot(3, 2, 6)
-#     plot(SOLUTION["y"][:, 2::3], linewidth=0.5)
-#     xlabel("steps")
-#     ylabel(L"$\theta$")
+#         plot(SOLUTION["y"][:, 2::3], linewidth=0.5)
+#         xlabel("steps")
+#         ylabel(L"$\theta$")
 #     show()
 # end
 # plottimeseries()
