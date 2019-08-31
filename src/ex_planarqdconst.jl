6837544b024642c863a15880a184b76dabbdc299
brendanjmeade <brendanjmeade@gmail.com>
2019-08-30 22:20:17 -0400

    Contourf visualization.  Added aging law to slip law.  Slip law produces really interesting behavior fast.

--- src/ex_planarqdconst.jl
+++ src/ex_planarqdconst.jl
@@ -4,7 +4,9 @@ using PyCall
 using PyPlot
 using Bem2d
 
-function plottimeseries(sol, nfault, siay)
+function plottimeseries(sol)
+    siay = 365.25 * 24 * 60 * 60
+    nfault = Int(size(sol)[1] / 3)
     t = [x / siay for x in sol.t]
     θ = zeros(length(t), nfault)
     vx = zeros(length(t), nfault)
@@ -19,49 +21,56 @@ function plottimeseries(sol, nfault, siay)
     figure(figsize = (15, 8))
 
     subplot(3, 2, 1)
-    plot(t, vx, "-b", linewidth = 0.5)
+    plot(t, vx, "-", linewidth = 0.5)
     yscale("log")
     ylabel(L"v_x")
     subplot(3, 2, 3)
-    plot(t, vy, "-b", linewidth = 0.5)
+    plot(t, vy, "-", linewidth = 0.5)
     yscale("log")
     ylabel(L"v_y")
     subplot(3, 2, 5)
-    plot(t, θ, "-b", linewidth = 0.5)
+    plot(t, θ, "-", linewidth = 0.5)
     yscale("log")
     xlabel("t (years)")
     ylabel(L"\theta")
 
     subplot(3, 2, 2)
-    plot(1:1:length(t), vx, "-b", linewidth = 0.5)
+    plot(1:1:length(t), vx, "-", linewidth = 0.5)
     yscale("log")
     ylabel(L"v_x")
     subplot(3, 2, 4)
-    plot(1:1:length(t), vy, "-b", linewidth = 0.5)
+    plot(1:1:length(t), vy, "-", linewidth = 0.5)
     yscale("log")
     ylabel(L"v_y")
     subplot(3, 2, 6)
-    plot(1:1:length(t), θ, "-b", linewidth = 0.5)
+    plot(1:1:length(t), θ, "-", linewidth = 0.5)
     yscale("log")
     xlabel("time step #")
     ylabel(L"\theta")
 
-
+    figure(figsize = (15, 5))
+    plotme = @.log10(vx')
+    contourf(plotme, 20)
+    colorbar()
+    contour(plotme, 20, linewidths = 0.5, linestyles = "solid", colors = "k")
+    xlabel("time step")
+    ylabel("element index")
     show()
 end
 
 # Derivatives to feed to ODE integrator
 function calc_dvθ(vθ, p, t)
-    # println(t / (365.25 * 24 * 60 * 60))
     ∂t, els, η, dc, blockvelx, blockvely = p
     θ = vθ[3:3:end]
-    vmag = @.sqrt(vθ[1:3:end].^2 + vθ[2:3:end].^2) # TODO: Flat fault only
+    # vmag = @.sqrt(vθ[1:3:end].^2 + vθ[2:3:end].^2) # TODO: Flat fault only
+    vmag = vθ[1:3:end] # TODO: Flat fault only
     dt = ∂t * [blockvelx .- vθ[1:3:end] blockvely .- vθ[2:3:end]]'[:] # interleaving!
     dτ = dt[1:2:end]
     dθ = zeros(els.endidx)
     dv = zeros(els.endidx)
     for i in 1:els.endidx
-        dθ[i] = -vmag[i] * θ[i] / dc * log(vmag[i] * θ[i] / dc)
+        dθ[i] = -vmag[i] * θ[i] / dc * log(vmag[i] * θ[i] / dc) # slip law
+        # dθ[i] = 1 - θ[i] * vmag[i] / dc # Aging law
         dv[i] = 1 / (η / els.σn[i] + els.a[i] / vmag[i]) * (dτ[i] / els.σn[i] - els.b[i] * dθ[i] / θ[i])
     end
     dvθ = zeros(3 * els.endidx)
@@ -72,9 +81,19 @@ function calc_dvθ(vθ, p, t)
 end
 
 function ex_planarqdconst()
+    # TODO: contourf plotting?
+    # TODO: Quadratic farfield partials
+    # TODO: Quadratic coincident partials
+    # TODO: Take only fault parallel velocity and traction
+    # TODO: Option: Creeping tensile
+    # TODO: Option: Tensile-slip rate scales with strike-slip rate
+    # TODO: Option: Tensile slip is viscous, v~σ
+
     # Constants and model parameters
     siay = 365.25 * 24 * 60 * 60
-    tspan = (0.0, siay * 500.00)
+    tspan = (0, siay * 800)
+    abstol = 1e-4
+    reltol = 1e-4
     μ = 3e10
     ν = 0.25
     ρ = 2700.0
@@ -82,12 +101,10 @@ function ex_planarqdconst()
     dc = 0.05
     blockvelx = 1e-9
     blockvely = 0.0
-    L = 60 * 1e3
-    Vp = 1e-9
 
     # Create fault elements
     els = Elements()
-    nfault = 50
+    nfault = 200
     nnodes = 1 * nfault
     faultwidth = 10000
     x1, y1, x2, y2 = discretizedline(-faultwidth, 0, faultwidth, 0, nfault)
@@ -115,9 +132,8 @@ function ex_planarqdconst()
 
     # Time integrate elastic model
     prob = ODEProblem(calc_dvθ, ics, tspan, (∂t, els, η, dc, blockvelx, blockvely))
-    println("Integrating")
-    @time sol = solve(prob, RK4(), abstol = 1e-4, reltol = 1e-4)
-
-    plottimeseries(sol, nfault, siay)
+    println("Time integrating")
+    @time sol = solve(prob, RK4(), abstol = abstol, reltol = reltol)
+    plottimeseries(sol)
 end
 ex_planarqdconst()
