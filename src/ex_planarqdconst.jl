edee6cfdbe1b891b0be74eb5e49222ea5840842c
brendanjmeade <brendanjmeade@gmail.com>
2019-09-06 23:56:01 -0400

    Ruptures with creep for fault perpendiculat seem to work for slip law and converge to a cycle invariant state.

--- src/ex_planarqdconst.jl
+++ src/ex_planarqdconst.jl
@@ -50,8 +50,8 @@ function plottimeseries(sol)
 
     figure(figsize = (15, 5))
     plotme = @.log10(vx')
-    contourf(plotme, 20)
-    colorbar()
+    contourf(plotme, 20, cmap = get_cmap("plasma"))
+    colorbar(fraction = 0.020, pad = 0.05, extend = "both", label = L"$\log_{10}v$ (m/s)")
     contour(plotme, 20, linewidths = 0.5, linestyles = "solid", colors = "k")
     xlabel("time step")
     ylabel("element index")
@@ -65,55 +65,33 @@ function calc_dvθ(vθ, p, t)
     vy = vθ[2:3:end]
     θ = vθ[3:3:end]
 
-    # Global block velocities from fault parallel and perpendicular velocity components
-    blockvpara, blockvperp = multmatvecsingle(els.rotmat[1:els.endidx, :, :], blockvx, blockvy)
-    blockvxpara, blockvypara = multmatvec(els.rotmatinv[1:els.endidx, :, :], blockvpara, zeros(size(blockvperp)))
-    blockvxperp, blockvyperp = multmatvec(els.rotmatinv[1:els.endidx, :, :], zeros(size(blockvpara)), blockvperp)
+    # # Global block velocities from fault parallel and perpendicular velocity components
+    # blockvpara, blockvperp = multmatvecsingle(els.rotmat[1:els.endidx, :, :], blockvx, blockvy)
+    # blockvxpara, blockvypara = multmatvec(els.rotmatinv[1:els.endidx, :, :], blockvpara, zeros(size(blockvperp)))
+    # blockvxperp, blockvyperp = multmatvec(els.rotmatinv[1:els.endidx, :, :], zeros(size(blockvpara)), blockvperp)
 
     # Global fault velocites from fault parallel and perpendicular components
     vpara, vperp = multmatvec(els.rotmat[1:els.endidx, :, :], vx, vy)
-    vxpara, vypara = multmatvec(els.rotmatinv[1:els.endidx, :, :], vpara, zeros(size(vperp)))
-    vxperp, vyperp = multmatvec(els.rotmatinv[1:els.endidx, :, :], zeros(size(vpara)), vperp)
+    # vxpara, vypara = multmatvec(els.rotmatinv[1:els.endidx, :, :], vpara, zeros(size(vperp)))
+    # vxperp, vyperp = multmatvec(els.rotmatinv[1:els.endidx, :, :], zeros(size(vpara)), vperp)
     
-    # Change in tractions due to fault parallel motion
-    Δvx = blockvxpara - vxpara
-    Δvy = blockvypara - vypara
-    dtpara = ∂t * [Δvx Δvy]'[:]
-    tparapara, tparaperp = multmatvec(els.rotmat[1:els.endidx, :, :], dtpara[1:2:end], dtpara[2:2:end])
-
-    # # NO TENSILE SLIP - Change in tractions due to fault perpendicular motion
-    # tperppara, tperpperp = zeros(size(vxperp)), zero(size(vxperp))
-
-    # CREEP - Change in tractions due to fault perpendicular motion
-    Δvx = blockvxperp - vxperp
-    Δvy = blockvyperp - vyperp
-    dtperp = ∂t * [Δvx Δvy]'[:]
-    tperppara, tperpperp = multmatvec(els.rotmat[1:els.endidx, :, :], dtperp[1:2:end], dtperp[2:2:end])
-
-    # # VISCOUS - Change in tractions due to fault perpendicular motion
-    # viscosity = 1e19 # Pa⋅s
-    # fault_width = 1.0 # meter
-    # Δvx = blockvxperp - vxperp
-    # Δvy = blockvyperp - vyperp
-    # dtperp = ∂t * [Δvx Δvy]'[:]
-    # tperppara, tperpperp = multmatvec(els.rotmat[1:els.endidx, :, :], dtperp[1:2:end], dtperp[2:2:end])
-
-    # TOTAL - Change in tractions
-    tpara = tparapara + tperppara
-    tperp = tparaperp + tperpperp
+    # Change in tractions (global then local)
+    dt = ∂t * [blockvx .- vx blockvy .- vy]'[:]
+    dtpara, dtperp = multmatvec(els.rotmat[1:els.endidx, :, :], dt[1:2:end], dt[2:2:end])
         
     # Frictional slip for fault parallel traction
-    dτ = tpara # Shear stress on fault
-    dθ = zeros(els.endidx)
-    dv = zeros(els.endidx)
+    dθ, dvpara, dvperp = zeros(els.endidx), zeros(els.endidx), zeros(els.endidx)
     for i in 1:els.endidx
-        dθ[i] = -vpara[i] * θ[i] / dc * log(vpara[i] * θ[i] / dc) # slip law
-        # dθ[i] = 1 - θ[i] * vmag[i] / dc # Aging law
-        dv[i] = 1 / (η / els.σn[i] + els.a[i] / vpara[i]) * (dτ[i] / els.σn[i] - els.b[i] * dθ[i] / θ[i])
+        # dθ[i] = -vpara[i] * θ[i] / dc * log(vpara[i] * θ[i] / dc) # slip law
+        dθ[i] = 1 - θ[i] * vpara[i] / dc # Aging law
+        dvpara[i] = 1 / (η / els.σn[i] + els.a[i] / vpara[i]) * (dtpara[i] / els.σn[i] - els.b[i] * dθ[i] / θ[i])
+        dvperp[i] = 0 # fault perpendicular creep
+        # dvperp[i] = dvpara[i] # fault perpendicular velocity proportional to fault parallel velocity
     end
+    dvx, dvy = multmatvec(els.rotmatinv[1:els.endidx, :, :], dvpara, dvperp)
     dvθ = zeros(3 * els.endidx)
-    dvθ[1:3:end] = dv # TODO: Flat fault only
-    dvθ[2:3:end] .= 0 # TODO: Flat fault only
+    dvθ[1:3:end] = dvx
+    dvθ[2:3:end] = dvy
     dvθ[3:3:end] = dθ
     return dvθ
 end
@@ -121,24 +99,29 @@ end
 function ex_planarqdconst()
     # Constants and model parameters
     siay = 365.25 * 24 * 60 * 60
-    tspan = (0, siay * 500)
-    abstol = 1e-4
-    reltol = 1e-4
+    tspan = (0, siay * 1500)
+    abstol = 1e-8
+    reltol = 1e-8
     μ = 3e10
     ν = 0.25
     ρ = 2700.0
     η = μ / (2.0 * sqrt(μ / ρ))
-    
     dc = 0.05
     blockvelx = 1e-9
     blockvely = 0.0
 
     # Create fault elements
     els = Elements(Int(1e5))
-    nfault = 30
+    nfault = 100
     nnodes = 1 * nfault
     faultwidth = 10000
     x1, y1, x2, y2 = discretizedline(-faultwidth, 0, faultwidth, 0, nfault)
+
+    # Modify y1, and y2 for a sinusoidal fault
+    amplitude = 1000.0
+    y1 = amplitude * @.sin(2 * π * x1 / faultwidth)
+    y2 = amplitude * @.sin(2 * π * x2 / faultwidth)
+
     els.x1[els.endidx + 1:els.endidx + nfault] = x1
     els.y1[els.endidx + 1:els.endidx + nfault] = y1
     els.x2[els.endidx + 1:els.endidx + nfault] = x2
