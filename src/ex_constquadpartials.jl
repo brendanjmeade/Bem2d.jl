85f5b79890467e291fe62cd87b1068965dca5911
brendanjmeade <brendanjmeade@gmail.com>
2019-09-13 00:02:25 -0400

    Quadratic visualization improved.  Need to generalize to multiple test cases.

--- src/ex_constquadpartials.jl
+++ src/ex_constquadpartials.jl
@@ -7,7 +7,7 @@ function ex_constquadpartials()
     # Material and geometric constants
     μ = 3e10
     ν = 0.25
-    nels = 3
+    nels = 10
     els = Elements(Int(1e5))
     L = 10000
     x1, y1, x2, y2 = discretizedline(-L, 0, L, 0, nels)
@@ -36,17 +36,13 @@ function ex_constquadpartials()
     # slipquad[1:2:end] .= 1
     
     # Linear strike-slip
-    slope = 0.001
+    slope = 0.0001
+    slipconst[1:2:end] = slope * xcenters
+    slipquad[1:2:end] = slope * xnodes
     constxslip = slope .* xcenters
     constyslip = zeros(nels)
     quadxslip = transpose(reshape(slope .* xnodes, 3, nels))
     quadyslip = zeros(size(quadxslip))
-
-    slipconst[1:2:end] = 0.001 * xcenters
-    slipquad[1:2:end] = 0.001 * xnodes
-    quadxslip = transpose(reshape(slope .* xnodes, 3, nels))
-    quadyslip = zeros(size(quadxslip))
-    display(slipquad)
 
     # Tensile-slip
     # slipquad[2:2:end] .= 1
@@ -71,54 +67,79 @@ function ex_constquadpartials()
 
     # Plot geometry of elements
     close("all")
-    figure(figsize = (15, 10))
-    subplot(2, 3, 1)
+    nearfield = false
+    fontsize = 6
+    println("hi")
+    figure(figsize = (12, 8))
+    ax = subplot(2, 3, 1)
     for i in 1:els.endidx
         plot([els.x1[i], els.x2[i]], [els.y1[i], els.y2[i]], "-b", color = "b", linewidth = 0.5)
         plot([els.x1[i] els.x2[i]], [els.y1[i] els.y2[i]], ".r", markersize = 10, linewidth = 0.5)
-        text(els.xcenter[i], els.ycenter[i], string(i), horizontalalignment = "center", verticalalignment = "center", fontsize = 8)
+        text(els.xcenter[i], els.ycenter[i], string(i), horizontalalignment = "center", verticalalignment = "center", fontsize = fontsize)
     end
-    ylabel("y (m)"); xlim([-10000, 10000]); xticks([-10000, 0, 10000])
+    ylabel("y (m)", fontsize = fontsize); xlim([-10000, 10000]); xticks([-10000, 0, 10000])
+    ax[:tick_params]("both", labelsize = fontsize)
 
-    subplot(2, 3, 2)
+    ax = subplot(2, 3, 2)
     plot(xcenters, uconst[1:2:end], "ob", markerfacecolor = "none", markeredgewidth = 0.5, label = L"$u_x$, constant")
-    plot(xcenters, uconstnear[:, 1], ".b", markeredgewidth = 0.5, label = L"$u_x$, constant (near)")
     plot(xnodes, uquad[1:2:end], "sr", markerfacecolor = "none", markeredgewidth = 0.5, label = L"$u_x$, quadratic")
-    plot(xnodes, uquadnear[:, 1], ".r", markeredgewidth = 0.5, label = L"$u_x$, quadratic (near)")
-    legend(loc = "lower right"); xlim([-10000, 10000]); xticks([-10000, 0, 10000])
-    ylabel("displacement (m)")
+    if nearfield
+        plot(xcenters, uconstnear[:, 1], ".b", markeredgewidth = 0.5, label = L"$u_x$, constant (near)")
+        plot(xnodes, uquadnear[:, 1], ".r", markeredgewidth = 0.5, label = L"$u_x$, quadratic (near)")
+    end
+    legend(loc = "lower right", fontsize = fontsize); xlim([-10000, 10000]); xticks([-10000, 0, 10000])
+    ylabel("displacement (m)", fontsize = fontsize)
+    ax[:tick_params]("both", labelsize = fontsize)
+    ylim([-0.6, 0.6])
 
-    subplot(2, 3, 3)
+
+    ax = subplot(2, 3, 3)
     plot(xcenters, uconst[2:2:end], "ob", markerfacecolor = "none", markeredgewidth = 0.5, label = L"$u_y$, constant")
-    plot(xcenters, uconstnear[:, 2], ".b", markeredgewidth = 0.5, label = L"$u_y$, constant (near)")
     plot(xnodes, uquad[2:2:end], "sr", markerfacecolor = "none", markeredgewidth = 0.5, label = L"$u_y$, quadratic")
-    plot(xnodes, uquadnear[:, 2], ".r", markeredgewidth = 0.5, label = L"$u_y$, quadratic (near)")
-    legend(loc = "lower right"); xlim([-10000, 10000]); xticks([-10000, 0, 10000])
-    ylabel("displacement (m)")
+    if nearfield
+        plot(xcenters, uconstnear[:, 2], ".b", markeredgewidth = 0.5, label = L"$u_y$, constant (near)")
+        plot(xnodes, uquadnear[:, 2], ".r", markeredgewidth = 0.5, label = L"$u_y$, quadratic (near)")
+    end
+    legend(loc = "lower right", fontsize = fontsize); xlim([-10000, 10000]); xticks([-10000, 0, 10000])
+    ylabel("displacement (m)", fontsize = fontsize)
+    ax[:tick_params]("both", labelsize = fontsize)
+    ylim([-0.6, 0.6])
 
-    subplot(2, 3, 4)
+    ax = subplot(2, 3, 4)
     plot(xcenters, σconst[1:3:end], "ob", markerfacecolor = "none", markeredgewidth = 0.5, label = L"$σ_{xx}$, constant")
-    plot(xcenters, σconstnear[:, 1], ".b", markeredgewidth = 0.5, label = L"$σ_{xx}$, constant (near)")
     plot(xnodes, σquad[1:3:end], "sr", markerfacecolor = "none", markeredgewidth = 0.5, label = L"$σ_{xx}$, quadratic")
-    plot(xnodes, σquadnear[:, 1], ".r", markeredgewidth = 0.5, label = L"$σ_{xx}$, quadratic (near)")
-    legend(loc = "lower right"); xlim([-10000, 10000]); xticks([-10000, 0, 10000])
-    xlabel("x (m)"); ylabel("stress (Pa)")
+    if nearfield
+        plot(xcenters, σconstnear[:, 1], ".b", markeredgewidth = 0.5, label = L"$σ_{xx}$, constant (near)")
+        plot(xnodes, σquadnear[:, 1], ".r", markeredgewidth = 0.5, label = L"$σ_{xx}$, quadratic (near)")
+    end
+    legend(loc = "lower right", fontsize = fontsize); xlim([-10000, 10000]); xticks([-10000, 0, 10000])
+    xlabel("x (m)", fontsize = fontsize); ylabel("stress (Pa)", fontsize = fontsize)
+    ax[:tick_params]("both", labelsize = fontsize)
+    ylim([-1e7, 1e7])
 
-    subplot(2, 3, 5)
+    ax = subplot(2, 3, 5)
     plot(xcenters, σconst[2:3:end], "ob", markerfacecolor = "none", markeredgewidth = 0.5, label = L"$σ_{yy}$, constant")
-    plot(xcenters, σconstnear[:, 2], ".b", markeredgewidth = 0.5, label = L"$σ_{yy}$, constant (near)")
     plot(xnodes, σquad[2:3:end], "sr", markerfacecolor = "none", markeredgewidth = 0.5, label = L"$σ_{yy}$, quadratic")
-    plot(xnodes, σquadnear[:, 2], ".r", markeredgewidth = 0.5, label = L"$σ_{yy}$, quadratic (near)")
-    legend(loc = "lower right"); xlim([-10000, 10000]); xticks([-10000, 0, 10000])
-    xlabel("x (m)"); ylabel("stress (Pa)")
+    if nearfield
+        plot(xcenters, σconstnear[:, 2], ".b", markeredgewidth = 0.5, label = L"$σ_{yy}$, constant (near)")
+        plot(xnodes, σquadnear[:, 2], ".r", markeredgewidth = 0.5, label = L"$σ_{yy}$, quadratic (near)")
+    end
+    legend(loc = "lower right", fontsize = fontsize); xlim([-10000, 10000]); xticks([-10000, 0, 10000])
+    xlabel("x (m)", fontsize = fontsize); ylabel("stress (Pa)", fontsize = fontsize)
+    ax[:tick_params]("both", labelsize = fontsize)
+    ylim([-1e7, 1e7])
 
-    subplot(2, 3, 6)
+    ax = subplot(2, 3, 6)
     plot(xcenters, σconst[3:3:end], "ob", markerfacecolor = "none", markeredgewidth = 0.5, label = L"$σ_{xy}$, constant")
-    plot(xcenters, σconstnear[:, 3], ".b", markeredgewidth = 0.5, label = L"$σ_{xy}$, constant (near)")
     plot(xnodes, σquad[3:3:end], "sr", markerfacecolor = "none", markeredgewidth = 0.5, label = L"$σ_{xy}$, quadratic")
-    plot(xnodes, σquadnear[:, 3], ".r", markeredgewidth = 0.5, label = L"$σ_{xy}$, quadratic (near)")
-    legend(loc = "lower right"); xlim([-10000, 10000]); xticks([-10000, 0, 10000])
-    xlabel("x (m)"); ylabel("stress (Pa)")
+    if nearfield
+        plot(xcenters, σconstnear[:, 3], ".b", markeredgewidth = 0.5, label = L"$σ_{xy}$, constant (near)")
+        plot(xnodes, σquadnear[:, 3], ".r", markeredgewidth = 0.5, label = L"$σ_{xy}$, quadratic (near)")
+    end
+    legend(loc = "lower right", fontsize = fontsize); xlim([-10000, 10000]); xticks([-10000, 0, 10000])
+    xlabel("x (m)", fontsize = fontsize); ylabel("stress (Pa)", fontsize = fontsize)
+    ax[:tick_params]("both", labelsize = fontsize)
+    ylim([-1e7, 1e7])
     tight_layout(); show()
 end
 ex_constquadpartials()
