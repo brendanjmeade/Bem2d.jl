a98be70e55dc0b77ca0e186e8a79e071cba9a40f
brendanjmeade <brendanjmeade@gmail.com>
2019-09-27 00:06:45 -0400

    Fixed free surface quadratic error.  It was the result of improperly checking for coincident case.  Only required a change in if statment in Bem2d.  Need to clean up plotting and show comparision with analytic solution.

--- src/ex_freesurface.jl
+++ src/ex_freesurface.jl
@@ -4,16 +4,12 @@ using PyPlot
 using Bem2d
 
 function ex_freesurface()
-    # Material properties and observation grid
     μ = 30e9
     ν = 0.25
-    npts = 50
-    obswidth = 5
-    xobs, yobs = obsgrid(-obswidth, -obswidth, obswidth, obswidth, npts)
 
     # Free surface
     els = Elements(Int(1e5))
-    nfreesurf = 20
+    nfreesurf = 40
     x1, y1, x2, y2 = discretizedline(-5, 0, 5, 0, nfreesurf)
     els.x1[els.endidx + 1:els.endidx + nfreesurf] = x1
     els.y1[els.endidx + 1:els.endidx + nfreesurf] = y1
@@ -35,26 +31,26 @@ function ex_freesurface()
     # Constant slip fault
     srcidx = findall(x->x == "fault", els.name)
     obsidx = findall(x->x == "freesurf", els.name)
-    ∂u1, ∂σ1, ∂t1 = ∂constuσ(slip2uσ, els, srcidx, obsidx, μ, ν)
+    ∂u1const, ∂σ1const, ∂t1const = ∂constuσ(slip2uσ, els, srcidx, obsidx, μ, ν)
     srcidx = findall(x->x == "freesurf", els.name)
     obsidx = findall(x->x == "freesurf", els.name)
-    ∂u2, ∂σ2, ∂t2 = ∂constuσ(slip2uσ, els, srcidx, obsidx, μ, ν)
+    ∂u2const, ∂σ2const, ∂t2const = ∂constuσ(slip2uσ, els, srcidx, obsidx, μ, ν)
     faultslipconst = sqrt(2) / 2 * [1 ; 1]
-    ufullspaceconst = ∂u1 * faultslipconst
-    ufreesurfaceconst = inv(∂t2) * (∂t1 * faultslipconst)
+    ufullspaceconst = ∂u1const * faultslipconst
+    ufreesurfaceconst = inv(∂t2const) * (∂t1const * faultslipconst)
     xplotconst = els.xcenter[findall(x->x == "freesurf", els.name)]
 
     # Quadratic slip fault
     srcidx = findall(x->x == "fault", els.name)
     obsidx = findall(x->x == "freesurf", els.name)
-    ∂u1, ∂σ1, ∂t1 = ∂quaduσ(slip2uσ, els, srcidx, obsidx, μ, ν)
+    ∂u1quad, ∂σ1quad, ∂t1quad = ∂quaduσ(slip2uσ, els, srcidx, obsidx, μ, ν)
     srcidx = findall(x->x == "freesurf", els.name)
     obsidx = findall(x->x == "freesurf", els.name)
-    ∂u2, ∂σ2, ∂t2 = ∂quaduσ(slip2uσ, els, srcidx, obsidx, μ, ν)
-    faultslipquad = sqrt(2) ./ 2 * ones(6)
-    ufullspacequad = ∂u1 * faultslipquad
-    ufreesurfacequad = inv(∂t2) * (∂t1 * faultslipquad)
+    ∂u2quad, ∂σ2quad, ∂t2quad = ∂quaduσ(slip2uσ, els, srcidx, obsidx, μ, ν)
     xplotquad = sort(els.xnodes[obsidx, :][:])
+    faultslipquad = sqrt(2) ./ 2 * ones(6)
+    ufullspacequad = ∂u1quad * faultslipquad
+    ufreesurfacequad = inv(∂t2quad) * (∂t1quad * faultslipquad)
 
     fontsize = 6
     markersize = 4
@@ -62,26 +58,27 @@ function ex_freesurface()
     close("all")
     figure(figsize = (5, 5))
 
-    subplot(2, 1, 1)
-    plot(xplotconst, ufullspaceconst[1:2:end], "b.", markeredgewidth=linewidth, markersize=markersize, label = "const fullspace")
+    ax = subplot(2, 1, 1)
+    plot(xplotconst, ufullspaceconst[1:2:end], "bo", markeredgewidth=linewidth, markersize=markersize, label = "const fullspace")
     plot(xplotconst, ufreesurfaceconst[1:2:end], "b+", markeredgewidth=linewidth, markersize=markersize, label = "const halfspace")
     plot(xplotquad, ufullspacequad[1:2:end], "r.", markeredgewidth=linewidth, markersize=markersize, label = "quad fullspace")
     plot(xplotquad, ufreesurfacequad[1:2:end], "r+", markeredgewidth=linewidth, markersize=markersize, label = "quad halfspace")
-    # gca().set_xlim([-5, 5]); gca().set_ylim([-0.6, 0.6])
-    # gca().set_xticks([-5, 0, 5]); gca().set_yticks([-0.5, 0.0, 0.5])
+    gca().set_xlim([-5, 5]); gca().set_ylim([-0.6, 0.6])
+    gca().set_xticks([-5, 0, 5]); gca().set_yticks([-0.5, 0.0, 0.5])
     legend(fontsize=fontsize)
+    ax.tick_params("both", labelsize = fontsize)
     ylabel(L"$u_x$ (m)", fontsize=fontsize)
 
-    subplot(2, 1, 2)
-    plot(xplotconst, ufullspaceconst[2:2:end], "b.", markeredgewidth=linewidth, markersize=markersize, label = "const fullspace")
+    ax = subplot(2, 1, 2)
+    plot(xplotconst, ufullspaceconst[2:2:end], "bo", markeredgewidth=linewidth, markersize=markersize, label = "const fullspace")
     plot(xplotconst, ufreesurfaceconst[2:2:end], "b+", markeredgewidth=linewidth, markersize=markersize, label = "const halfspace")
     plot(xplotquad, ufullspacequad[2:2:end], "r.", markeredgewidth=linewidth, markersize=markersize, label = "quad fullspace")
     plot(xplotquad, ufreesurfacequad[2:2:end], "r+", markeredgewidth=linewidth, markersize=markersize, label = "quad halfspace")
-    # gca().set_xlim([-5, 5]); gca().set_ylim([-0.6, 0.6])
-    # gca().set_xticks([-5, 0, 5]); gca().set_yticks([-0.5, 0.0, 0.5])
+    gca().set_xlim([-5, 5]); gca().set_ylim([-0.6, 0.6])
+    gca().set_xticks([-5, 0, 5]); gca().set_yticks([-0.5, 0.0, 0.5])
     legend(fontsize=fontsize)
+    ax.tick_params("both", labelsize = fontsize)
     xlabel(L"$x$ (m)", fontsize=fontsize); ylabel(L"$u_y$ (m)", fontsize=fontsize)
     show()
-
 end
 ex_freesurface()
