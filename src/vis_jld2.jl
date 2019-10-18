65ae4136d1b2954c53067bf950bdf4509de7412e
brendanjmeade <brendanjmeade@gmail.com>
2019-10-18 12:13:38 -0400

    Successful calculation of number of faults and splitting time series from slip/velocity plotting

--- src/vis_jld2.jl
+++ src/vis_jld2.jl
@@ -3,11 +3,12 @@ using JLD2
 using PyCall
 using PyPlot
 
-function plottimeseries(sol)
-    filename = "2019-10-17T15:18:07.899_cf0c11f0-4bb7-4846-94dd-50fe7249a463.jld2"
+function plottimeseries()
+    filename = "2019-10-18T12:02:03.37.jld2"
     JLD2.@load filename sol
+
     siay = 365.25 * 24 * 60 * 60
-    nfault = Int(size(sol)[1] / 3)
+    nfault = Int(size(sol.u[1])[1] / 3)
     t = [x / siay for x in sol.t]
     θ = zeros(length(t), nfault)
     vx = zeros(length(t), nfault)
@@ -57,3 +58,31 @@ function plottimeseries(sol)
     PyPlot.ylabel("element index")
     PyPlot.show()
 end
+# plottimeseries()
+
+function plotvelocities()
+    filename = "2019-10-18T12:02:03.37.jld2"
+    JLD2.@load filename sol
+
+    siay = 365.25 * 24 * 60 * 60
+    nfault = Int(size(sol.u[1])[1] / 3)
+    t = [x / siay for x in sol.t]
+    θ = zeros(length(t), nfault)
+    vx = zeros(length(t), nfault)
+    vy = zeros(length(t), nfault)
+    for i in 1:length(t)
+        vx[i, :] = sol.u[i][1:3:end]
+        vy[i, :] = sol.u[i][2:3:end]
+        θ[i, :] = sol.u[i][3:3:end]
+    end
+
+    PyPlot.figure(figsize = (15, 5))
+    plotme = log10.(vx')
+    PyPlot.contourf(plotme, 200, cmap = get_cmap("plasma"))
+    PyPlot.colorbar(fraction = 0.020, pad = 0.05, extend = "both", label = L"$\log_{10}v$ (m/s)")
+    PyPlot.contour(plotme, 20, linewidths = 0.5, linestyles = "solid", colors = "k")
+    PyPlot.xlabel("time step")
+    PyPlot.ylabel("element index")
+    PyPlot.show()
+end
+plotvelocities()
