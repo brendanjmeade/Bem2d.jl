7732065efb849fef6a37b51e08fd752b229ddfe7
brendanjmeade <brendanjmeade@gmail.com>
2019-10-18 14:00:44 -0400

    Cumulative slip is now calculated

--- src/vis_jld2.jl
+++ src/vis_jld2.jl
@@ -3,6 +3,8 @@ using JLD2
 using PyCall
 using PyPlot
 
+PyPlot.close("all")
+
 function plottimeseries()
     filename = "2019-10-18T12:02:03.37.jld2"
     JLD2.@load filename sol
@@ -76,13 +78,45 @@ function plotvelocities()
         θ[i, :] = sol.u[i][3:3:end]
     end
 
+    # Calculate cumulative slip
+    sx = zeros(size(vx))
+    # for itime in 1:length(t)
+    #     for ifault in 1:nfault
+    #         # sx[itime, ifault] = sum(vx[1:itime, ifault])
+    #         sx[itime, ifault] = sum(vx[1:itime, ifault])
+    #     end
+    # end
+
+    # Swap order of loops here?
+    # dt = diff(sol.t)
+    for ifault in 1:nfault
+        for itime in 2:length(t)
+            # sx[itime, ifault] = sx[itime - 1, ifault] + vx[itime, ifault] * dt[itime - 1]
+            sx[itime, ifault] = sx[itime - 1, ifault] + vx[itime, ifault] * (t[itime] - t[itime - 1])
+        end
+    end
+
+
+    # PyPlot.figure(figsize = (15, 5))
+    # plotme = log10.(vx')
+    # PyPlot.contourf(plotme, 200, cmap = get_cmap("plasma"))
+    # PyPlot.colorbar(fraction = 0.020, pad = 0.05, extend = "both", label = L"$\log_{10}v$ (m/s)")
+    # PyPlot.contour(plotme, 20, linewidths = 0.5, linestyles = "solid", colors = "k")
+    # PyPlot.xlabel("time step")
+    # PyPlot.ylabel("element index")
+    # PyPlot.show()
+
     PyPlot.figure(figsize = (15, 5))
-    plotme = log10.(vx')
+    # for i in 1:30
+    #     PyPlot.plot(sx[], "-k")
+    # end
+    plotme = sx'
     PyPlot.contourf(plotme, 200, cmap = get_cmap("plasma"))
     PyPlot.colorbar(fraction = 0.020, pad = 0.05, extend = "both", label = L"$\log_{10}v$ (m/s)")
     PyPlot.contour(plotme, 20, linewidths = 0.5, linestyles = "solid", colors = "k")
     PyPlot.xlabel("time step")
     PyPlot.ylabel("element index")
     PyPlot.show()
+
 end
 plotvelocities()
