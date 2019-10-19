1471e65cfd3c1bf10ed4d777dd18c0d211d13b77
brendanjmeade <brendanjmeade@gmail.com>
2019-10-19 00:00:12 -0400

    Some better colormaps for plotting.  Reducing the number of color contours really helps in seeing details.

--- src/vis_jld2.jl
+++ src/vis_jld2.jl
@@ -2,12 +2,12 @@ using Revise
 using JLD2
 using PyCall
 using PyPlot
+using Bem2d
 
 PyPlot.close("all")
 
 function plottimeseries()
     filename = "2019-10-18T14:40:57.735.jld2"
-
     JLD2.@load filename sol
 
     siay = 365.25 * 24 * 60 * 60
@@ -64,11 +64,12 @@ end
 # plottimeseries()
 
 function plotvelocities()
-    # filename = "2019-10-18T14:40:57.735.jld2"
-    filename = "2019-10-18T12:02:03.37.jld2"
+    filename = "2019-10-18T14:40:57.735.jld2"
+    # filename = "2019-10-18T12:02:03.37.jld2"
 
     JLD2.@load filename sol
 
+    blockvelx = 1e-9 # should read this in from .jld2 file
     siay = 365.25 * 24 * 60 * 60
     nfault = Int(size(sol.u[1])[1] / 3)
     t = [x / siay for x in sol.t]
@@ -83,42 +84,42 @@ function plotvelocities()
 
     # Calculate cumulative slip
     sx = zeros(size(vx))
-    # for itime in 1:length(t)
-    #     for ifault in 1:nfault
-    #         # sx[itime, ifault] = sum(vx[1:itime, ifault])
-    #         sx[itime, ifault] = sum(vx[1:itime, ifault])
-    #     end
-    # end
+    sxlinear = zeros(size(vx))
 
-    # Swap order of loops here?
     for itime in 2:length(t)
         for ifault in 1:nfault
             sx[itime, ifault] = sx[itime - 1, ifault] + vx[itime, ifault] * (t[itime] - t[itime - 1])
+            sxlinear[itime, ifault] = sxlinear[itime - 1, ifault] + blockvelx * (t[itime] - t[itime - 1])
         end
     end
 
 
+    # cmap = rycroftcmap()
+    fontsize = 24
     PyPlot.figure(figsize = (35, 10))
-    plotme = log10.(vx')
-    PyPlot.contourf(plotme, 200, cmap = get_cmap("plasma"))
+    plotme = log10.(transpose(vx))
+    PyPlot.contourf(plotme, 10, cmap = rycroftcmap())
     PyPlot.colorbar(fraction = 0.020, pad = 0.05, extend = "both", label = L"$\log_{10}v$ (m/s)")
-    PyPlot.contour(plotme, 20, linewidths = 0.5, linestyles = "solid", colors = "k")
-    PyPlot.xlabel("time step")
-    PyPlot.ylabel("element index")
-    PyPlot.show()
+    PyPlot.contour(plotme, 10, linewidths = 0.5, linestyles = "solid", colors = "k")
+    PyPlot.xlabel("time step", fontsize=fontsize)
+    PyPlot.ylabel("element index", fontsize=fontsize)
 
-    PyPlot.figure(figsize = (25, 10))
-    # nlines = 100
-    # plotidx = Int.(round.(collect(LinRange(1, length(sol.t)-1, nlines))))
-    # for i in 1:nlines
-    #     PyPlot.plot(sx[plotidx[i], :], "-k")
-    # end
-    plotme = sx'
-    PyPlot.contourf(plotme, 200, cmap = get_cmap("plasma"))
+    PyPlot.figure(figsize = (35, 10))
+    plotme = transpose(sx)
+    PyPlot.contourf(plotme, 20, cmap = get_cmap("plasma"))
     PyPlot.colorbar(fraction = 0.020, pad = 0.05, extend = "both", label = L"$\log_{10}v$ (m/s)")
     PyPlot.contour(plotme, 20, linewidths = 0.5, linestyles = "solid", colors = "k")
-    PyPlot.xlabel("time step")
-    PyPlot.ylabel("element index")
+    PyPlot.xlabel("time step", fontsize=fontsize)
+    PyPlot.ylabel("element index", fontsize=fontsize)
+
+    PyPlot.figure(figsize = (35, 10))
+    plotme = transpose(sxlinear - sx)
+    PyPlot.contourf(plotme, 20, cmap = get_cmap("PRGn"), vmin=-1e-7, vmax=1e-7)
+    PyPlot.colorbar(fraction = 0.020, pad = 0.05, extend = "both", label = L"$block motion - slip$ (m/s)")
+    PyPlot.contour(plotme, [0], linewidths = 0.5, linestyles = "solid", colors = "k")
+    PyPlot.xlabel("time step", fontsize=fontsize)
+    PyPlot.ylabel("element index", fontsize=fontsize)
+
     PyPlot.show()
 
 end
