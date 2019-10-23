5c2443c52e8db3251a104fba211e757c039cca75
brendanjmeade <brendanjmeade@gmail.com>
2019-10-22 21:16:24 -0400

    New file for trying to evaluate how continuity works.  First step is old-school looping for geometry assignement

---
+++ src/ex_freesurfacecont.jl
@@ -0,0 +1,114 @@
+using Revise
+using PyCall
+using PyPlot
+using Bem2d
+
+
+function ex_freesurface()
+    μ = 30e9
+    ν = 0.25
+
+    # Free surface
+    els = Elements(Int(1e5))
+    nfreesurf = 20
+    x1, y1, x2, y2 = discretizedline(-5, 0, 5, 0, nfreesurf)
+    for i in 1:length(x1)
+        els.x1[els.endidx + i] = x1[i]
+        els.y1[els.endidx + i] = y1[i]
+        els.x2[els.endidx + i] = x2[i]
+        els.y2[els.endidx + i] = y2[i]
+        els.name[els.endidx + i] = "freesurf"
+    end
+    standardize_elements!(els)
+
+    # 45 degree dipping fault
+    nfault = 1
+    x1, y1, x2, y2 = discretizedline(-1, -1, 0, 0, nfault)
+    for i in 1:length(x1)
+        els.x1[els.endidx + i] = x1[i]
+        els.y1[els.endidx + i] = y1[i]
+        els.x2[els.endidx + i] = x2[i]
+        els.y2[els.endidx + i] = y2[i]
+        els.name[els.endidx + i] = "fault"
+    end
+    standardize_elements!(els)
+
+    # Constant slip fault
+    srcidx = findall(x->x == "fault", els.name)
+    obsidx = findall(x->x == "freesurf", els.name)
+    ∂u1const, ∂σ1const, ∂t1const = ∂constuσ(slip2uσ, els, srcidx, obsidx, μ, ν)
+    srcidx = findall(x->x == "freesurf", els.name)
+    obsidx = findall(x->x == "freesurf", els.name)
+    ∂u2const, ∂σ2const, ∂t2const = ∂constuσ(slip2uσ, els, srcidx, obsidx, μ, ν)
+    faultslipconst = sqrt(2) / 2 * [1 ; 1]
+    ufullspaceconst = ∂u1const * faultslipconst
+    ufreesurfaceconst = inv(∂t2const) * (∂t1const * faultslipconst)
+    xplotconst = els.xcenter[findall(x->x == "freesurf", els.name)]
+
+    # Quadratic slip fault
+    srcidx = findall(x->x == "fault", els.name)
+    obsidx = findall(x->x == "freesurf", els.name)
+    ∂u1quad, ∂σ1quad, ∂t1quad = ∂quaduσ(slip2uσ, els, srcidx, obsidx, μ, ν)
+    srcidx = findall(x->x == "freesurf", els.name)
+    obsidx = findall(x->x == "freesurf", els.name)
+    ∂u2quad, ∂σ2quad, ∂t2quad = ∂quaduσ(slip2uσ, els, srcidx, obsidx, μ, ν)
+    xplotquad = sort(els.xnodes[obsidx, :][:])
+    faultslipquad = sqrt(2) ./ 2 * ones(6)
+    ufullspacequad = ∂u1quad * faultslipquad
+    ufreesurfacequad = inv(∂t2quad) * (∂t1quad * faultslipquad)
+
+    # Okada solution
+    ow = pyimport("okada_wrapper")# from okada_wrapper import dc3dwrapper
+    xokada = collect(LinRange(-5, 5, 1000))
+    uxokada = zeros(length(xokada))
+    uyokada = zeros(length(xokada))
+
+    for i in 1:length(xokada)
+        # Fault dipping at 45 degrees
+        _, u, _ = ow.dc3dwrapper(
+            2.0 / 3.0,
+            [0, xokada[i] + 0.5, 0],
+            0.5,
+            45,  # 135
+            [-1000, 1000],
+            [-sqrt(2) / 2, sqrt(2) / 2],
+            [0.0, 1.0, 0.0],
+        )
+        uxokada[i] = u[2]
+        uyokada[i] = u[3]
+    end
+
+    fontsize = 24
+    markersize = 15
+    linewidth = 0.5
+    close("all")
+    figure(figsize = (14, 18))
+
+    ax = subplot(2, 1, 1)
+    plot(xokada, uxokada, "-k", linewidth=linewidth, label="Okada")
+    plot(xplotconst, ufreesurfaceconst[1:2:end], "bo", markeredgewidth=linewidth, markersize=markersize, label = "const halfspace")
+    plot(xplotquad, ufreesurfacequad[1:2:end], "r+", markeredgewidth=linewidth, markersize=markersize, label = "quad halfspace")
+    gca().set_xlim([-5, 5]); gca().set_ylim([-1.0, 1.0])
+    gca().set_xticks([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5])
+    gca().set_yticks([-1.00, -0.75, -0.50, -0.25, 0.0, 0.25, 0.50, 0.75, 1.00])
+    legend(fontsize=fontsize)
+    ax.tick_params("both", labelsize = fontsize)
+    ylabel(L"$u_x$ (m)", fontsize=fontsize)
+
+    ax = subplot(2, 1, 2)
+    plot(xokada, uyokada, "-k", linewidth=linewidth, label="Okada")
+    plot(xplotconst, ufreesurfaceconst[2:2:end], "bo", markeredgewidth=linewidth, markersize=markersize, label = "const halfspace")
+    plot(xplotquad, ufreesurfacequad[2:2:end], "r+", markeredgewidth=linewidth, markersize=markersize, label = "quad halfspace")
+    gca().set_xlim([-5, 5]); gca().set_ylim([-1.0, 1.0])
+    gca().set_xticks([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5])
+    gca().set_yticks([-1.00, -0.75, -0.50, -0.25, 0.0, 0.25, 0.50, 0.75, 1.00])
+    legend(fontsize=fontsize)
+    ax.tick_params("both", labelsize = fontsize)
+    xlabel(L"$x$ (m)", fontsize=fontsize); ylabel(L"$u_y$ (m)", fontsize=fontsize)
+    show()
+
+    # Now do volume solution to assess the effects of discontinuities
+    # ufaultconst
+
+end
+ex_freesurface()
