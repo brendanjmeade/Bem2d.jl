631f9d39bffffa07d9ec98c1cba2af0d85ac3eb4
brendanjmeade <brendanjmeade@gmail.com>
2019-10-31 23:32:05 -0400

    Nice figure rendering for stress residuals. Scaled to real geometry.  Hopefully done.

--- src/ex_freesurfacecont.jl
+++ src/ex_freesurfacecont.jl
@@ -12,9 +12,9 @@ function ex_freesurface()
     # Free surface
     els = Elements(Int(1e5))
     # nfreesurf = 20
-    # x1, y1, x2, y2 = discretizedline(-5e3, 0, 5e3, 0, nfreesurf)
-    nfreesurf = 200
-    x1, y1, x2, y2 = discretizedline(-50e3, 0, 50e3, 0, nfreesurf)
+    # x1, y1, x2, y2 = discretizedline(-50e3, 0, 50e3, 0, nfreesurf)
+    nfreesurf = 100
+    x1, y1, x2, y2 = discretizedline(-100e3, 0, 100e3, 0, nfreesurf)
     for i in 1:length(x1)
         els.x1[els.endidx + i] = x1[i]
         els.y1[els.endidx + i] = y1[i]
@@ -26,7 +26,7 @@ function ex_freesurface()
 
     # 45 degree dipping fault
     nfault = 1
-    x1, y1, x2, y2 = discretizedline(-1e3, -1e3, 0, 0, nfault)
+    x1, y1, x2, y2 = discretizedline(-10e3, -10e3, 0, 0, nfault)
     for i in 1:length(x1)
         els.x1[els.endidx + i] = x1[i]
         els.y1[els.endidx + i] = y1[i]
@@ -54,8 +54,9 @@ function ex_freesurface()
 
     # Okada solution
     ow = pyimport("okada_wrapper")# from okada_wrapper import dc3dwrapper
-    xokada = collect(LinRange(-5e3, 5e3, 1000))
-    yokada = -1.666666667e2 * ones(size(xokada))
+    xokada = collect(LinRange(-50e3, 50e3, 1000))
+    offset = abs(els.xnodes[1, 2] - els.xnodes[1, 1])
+    yokada = -offset * ones(size(xokada))
     uxokada = zeros(length(xokada))
     uyokada = zeros(length(xokada))
     σxxokada = zeros(length(xokada))
@@ -66,11 +67,11 @@ function ex_freesurface()
         # Fault dipping at 45 degrees
         _, u, s = ow.dc3dwrapper(
             2.0 / 3.0,
-            [0, xokada[i] + 0.5e3, yokada[i]],
-            0.5e3,
+            [0, xokada[i] + 5e3, yokada[i]],
+            5e3,
             45,
-            [-1e5, 1e5],
-            [-1e3*sqrt(2) / 2, 1e3*sqrt(2) / 2],
+            [-1e7, 1e7],
+            [-10e3*sqrt(2) / 2, 10e3*sqrt(2) / 2],
             [0.0, 1.0, 0.0],
         )
         uxokada[i] = u[2]
@@ -109,69 +110,73 @@ function ex_freesurface()
     # Plot ux and uy profiles
     fontsize = 24
     markersize = 15
-    linewidth = 5.0
+    linewidth = 2.0
     close("all")
     figure(figsize = (30, 15))
 
     ax = subplot(2, 3, 1)
     plot(xokada, log10.(abs.(σconst[:, 1])), "-c", linewidth=linewidth, label="CS BEM")
     plot(xokada, log10.(abs.(σquad[:, 1])), "-r", linewidth=linewidth, label="3QN BEM")
-    plot(xokada, log10.(abs.(σxxokada[:, 1])), "--k", linewidth=2.0, label="Okada")
-    # gca().set_xlim([-5e3, 5e3])
-    # gca().set_ylim([6, 12])
-    # gca().set_xticks([-5e3, 0, 5e3])
-    # gca().set_yticks([6, 9, 12])
-    legend(fontsize=fontsize, frameon=false)
+    plot(xokada, log10.(abs.(σxxokada[:, 1])), ":k", linewidth=2.0, label="Okada")
+    gca().set_xlim([-50e3, 50e3])
+    gca().set_ylim([0, 8])
+    gca().set_xticks([])
+    gca().set_yticks([0, 4, 8])
+    legend(fontsize=fontsize, frameon=true, facecolor="white", framealpha=1.0, loc=1)
     ax.tick_params("both", labelsize = fontsize)
-    xlabel(L"$x$ (m)", fontsize=fontsize); ylabel(L"$\log \, \sigma_{xx}$ (Pa)", fontsize=fontsize)
+    # xlabel(L"$x$ (m)", fontsize=fontsize)
+    ylabel(L"$\log \, \sigma_{xx}$ (Pa)", fontsize=fontsize)
 
     ax = subplot(2, 3, 2)
     plot(xokada, log10.(abs.(σconst[:, 2])), "-c", linewidth=linewidth, label="CS BEM")
     plot(xokada, log10.(abs.(σquad[:, 2])), "-r", linewidth=linewidth, label="3QN BEM")
-    plot(xokada, log10.(abs.(σyyokada[:, 1])), "--k", linewidth=2.0, label="Okada")
-    # gca().set_xlim([-5e3, 5e3])
-    # gca().set_ylim([6, 12])
-    # gca().set_xticks([-5e3, 0, 5e3])
-    # gca().set_yticks([6, 9, 12])
-    legend(fontsize=fontsize, frameon=false)
+    plot(xokada, log10.(abs.(σyyokada[:, 1])), ":k", linewidth=2.0, label="Okada")
+    gca().set_xlim([-50e3, 50e3])
+    gca().set_ylim([0, 8])
+    gca().set_xticks([])
+    gca().set_yticks([0, 4, 8])
+    legend(fontsize=fontsize, frameon=true, facecolor="white", framealpha=1.0, loc=1)
     ax.tick_params("both", labelsize = fontsize)
-    xlabel(L"$x$ (m)", fontsize=fontsize); ylabel(L"$\log \, \sigma_{yy}$ (Pa)", fontsize=fontsize)
+    # xlabel(L"$x$ (m)", fontsize=fontsize)
+    ylabel(L"$\log \, \sigma_{yy}$ (Pa)", fontsize=fontsize)
 
     ax = subplot(2, 3, 3)
     plot(xokada, log10.(abs.(σconst[:, 3])), "-c", linewidth=linewidth, label="CS BEM")
     plot(xokada, log10.(abs.(σquad[:, 3])), "-r", linewidth=linewidth, label="3QN BEM")
-    plot(xokada, log10.(abs.(σxyokada[:, 1])), "--k", linewidth=2.0, label="Okada")
-    # gca().set_xlim([-5e3, 5e3])
-    # gca().set_ylim([6, 12])
-    # gca().set_xticks([-5e3, 0, 5e3])
-    # gca().set_yticks([6, 9, 12])
-    legend(fontsize=fontsize, frameon=false)
+    plot(xokada, log10.(abs.(σxyokada[:, 1])), ":k", linewidth=2.0, label="Okada")
+    gca().set_xlim([-50e3, 50e3])
+    gca().set_ylim([0, 8])
+    gca().set_xticks([])
+    gca().set_yticks([0, 4, 8])
+    legend(fontsize=fontsize,frameon=true, facecolor="white", framealpha=1.0, loc=1)
     ax.tick_params("both", labelsize = fontsize)
-    xlabel(L"$x$ (m)", fontsize=fontsize); ylabel(L"$\log \, \sigma_{xy}$ (Pa)", fontsize=fontsize)
+    # xlabel(L"$x$ (m)", fontsize=fontsize)
+    ylabel(L"$\log \, \sigma_{xy}$ (Pa)", fontsize=fontsize)
 
     ax = subplot(2, 3, 4)
     σxxconsterror = 100 * (σconst[:, 1] - σxxokada[:, 1]) ./ σxxokada[:, 1]
     σxxquaderror = 100 * (σquad[:, 1] - σxxokada[:, 1]) ./ σxxokada[:, 1]
     plot(xokada, log10.(abs.(σxxconsterror)), "-c", linewidth=linewidth, label=@sprintf "CS BEM median %% error = %05.2f" median(abs.(σxxconsterror)))
     plot(xokada, log10.(abs.(σxxquaderror)), "-r", linewidth=linewidth, label=@sprintf "3QN BEM median %% error = %05.2f" median(abs.(σxxquaderror)))
-    gca().set_xlim([-5e3, 5e3])
+    gca().set_xlim([-50e3, 50e3])
     gca().set_ylim([-3, 6])
-    gca().set_xticks([-5e3, 0, 5e3])
+    gca().set_xticks([-50e3, 0, 50e3])
     gca().set_yticks([-3, 0, 3, 6])
-    legend(fontsize=fontsize, frameon=false)
+    legend(fontsize=fontsize, frameon=true, facecolor="white", framealpha=1.0, loc=1)
     ax.tick_params("both", labelsize = fontsize)
-    xlabel(L"$x$ (m)", fontsize=fontsize); ylabel(L"$\log \, \sigma_{xx} \, \%$ error", fontsize=fontsize)
+    xlabel(L"$x$ (m)", fontsize=fontsize)
+    ylabel(L"$\log \, \sigma_{xx} \, \%$ error", fontsize=fontsize)
 
     ax = subplot(2, 3, 5)
     σyyconsterror = 100 * (σconst[:, 2] - σyyokada[:, 1]) ./ σyyokada[:, 1]
     σyyquaderror = 100 * (σquad[:, 2] - σyyokada[:, 1]) ./ σyyokada[:, 1]
     plot(xokada, log10.(abs.(σyyconsterror)), "-c", linewidth=linewidth, label=@sprintf "CS BEM median %% error = %05.2f" median(abs.(σyyconsterror)))
     plot(xokada, log10.(abs.(σyyquaderror)), "-r", linewidth=linewidth, label=@sprintf "3QN BEM median %% error = %05.2f" median(abs.(σyyquaderror)))
-    gca().set_xlim([-5e3, 5e3])
+    gca().set_xlim([-50e3, 50e3])
     gca().set_ylim([-3, 6])
-    gca().set_xticks([-5e3, 0, 5e3])
+    gca().set_xticks([-50e3, 0, 50e3])
     gca().set_yticks([-3, 0, 3, 6])
-    legend(fontsize=fontsize, frameon=false)
+    lh = legend(fontsize=fontsize, frameon=true, facecolor="white", framealpha=1.0, loc=1)
     ax.tick_params("both", labelsize = fontsize)
     xlabel(L"$x$ (m)", fontsize=fontsize); ylabel(L"$\log \, \sigma_{yy} \, \%$ error", fontsize=fontsize)
 
@@ -180,11 +185,11 @@ function ex_freesurface()
     σxyquaderror = 100 * (σquad[:, 3] - σxyokada[:, 1]) ./ σxyokada[:, 1]
     plot(xokada, log10.(abs.(σxyconsterror)), "-c", linewidth=linewidth, label=@sprintf "CS BEM median %% error = %05.2f" median(abs.(σxyconsterror)))
     plot(xokada, log10.(abs.(σxyquaderror)), "-r", linewidth=linewidth, label=@sprintf "3QN BEM median %% error = %05.2f" median(abs.(σxyquaderror)))
-    gca().set_xlim([-5e3, 5e3])
+    gca().set_xlim([-50e3, 50e3])
     gca().set_ylim([-3, 6])
-    gca().set_xticks([-5e3, 0, 5e3])
+    gca().set_xticks([-50e3, 0, 50e3])
     gca().set_yticks([-3, 0, 3, 6])
-    legend(fontsize=fontsize, frameon=false)
+    legend(fontsize=fontsize, frameon=true, facecolor="white", framealpha=1.0, loc=1)
     ax.tick_params("both", labelsize = fontsize)
     xlabel(L"$x$ (m)", fontsize=fontsize); ylabel(L"$\log \, \sigma_{xy} \, \%$ error", fontsize=fontsize)
 
