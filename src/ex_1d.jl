217e54d5650446a7b703d35900fdd53aad50cd13
brendanjmeade <brendanjmeade@gmail.com>
2019-11-09 22:08:25 -0500

    Inplace integration now works.  It's only a 2x faster but nice to understand
    * src/ex_freesurfacecont.jl:

--- src/ex_1d.jl
+++ src/ex_1d.jl
@@ -1,9 +1,8 @@
-using Revise
 using DifferentialEquations
+using BenchmarkTools
 using PyCall
 using PyPlot
 
-# Derivatives to feed to ODE integrator (returned values)
 function calcdvθ(vθ, p, t)
     dc, η, σn, a, b, μ, Vp, L, ρ = p
     θ = vθ[1]
@@ -14,25 +13,19 @@ function calcdvθ(vθ, p, t)
     return dvθ
 end
 
-# Doesn't work...yet: Derivatives to feed to ODE integrator (in place)
 function calcdvθ!(dvθ, vθ, p, t)
     dc, η, σn, a, b, μ, Vp, L, ρ = p
     θ = vθ[1]
     v = vθ[2]
-    dθ = -v * θ / dc * log(v * θ / dc)
-    dv = 1 / (η / σn + a / v) * (μ * (Vp - v) / (L * σn) - b * dθ / θ)
-    dvθ = [dθ, dv]
+    dvθ[1] = -v * θ / dc * log(v * θ / dc)
+    dvθ[2] = 1 / (η / σn + a / v) * (μ * (Vp - v) / (L * σn) - b * dvθ[1] / θ)
     return nothing
 end
 
 function ex_1d()
-
-    # Try inplace update following lorenz style at:
-    https://docs.juliadiffeq.org/latest/tutorials/ode_example.html
-    
     # Model parameters
     siay = 365.25 * 24 * 60 * 60
-    tspan = (0.0, siay * 2000.0)
+    tspan = (0.0, siay * 20000.0)
     μ = 3e10
     ν = 0.25
     ρ = 2700.0
@@ -43,40 +36,52 @@ function ex_1d()
     Vp = 1e-9
     σn = 30e6
     dc = 0.2
+    abstol = 1e-10
+    reltol = 1e-10
     
     # Initial conditions
-    ics = [1e8, Vp / 1000]
-    @show ics
+    ics = [1e8; Vp / 1000]
     
     # Time integrate
     p = (dc, η, σn, a, b, μ, Vp, L, ρ)
-    prob = ODEProblem(calcdvθ, ics, tspan, p)
-    @time sol = solve(prob, Rosenbrock23(autodiff = true), abstol = 1e-4, reltol = 1e-4)    
-    t = [x / siay for x in sol.t]
-    θ = [x[1] for x in sol.u]
-    v = [x[2] for x in sol.u]
+    prob1 = ODEProblem(calcdvθ, ics, tspan, p)
+    @time sol1 = solve(prob1, RK4(), abstol = abstol, reltol = reltol)
+    prob2 = ODEProblem(calcdvθ!, ics, tspan, p)
+    @time sol2 = solve(prob2, RK4(), abstol = abstol, reltol = reltol)
     
+    t1 = [x / siay for x in sol1.t]
+    θ1 = [x[1] for x in sol1.u]
+    v1 = [x[2] for x in sol1.u]
+    t2 = [x / siay for x in sol2.t]
+    θ2 = [x[1] for x in sol2.u]
+    v2 = [x[2] for x in sol2.u]
+
     close("all")
     figure(figsize = (12, 6))
     subplot(2, 2, 1)
-    plot(t, θ, "-b", linewidth = 0.5)
+    plot(t1, θ1, "-b", linewidth = 0.5)
+    plot(t2, θ2, "-r", linewidth = 0.5)
     ylabel(L"\theta")
 
     subplot(2, 2, 2)
-    plot(1:1:length(t), θ, "-b", linewidth = 0.5)
+    plot(1:1:length(t1), θ1, "-b", linewidth = 0.5)
+    plot(1:1:length(t2), θ2, "-r", linewidth = 0.5)
     ylabel(L"\theta")
 
     subplot(2, 2, 3)
-    plot(t, v, "-b", linewidth = 0.5)
+    plot(t1, v1, "-b", linewidth = 0.5)
+    plot(t2, v2, "-r", linewidth = 0.5)
     yscale("log")
     xlabel("time (years)")
     ylabel("v (m/s)")
 
     subplot(2, 2, 4)
-    plot(1:1:length(t), v, "-b", linewidth = 0.5)
+    plot(1:1:length(t1), v1, "-b", linewidth = 0.5)
+    plot(1:1:length(t2), v2, "-r", linewidth = 0.5)
     yscale("log")
     xlabel("time (step)")
     ylabel("v (m/s)")
+
     return nothing
 end
 ex_1d()
