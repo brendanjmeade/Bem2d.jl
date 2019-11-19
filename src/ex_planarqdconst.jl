e6956c60c7fa55f9391c6593da404e1fc8f16f77
brendanjmeade <brendanjmeade@gmail.com>
2019-11-18 22:21:19 -0500

    Passing of partials dictionary (~15% slower).  Quadratic partials now calculated but not yet used.  Need to clean up derivatives function.

--- src/ex_planarqdconst.jl
+++ src/ex_planarqdconst.jl
@@ -5,24 +5,24 @@ using Dates
 using Bem2d
 
 function calc_dvθ(vθ, p, t)
-    ∂t, els, η, dc, blockvxglobal, blockvyglobal = p
+    ∂const, els, η, dc, blockvxglobal, blockvyglobal = p
     vxglobal = vθ[1:3:end]
     vyglobal = vθ[2:3:end]
     θ = vθ[3:3:end]
     vx, vy = multmatvec(els.rotmat[1:els.endidx, :, :], vxglobal, vyglobal)
-    dt = ∂t * [blockvxglobal .- vxglobal blockvyglobal .- vyglobal]'[:]
+    dt = ∂const["t"]["fault"]["freesurfflat"] * [blockvxglobal .- vxglobal blockvyglobal .- vyglobal]'[:]
     dtx, dty = multmatvec(els.rotmat[1:els.endidx, :, :], dt[1:2:end], dt[2:2:end])
     dθ, dvx, dvy = zeros(els.endidx), zeros(els.endidx), zeros(els.endidx)
     for i in 1:els.endidx
-        vx = abs.(vx) # Chris R. suggestion
-        θ = abs.(θ) # Chris R. suggestion
+        vx = abs.(vx)
+        θ = abs.(θ)
         dθ[i] = -vx[i] * θ[i] / dc * log(vx[i] * θ[i] / dc) # slip law
         # dθ[i] = 1 - θ[i] * vx[i] / dc # Aging law
         dvx[i] = 1 / (η / els.σn[i] + els.a[i] / vx[i]) * (dtx[i] / els.σn[i] - els.b[i] * dθ[i] / θ[i])
         dvy[i] = 0 # fault perpendicular creep
         # dvperp[i] = dvpara[i] # fault perpendicular velocity proportional to fault parallel velocity
     end
-    dvxglobal, dvyglobal = Bem2d.multmatvec(els.rotmatinv[1:els.endidx, :, :], dvx, dvy)
+    dvxglobal, dvyglobal = multmatvec(els.rotmatinv[1:els.endidx, :, :], dvx, dvy)
     dvθ = zeros(3 * els.endidx)
     dvθ[1:3:end] = dvxglobal
     dvθ[2:3:end] = dvyglobal
@@ -32,7 +32,7 @@ end
 
 function ex_planarqdconst()
     # Constants and model parameters
-    outfilename = string(Dates.now()) * ".jld2"
+    outfilename = string(now()) * ".jld2"
     siay = 365.25 * 24 * 60 * 60
     tspan = (0, siay * 300)
     abstol = 1e-4
@@ -52,21 +52,26 @@ function ex_planarqdconst()
     faultwidth = 10000
     x1, y1, x2, y2 = discretizedline(-faultwidth, 0, faultwidth, 0, nfault)
 
-    els.x1[els.endidx + 1:els.endidx + nfault] = x1
-    els.y1[els.endidx + 1:els.endidx + nfault] = y1
-    els.x2[els.endidx + 1:els.endidx + nfault] = x2
-    els.y2[els.endidx + 1:els.endidx + nfault] = y2
-    els.a[els.endidx + 1:els.endidx + nfault] .= 0.015
-    els.b[els.endidx + 1:els.endidx + nfault] .= 0.020
-    els.σn[els.endidx + 1:els.endidx + nfault] .= 50e6
-    els.name[els.endidx + 1:els.endidx + nfault] .= "fault"
-    Bem2d.standardize_elements!(els)
+    for i in 1:length(x1)
+        els.x1[els.endidx + i] = x1[i]
+        els.y1[els.endidx + i] = y1[i]
+        els.x2[els.endidx + i] = x2[i]
+        els.y2[els.endidx + i] = y2[i]
+        els.a[els.endidx + i] = 0.015
+        els.b[els.endidx + i] = 0.020
+        els.σn[els.endidx + i] = 50e6
+        els.name[els.endidx + i] = "fault"
+    end
+    standardize_elements!(els)
 
+    # Create convience tools
+    idx = getidxdict(els)
+    ∂const = init∂(els)
+    ∂quad = init∂(els)
+    
     # Calculate slip to traction partials on the fault
-    println("Calculating velocity to traction matrix")
-    srcidx = findall(x->x == "fault", els.name)
-    obsidx = srcidx
-    @time ∂u, ∂σ, ∂t = Bem2d.∂constuσ(slip2uσ, els, srcidx, obsidx, μ, ν)
+    @time _, _, ∂const["t"]["fault"]["freesurfflat"] = ∂constuσ(slip2uσ, els, idx["fault"], idx["fault"], μ, ν)
+    @time _, _, ∂quad["t"]["fault"]["freesurfflat"] = ∂quaduσ(slip2uσ, els, idx["fault"], idx["fault"], μ, ν)
 
     # Set initial conditions
     ics = zeros(3 * nnodes)
@@ -75,7 +80,7 @@ function ex_planarqdconst()
     ics[3:3:end] = 1e8 * ones(nnodes)
 
     # (Bulk) Time integrate elastic model
-    p = (∂t, els, η, dc, blockvelx, blockvely)
+    p = (∂const, els, η, dc, blockvelx, blockvely)
     prob = ODEProblem(calc_dvθ, ics, tspan, p)
     @time sol = solve(prob, DP5(), abstol=abstol, reltol=reltol)
     @time @save outfilename sol
