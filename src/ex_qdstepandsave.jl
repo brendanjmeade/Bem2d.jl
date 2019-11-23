d320c263eb9b0dfeadf59a1389c677e18459edbe
brendanjmeade <brendanjmeade@gmail.com>
2019-11-22 22:39:05 -0500

    Starting the elimination of all Greek characters.  Too hard to search with.

--- src/ex_qdstepandsave.jl
+++ src/ex_qdstepandsave.jl
@@ -4,33 +4,33 @@ using JLD2
 using Dates
 using Bem2d
 
-function calc_dvθ(vθ, p, t)
-    ∂t, els, η, dc, blockvxglobal, blockvyglobal = p
-    vxglobal = vθ[1:3:end]
-    vyglobal = vθ[2:3:end]
-    θ = vθ[3:3:end]
+function derivs(u, p, t)
+    partrac, els, eta, dc, blockvxglobal, blockvyglobal = p
+    vxglobal = u[1:3:end]
+    vyglobal = u[2:3:end]
+    theta = u[3:3:end]
     vx, vy = multmatvec(els.rotmat[1:els.endidx, :, :], vxglobal, vyglobal)
-    dt = ∂t * [blockvxglobal .- vxglobal blockvyglobal .- vyglobal]'[:]
+    dt = partrac * [blockvxglobal .- vxglobal blockvyglobal .- vyglobal]'[:]
     dtx, dty = multmatvec(els.rotmat[1:els.endidx, :, :], dt[1:2:end], dt[2:2:end])
-    dθ, dvx, dvy = zeros(els.endidx), zeros(els.endidx), zeros(els.endidx)
+    dtheta, dvx, dvy = zeros(els.endidx), zeros(els.endidx), zeros(els.endidx)
+    vx = @. abs(vx)
+    theta = @. abs(theta)
+
     for i in 1:els.endidx
-        vx = abs.(vx)
-        θ = abs.(θ)
-        dθ[i] = -vx[i] * θ[i] / dc * log(vx[i] * θ[i] / dc) # slip law
-        # dθ[i] = 1 - θ[i] * vx[i] / dc # Aging law
-        dvx[i] = 1 / (η / els.σn[i] + els.a[i] / vx[i]) * (dtx[i] / els.σn[i] - els.b[i] * dθ[i] / θ[i])
+        dtheta[i] = 1 - theta[i] * vx[i] / dc # Aging law
+        dvx[i] = 1 / (eta / els.σn[i] + els.a[i] / vx[i]) * (dtx[i] / els.σn[i] - els.b[i] * dtheta[i] / theta[i])
         dvy[i] = 0 # fault perpendicular creep
-        # dvperp[i] = dvpara[i] # fault perpendicular velocity proportional to fault parallel velocity
     end
+
     dvxglobal, dvyglobal = multmatvec(els.rotmatinv[1:els.endidx, :, :], dvx, dvy)
-    dvθ = zeros(3 * els.endidx)
-    dvθ[1:3:end] = dvxglobal
-    dvθ[2:3:end] = dvyglobal
-    dvθ[3:3:end] = dθ
-    return dvθ
+    dudt = zeros(3 * els.endidx)
+    dudt[1:3:end] = dvxglobal
+    dudt[2:3:end] = dvyglobal
+    dudt[3:3:end] = dtheta
+    return dudt
 end
 
-function ex_qdmakie()
+function ex_qdstepandsave()
     # Fun things to play with
     nsteps = 1000
     amplitude = 1.0
@@ -42,10 +42,10 @@ function ex_qdmakie()
     tspan = (0, siay * 1000)
     abstol = 1e-4
     reltol = 1e-4
-    μ = 3e10
-    ν = 0.25
-    ρ = 2700.0
-    η = μ / (2.0 * sqrt(μ / ρ))
+    mu = 3e10
+    nu = 0.25
+    rho = 2700.0
+    eta = mu / (2.0 * sqrt(mu / rho))
     dc = 0.05
     blockvelx = 1e-9
     blockvely = 0.0
@@ -62,7 +62,7 @@ function ex_qdmakie()
         els.y2[els.endidx + i] = y2[i]
         els.a[els.endidx + i] = 0.015
         els.b[els.endidx + i] = 0.020
-        els.σn[els.endidx + i] = 50e6
+        els.σn[els.endidx + i] = 50e6 # sigman
         els.name[els.endidx + i] = "fault"
     end
     standardize_elements!(els)
@@ -71,16 +71,17 @@ function ex_qdmakie()
     println("Calculating velocity to traction matrix")
     srcidx = findall(x->x == "fault", els.name)
     obsidx = srcidx
-    @time ∂u, ∂σ, ∂t = ∂constuσ(slip2uσ, els, srcidx, obsidx, μ, ν)
-
+    @time _, _, partrac = ∂constuσ(slip2uσ, els, srcidx, obsidx, mu, nu)
+    # @time _, _, partrac = parconst_dispstress(slip2dispstress, els, srcidx, obsidx, mu, nu)
+    
     # Set initial conditions
     ics = zeros(3 * nnodes)
     ics[1:3:end] = 1e-3 * blockvelx * ones(nnodes)
     ics[2:3:end] = 0.0 * blockvely * ones(nnodes)
     ics[3:3:end] = 1e8 * ones(nnodes)
-    p = (∂t, els, η, dc, blockvelx, blockvely)
-    prob = ODEProblem(calc_dvθ, ics, tspan, p)
-    integrator = init(prob, Vern7(), abstol = abstol, reltol = reltol, progress = true)
+    p = (partrac, els, eta, dc, blockvelx, blockvely)
+    prob = ODEProblem(derivs, ics, tspan, p)
+    integrator = init(prob, Vern7(), abstol = abstol, reltol = reltol)
 
     println("Step integrating")
     @time for i in 1:nsteps
@@ -88,9 +89,9 @@ function ex_qdmakie()
         println("step: " * string(i) * " of " * string(nsteps) * ", time: " * string(integrator.sol.t[end] / siay))
     end
 
-    @time @save outfilename integrator.sol
-    println("Wrote integration results to:")
-    println(outfilename)
+    # @time @save outfilename integrator.sol
+    # println("Wrote integration results to:")
+    # println(outfilename)
 
 end
-ex_qdmakie()
+ex_qdstepandsave()
