d3508e3f2b2102f11faffba555e0b282aded9084
brendanjmeade <brendanjmeade@gmail.com>
2019-11-28 22:42:15 -0500

    Removed all Greek letters and introduced modern data structures

--- src/ex_freesurface.jl
+++ src/ex_freesurface.jl
@@ -3,74 +3,58 @@ using PyCall
 using PyPlot
 using Bem2d
 
-# From Segall equations 3.73 on page 67
-function analyticthrust()
-    x = collect(LinRange(-5, 5, 1000))
-    δ = deg2rad(135.0)
-    d = 1.0
-    ξ = d / tan(δ)
-    ζ = @. (x - ξ) / d
-    s = 1.0
-    ux = @. -s / π * (cos(δ) * (atan(ζ) - π / 2 * sign(x)) + (sin(δ) - ζ * cos(δ)) / (1 + ζ^2)^2)
-    uy =  @. s / π * (sin(δ) * (atan(ζ) - π / 2 * sign(x)) + (cos(δ) + ζ * sin(δ)) / (1 + ζ^2)^2)
-    return x, ux, uy
-end
-
 function ex_freesurface()
-    μ = 30e9
-    ν = 0.25
-    println(rand(1))
+    mu = 30e9
+    nu = 0.25
 
     # Free surface
     els = Elements(Int(1e5))
     nfreesurf = 200
     x1, y1, x2, y2 = discretizedline(-50, 0, 50, 0, nfreesurf)
-    els.x1[els.endidx + 1:els.endidx + nfreesurf] = x1
-    els.y1[els.endidx + 1:els.endidx + nfreesurf] = y1
-    els.x2[els.endidx + 1:els.endidx + nfreesurf] = x2
-    els.y2[els.endidx + 1:els.endidx + nfreesurf] = y2
-    els.name[els.endidx + 1:els.endidx + nfreesurf] .= "freesurf"
+    for i in 1:length(x1)
+        els.x1[els.endidx + i] = x1[i]
+        els.y1[els.endidx + i] = y1[i]
+        els.x2[els.endidx + i] = x2[i]
+        els.y2[els.endidx + i] = y2[i]
+        els.name[els.endidx + i] = "freesurf"
+    end
     standardize_elements!(els)
 
     # 45 degree dipping fault
     nfault = 1
     x1, y1, x2, y2 = discretizedline(-1, -1, 0, 0, nfault)
-    els.x1[els.endidx + 1:els.endidx + nfault] = x1
-    els.y1[els.endidx + 1:els.endidx + nfault] = y1
-    els.x2[els.endidx + 1:els.endidx + nfault] = x2
-    els.y2[els.endidx + 1:els.endidx + nfault] = y2
-    els.name[els.endidx + 1:els.endidx + nfault] .= "fault"
+    for i in 1:length(x1)
+        els.x1[els.endidx + i] = x1[i]
+        els.y1[els.endidx + i] = y1[i]
+        els.x2[els.endidx + i] = x2[i]
+        els.y2[els.endidx + i] = y2[i]
+        els.name[els.endidx + i] = "fault"
+    end
     standardize_elements!(els)
 
+    # Conviencience structures
+    idx = getidxdict(els)
+    partialsconst = initpartials(els)
+    partialsquad = initpartials(els)
+
     # Constant slip fault
-    srcidx = findall(x->x == "fault", els.name)
-    obsidx = findall(x->x == "freesurf", els.name)
-    ∂u1const, ∂σ1const, ∂t1const = ∂constuσ(slip2uσ, els, srcidx, obsidx, μ, ν)
-    srcidx = findall(x->x == "freesurf", els.name)
-    obsidx = findall(x->x == "freesurf", els.name)
-    ∂u2const, ∂σ2const, ∂t2const = ∂constuσ(slip2uσ, els, srcidx, obsidx, μ, ν)
+    partialsconst["disp"]["fault"]["freesurf"], partialsconst["stress"]["fault"]["freesurf"], partialsconst["trac"]["fault"]["freesurf"] = partialsconstdispstress(slip2dispstress, els, idx["fault"], idx["freesurf"], mu, nu)
+    partialsconst["disp"]["freesurf"]["freesurf"], partialsconst["stress"]["freesurf"]["freesurf"], partialsconst["trac"]["freesurf"]["freesurf"] = partialsconstdispstress(slip2dispstress, els, idx["freesurf"], idx["freesurf"], mu, nu)
     faultslipconst = sqrt(2) / 2 * [1 ; 1]
-    ufullspaceconst = ∂u1const * faultslipconst
-    ufreesurfaceconst = inv(∂t2const) * (∂t1const * faultslipconst)
-    xplotconst = els.xcenter[findall(x->x == "freesurf", els.name)]
+    ufullspaceconst = partialsconst["disp"]["fault"]["freesurf"] * faultslipconst
+    ufreesurfaceconst = inv(partialsconst["trac"]["freesurf"]["freesurf"]) * (partialsconst["trac"]["fault"]["freesurf"] * faultslipconst)
+    xplotconst = els.xcenter[idx["freesurf"]]
 
     # Quadratic slip fault
-    srcidx = findall(x->x == "fault", els.name)
-    obsidx = findall(x->x == "freesurf", els.name)
-    ∂u1quad, ∂σ1quad, ∂t1quad = ∂quaduσ(slip2uσ, els, srcidx, obsidx, μ, ν)
-    srcidx = findall(x->x == "freesurf", els.name)
-    obsidx = findall(x->x == "freesurf", els.name)
-    ∂u2quad, ∂σ2quad, ∂t2quad = ∂quaduσ(slip2uσ, els, srcidx, obsidx, μ, ν)
-    xplotquad = sort(els.xnodes[obsidx, :][:])
+    partialsquad["disp"]["fault"]["freesurf"], partialsquad["stress"]["fault"]["freesurf"], partialsquad["trac"]["fault"]["freesurf"] = partialsquaddispstress(slip2dispstress, els, idx["fault"], idx["freesurf"], mu, nu)
+    partialsquad["disp"]["freesurf"]["freesurf"], partialsquad["stress"]["freesurf"]["freesurf"], partialsquad["trac"]["freesurf"]["freesurf"] = partialsquaddispstress(slip2dispstress, els, idx["freesurf"], idx["freesurf"], mu, nu)
+    xplotquad = sort(els.xnodes[idx["freesurf"], :][:])
     faultslipquad = sqrt(2) ./ 2 * ones(6)
-    ufullspacequad = ∂u1quad * faultslipquad
-    ufreesurfacequad = inv(∂t2quad) * (∂t1quad * faultslipquad)
-
-    # Analytic solution
-    xanalytic, uxanalytic, uyanalytic = analyticthrust()
+    ufullspacequad = partialsquad["disp"]["fault"]["freesurf"] * faultslipquad
+    ufreesurfacequad = inv(partialsquad["trac"]["freesurf"]["freesurf"]) * (partialsquad["trac"]["fault"]["freesurf"] * faultslipquad)
 
     # Okada solution
-    ow = pyimport("okada_wrapper")# from okada_wrapper import dc3dwrapper
+    ow = pyimport("okada_wrapper") # from okada_wrapper import dc3dwrapper
     xokada = collect(LinRange(-5, 5, 1000))
     uxokada = zeros(length(xokada))
     uyokada = zeros(length(xokada))
@@ -97,7 +81,6 @@ function ex_freesurface()
     figure(figsize = (3, 4))
 
     ax = subplot(2, 1, 1)
-    # plot(xanalytic, uxanalytic, "-k", linewidth=linewidth, label="analytic")
     plot(xokada, uxokada, "-k", linewidth=linewidth, label="Okada")
 
     # plot(xplotconst, ufullspaceconst[1:2:end], "bo", markeredgewidth=linewidth, markersize=markersize, label = "const fullspace")
