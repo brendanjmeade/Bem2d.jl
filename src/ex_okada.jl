c90505ffc38323eb56fab68f173f13ae2e5b2cf6
brendanjmeade <brendanjmeade@gmail.com>
2020-02-21 00:16:43 -0500

    Making progress on tensile- and strike-slip comparisions

--- src/ex_okada.jl
+++ src/ex_okada.jl
@@ -26,26 +26,24 @@ function ex_okada()
     x, y = Bem2d.obsgrid(-obswidth, -obswidth, obswidth, obswidth, npts)
 
     # Constant slip fault
-    strikeslip = 1.0 * ones(els.endidx)
-    tensileslip = 0.0 * ones(els.endidx)
-    UfU, σfU = Bem2d.constdispstress(slip2dispstress, x, y, els, idx["fault"], strikeslip, tensileslip, mu, nu)
+    UfUss, σfUss = Bem2d.constdispstress(slip2dispstress, x, y, els, idx["fault"], 1, 0, mu, nu)
+    UfUts, σfUts = Bem2d.constdispstress(slip2dispstress, x, y, els, idx["fault"], 0, 1, mu, nu)
 
     # Okada solution
     ow = pyimport("okada_wrapper") # from okada_wrapper import dc3dwrapper
-    uokada = zeros(length(x), 2)
-    σokada = zeros(length(x), 3)
-
+    uokadass = zeros(length(x), 2)
+    σokadass = zeros(length(x), 3)
     for i in 1:length(x)
         deep = 1000.0
-        _, u, ∇u = ow.dc3dwrapper(0.6, [0.0, x[i], y[i]-deep], 0.0 + deep,
-                                  0.0, [-0.7, 0.7], [-0.7, 0.7],
-                                  [0.0, 0.0, 1.0])
-        uokada[i, 1] = u[2]
-        uokada[i, 2] = u[3]
+        _, u, ∇u = ow.dc3dwrapper(0.6, [0.0, x[i], y[i]-deep], 0.0 + deep, 0.0, [-0.7, 0.7], [-0.7, 0.7], [0.0, 1.0, 0.0])
+        uokadass[i, 1] = u[2]
+        uokadass[i, 2] = u[3]
     end
+
     PyPlot.close("all")
-    Bem2d.plotfields(els, reshape(x, npts, npts), reshape(y, npts, npts),
-               uokada, σokada, "Okada strike-slip")
-    PyPlot.show()
+    Bem2d.plotfields(els, reshape(x, npts, npts), reshape(y, npts, npts), uokadass, σokadass, "Okada (strike-slip)")
+    Bem2d.plotfields(els, reshape(x, npts, npts), reshape(y, npts, npts), UfUss, σfUss, "BEM (strike-slip)")
+
+
 end
 ex_okada()
