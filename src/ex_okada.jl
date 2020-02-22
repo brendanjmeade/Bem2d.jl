9e101d652ae64e8dd67ae7b83a09784da154be20
brendanjmeade <brendanjmeade@gmail.com>
2020-02-22 00:05:03 -0500

    OK.  Lots here.  1) Tensile slip x-displacements are almost definitely wrong (because they don't agree with Okada).  Almost all stress components seem wrong as well.  Start with displacements.  Need to clean up stress plotting too.

--- src/ex_okada.jl
+++ src/ex_okada.jl
@@ -1,7 +1,58 @@
 using Revise
 using PyCall
 using PyPlot
+using LinearAlgebra
 using Bem2d
+
+function plotfields_contours_local(els, xobs, yobs, idx, field, title)
+    ncontours = 40
+    xlim = [minimum(xobs) maximum(xobs)]
+    ylim = [minimum(yobs) maximum(yobs)]
+    subplot(2, 3, idx)
+    scale = 1.0
+    fieldmax = maximum(@.abs(field))
+    contourf(xobs, yobs, reshape(field, size(xobs)), ncontours,
+        vmin = -scale * fieldmax, vmax = scale * fieldmax, cmap = rycroftcmap())
+    clim(-scale * fieldmax, scale * fieldmax)
+    colorbar(fraction = 0.020, pad = 0.05, extend = "both")
+    contour(xobs, yobs, reshape(field, size(xobs)), ncontours,
+        vmin = -scale * fieldmax, vmax = scale * fieldmax, linewidths = 0.25, colors = "k")
+    PyPlot.title(title)
+    # stylesubplots(xlim, ylim)
+    plotelements(els)
+    return nothing
+end
+
+function plotfields_local(els, xobs, yobs, disp, stress, suptitle)
+    figure(figsize = (25, 15))
+    subplot(2, 3, 1)
+    quiver(xobs[:], yobs[:], disp[:, 1], disp[:, 2], units = "width", color = "b")
+    # stylesubplots([minimum(xobs) maximum(xobs)], [minimum(yobs) maximum(yobs)])
+    Bem2d.plotelements(els)
+    PyPlot.title("displacements")
+
+    contourvec = collect(LinRange(-0.5, 0.5, 50))
+    subplot(2, 3, 2)
+    contourf(xobs, yobs, reshape(disp[:, 1], size(xobs)), contourvec, cmap = rycroftcmap())
+    colorbar(fraction = 0.020, pad = 0.05, extend = "both")
+    contour(xobs, yobs, reshape(disp[:, 1], size(xobs)), contourvec, linewidths = 0.25, colors = "k")
+    PyPlot.title(L"u_x")
+    plotelements(els)
+
+    subplot(2, 3, 3)
+    contourf(xobs, yobs, reshape(disp[:, 2], size(xobs)), contourvec, cmap = rycroftcmap())
+    colorbar(fraction = 0.020, pad = 0.05, extend = "both")
+    contour(xobs, yobs, reshape(disp[:, 2], size(xobs)), contourvec, linewidths = 0.25, colors = "k")
+    PyPlot.title(L"u_y")
+    plotelements(els)
+
+    plotfields_contours_local(els, xobs, yobs, 4, stress[:, 1], L"\sigma_{xx}")
+    plotfields_contours_local(els, xobs, yobs, 5, stress[:, 2], L"\sigma_{yy}")
+    plotfields_contours_local(els, xobs, yobs, 6, stress[:, 3], L"\sigma_{xy}")
+    PyPlot.suptitle(suptitle, fontsize=20)
+    show()
+    return nothing
+end
 
 function ex_okada()
     mu = 30e9
@@ -9,7 +60,7 @@ function ex_okada()
 
     # Flat fault
     nfault = 1
-    x1, y1, x2, y2 = Bem2d.discretizedline(-1, 0, 1, 0, nfault)
+    x1, y1, x2, y2 = Bem2d.discretizedline(-0.5, 0, 0.5, 0, nfault)
     els = Bem2d.Elements(Int(1e5))
     for i in 1:length(x1)
         els.x1[els.endidx + i] = x1[i]
@@ -21,7 +72,7 @@ function ex_okada()
     Bem2d.standardize_elements!(els)
     idx = Bem2d.getidxdict(els)
 
-    obswidth = 10
+    obswidth = 3
     npts = 99
     x, y = Bem2d.obsgrid(-obswidth, -obswidth, obswidth, obswidth, npts)
 
@@ -33,17 +84,39 @@ function ex_okada()
     ow = pyimport("okada_wrapper") # from okada_wrapper import dc3dwrapper
     uokadass = zeros(length(x), 2)
     σokadass = zeros(length(x), 3)
+    uokadats = zeros(length(x), 2)
+    σokadats = zeros(length(x), 3)
+    deep = 10000.0
     for i in 1:length(x)
-        deep = 1000.0
-        _, u, ∇u = ow.dc3dwrapper(0.6, [0.0, x[i], y[i]-deep], 0.0 + deep, 0.0, [-1000, 1000], [-0.5, 0.5], [0.0, 1.0, 0.0])
+        _, u, ∇u = ow.dc3dwrapper(0.6, [0.0, x[i], y[i]-deep], 0.0 + deep, 0.0, [-100000, 100000], [-0.5, 0.5], [0.0, 1.0, 0.0])
+        ∇u = [∇u[2,2] ∇u[2,3] ; ∇u[3,2] ∇u[3,3]]
+        ϵ = @. 0.5 * (∇u + transpose(∇u))
+        σ = mu*I(2)*tr(ϵ) + 2*mu*ϵ
         uokadass[i, 1] = u[2]
         uokadass[i, 2] = u[3]
+        σokadass[i, 1] = σ[1, 1]
+        σokadass[i, 2] = σ[2, 2]
+        σokadass[i, 3] = σ[1, 2]
+
+        _, u, ∇u = ow.dc3dwrapper(0.6, [0.0, x[i], y[i]-deep], 0.0 + deep, 0.0, [-100000, 100000], [-0.5, 0.5], [0.0, 0.0, 1.0])
+        ∇u = [∇u[2,2] ∇u[2,3] ; ∇u[3,2] ∇u[3,3]]
+        ϵ = @. 0.5 * (∇u + transpose(∇u))
+        σ = mu*I(2)*tr(ϵ) + 2*mu*ϵ
+        uokadats[i, 1] = u[2]
+        uokadats[i, 2] = u[3]
+        σokadats[i, 1] = σ[1, 1]
+        σokadats[i, 2] = σ[2, 2]
+        σokadats[i, 3] = σ[1, 2]
     end
 
     PyPlot.close("all")
-    Bem2d.plotfields(els, reshape(x, npts, npts), reshape(y, npts, npts), uokadass, σokadass, "Okada (strike-slip)")
-    Bem2d.plotfields(els, reshape(x, npts, npts), reshape(y, npts, npts), UfUss, σfUss, "BEM (strike-slip)")
+    plotfields_local(els, reshape(x, npts, npts), reshape(y, npts, npts), uokadass, σokadass, "Okada (strike-slip)")
+    plotfields_local(els, reshape(x, npts, npts), reshape(y, npts, npts), UfUss, σfUss, "BEM (strike-slip)")
+    plotfields_local(els, reshape(x, npts, npts), reshape(y, npts, npts), uokadass - UfUss, σokadass - σfUss, "Residuals (strike-slip)")
 
+    plotfields_local(els, reshape(x, npts, npts), reshape(y, npts, npts), uokadats, σokadats, "Okada (tensile-slip)")
+    plotfields_local(els, reshape(x, npts, npts), reshape(y, npts, npts), UfUts, σfUts, "BEM (tensile-slip)")
+    plotfields_local(els, reshape(x, npts, npts), reshape(y, npts, npts), uokadats - UfUts, σokadats - σfUts, "Residuals (tensile-slip)")
 
 end
 ex_okada()
