f88f3504722b598daa90c7aec8de8b2497cafb2c
brendanjmeade <brendanjmeade@gmail.com>
2020-03-03 23:18:00 -0500

    Experiments with different spacing and free surface endpoints.  No real change.

--- src/ex_flamant.jl
+++ src/ex_flamant.jl
@@ -24,14 +24,12 @@ function local_subplot(x, y, mat, npts, title_string)
     PyPlot.gca().tick_params(labelsize=fontsize)
 end
 
-
 function stylesubplots_local()
     gca().set_aspect("equal")
     PyPlot.xlim([-1000, 1000])
     PyPlot.ylim([-1000, 1000])
-
-    # gca().set_xticks([])
-    # gca().set_yticks([])
+    gca().set_xticks([])
+    gca().set_yticks([])
     return nothing
 end
 
@@ -186,6 +184,7 @@ function plot18_local(els, x, y, disp1, stress1, string1, disp2, stress2, string
     PyPlot.title("sxy (" * string1 * "-" * string2 * ")", fontsize=fontsize)
 
     PyPlot.suptitle(title_string, fontsize=fontsize)
+    PyPlot.tight_layout()
     PyPlot.show()
 end
 
@@ -194,15 +193,17 @@ function ex_flamant()
     obswidth = 1000
 
     #! Try wierd symmetric quadratic spacing with small elements i the middle
-    nquad = 100
+    nquad = 200
     x1 = zeros(3 + 2 * (nquad-1))
     x2 = zeros(3 + 2 * (nquad-1))
     quadspacing = collect(LinRange(1, sqrt(3*obswidth), nquad).^2)
     x1[1:1:nquad-1] = -reverse(quadspacing[2:1:end], dims=1)
     x1[nquad:1:nquad+2] = [-1.0 -0.5 0.5]
+    # x1[nquad:1:nquad+2] = [-1.0 -0.1 0.1] # smaller central element
     x1[nquad+3:1:end] = quadspacing[1:1:end-1]
     x2[1:1:nquad-1] = -reverse(quadspacing[1:1:end-1], dims=1)
     x2[nquad:1:nquad+2] = [-0.5 0.5 1.0]
+    # x2[nquad:1:nquad+2] = [-0.1 0.1 1.0] # smaller central element
     x2[nquad+3:1:end] = quadspacing[2:1:end]
     y1 = zeros(size(x1))
     y2 = zeros(size(x1))
@@ -223,7 +224,6 @@ function ex_flamant()
     σrr = @. -2.0/(pi*r) * (fx[mididx]*cosd(θ) + fy[mididx]*sind(θ))
     σθθ = zeros(length(x))
     σrθ = zeros(length(x))
-
     σxx_flamant = zeros(length(x))
     σyy_flamant = zeros(length(x))
     σxy_flamant = zeros(length(x))
@@ -261,7 +261,9 @@ function ex_flamant()
     # Given the other tractions calcuate the induced displacements on the boudaries
     T, _, _ = Bem2d.partialsconstdispstress(slip2dispstress, els, idx["line"], idx["line"], mu, nu)
     U, _, _ = Bem2d.partialsconstdispstress(trac2dispstress, els, idx["line"], idx["line"], mu, nu)
+    # dispall = (inv(T + 0.5 * LinearAlgebra.I(size(T)[1]))) * U * Bem2d.interleave(fx, fy)
     dispall = (inv(T + 0.5 * LinearAlgebra.I(size(T)[1]))) * U * Bem2d.interleave(fx, fy)
+
 
     @show cond(T + 0.5 * LinearAlgebra.I(size(T)[1]))
     # U[diagind(U)] .= 0
@@ -278,26 +280,29 @@ function ex_flamant()
     kappaplanestress = (3-nu)/(1+nu)
     
     #! Set everything in the upper half-plane to zero because that is the exterior solution
-    # to_nan_idx = findall(x -> x > 0.0, y)
-    # σrr[to_nan_idx] .= NaN
-    # σθθ[to_nan_idx] .= NaN
-    # σrθ[to_nan_idx] .= NaN
-    # σxx_flamant[to_nan_idx] .= NaN
-    # σyy_flamant[to_nan_idx] .= NaN
-    # σxy_flamant[to_nan_idx] .= NaN
-    # stresstrac[to_nan_idx, 1] .= NaN
-    # stresstrac[to_nan_idx, 2] .= NaN
-    # stresstrac[to_nan_idx, 3] .= NaN
-    # stressdisp[to_nan_idx, 1] .= NaN
-    # stressdisp[to_nan_idx, 2] .= NaN
-    # stressdisp[to_nan_idx, 3] .= NaN
+    to_nan_idx = findall(x -> x > 0.0, y)
+    σrr[to_nan_idx] .= NaN
+    σθθ[to_nan_idx] .= NaN
+    σrθ[to_nan_idx] .= NaN
+    σxx_flamant[to_nan_idx] .= NaN
+    σyy_flamant[to_nan_idx] .= NaN
+    σxy_flamant[to_nan_idx] .= NaN
+    stresstrac[to_nan_idx, 1] .= NaN
+    stresstrac[to_nan_idx, 2] .= NaN
+    stresstrac[to_nan_idx, 3] .= NaN
+    stressdisp[to_nan_idx, 1] .= NaN
+    stressdisp[to_nan_idx, 2] .= NaN
+    stressdisp[to_nan_idx, 3] .= NaN
 
     #! 18 panel plot
     PyPlot.close("all")
     dispbem = zeros(length(x), 2)
-    stressbem = @. stresstrac + stressdisp
+    stressbem = stresstrac .- stressdisp
     dispanalytic = zeros(length(x), 2)
     stressanalytic = [σxx_flamant σyy_flamant σxy_flamant] 
+    @show size(stressbem)
+    @show size(stressanalytic)
+
     xobs = reshape(x, npts, npts)
     yobs = reshape(y, npts, npts)
     plot18_local(els, xobs, yobs, dispbem, stressbem, "BEM", dispanalytic, stressanalytic, "analytic", "Flamant (BEM vs. analytic)")
