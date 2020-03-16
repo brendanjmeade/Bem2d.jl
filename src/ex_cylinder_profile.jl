003584fe2bb78aa80b3fb96ee711c295fb46e96a
brendanjmeade <brendanjmeade@gmail.com>
2020-03-16 14:51:59 -0400

    Experiments with arclen.  Same systematic diferences

--- src/ex_cylinder_profile.jl
+++ src/ex_cylinder_profile.jl
@@ -92,7 +92,7 @@ function ex_cylinder_profile()
 
     # Solution from Hondros (1959) as summarized by Wei and Chau 2013
     p = -1.0e5 # Applied radial pressure over arc
-    arclen = deg2rad(1.0) # Arc length over which pressure is applied
+    arclen = deg2rad(45.0) # Arc length over which pressure is applied
     R = 57.296 # Radius of disc
     y = @. R * collect(0.0:0.1:1.0)
     x = zeros(size(y))
