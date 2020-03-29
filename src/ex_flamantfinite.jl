92a00265fdabf483d91db0a0a19910953ad23e80
brendanjmeade <brendanjmeade@gmail.com>
2020-03-28 22:46:40 -0400

    Variable renaming.  disp -> D, stress -> S

--- src/ex_flamantfinite.jl
+++ src/ex_flamantfinite.jl
@@ -48,15 +48,15 @@ function ex_flamantfinite()
     fyscaled = @. fy / (2*a) #! Strange length dependent normalization
 
     #! Analytic finite Flamant solution
-    dispanalytic = zeros(length(x), 2)
-    stressanalytic = zeros(length(x), 3)
+    Danalytic = zeros(length(x), 2)
+    Sanalytic = zeros(length(x), 3)
     r1 = @. (x-a)^2 + y^2
     r2 = @. (x+a)^2 + y^2
     theta1 = @. atan(y, x-a)
     theta2 = @. atan(y, x+a)
-    stressanalytic[:, 1] = @. -P/pi * (theta1-theta2 + y*(x-a)*r1^-2 - y*(x+a)*r2^-2)
-    stressanalytic[:, 2] = @. -P/pi * (theta1-theta2 - y*(x-a)*r1^-2 + y*(x+a)*r2^-2)
-    stressanalytic[:, 3] = @. -P/pi * y^2 * (r1^-2 - r2^-2)
+    Sanalytic[:, 1] = @. -P/pi * (theta1-theta2 + y*(x-a)*r1^-2 - y*(x+a)*r2^-2)
+    Sanalytic[:, 2] = @. -P/pi * (theta1-theta2 - y*(x-a)*r1^-2 + y*(x+a)*r2^-2)
+    Sanalytic[:, 3] = @. -P/pi * y^2 * (r1^-2 - r2^-2)
 
     #! BEM solution
     els = Elements(Int(1e5))
@@ -70,34 +70,35 @@ function ex_flamantfinite()
     standardize_elements!(els)
     idx = getidxdict(els)
 
-    #! Direct BEM
+    #! Kernels
     Tstar, Sstar, Hstar = partialsconstdispstress(slip2dispstress, els, idx["line"], idx["line"], mu, nu)
     Ustar, Dstar, Astar = partialsconstdispstress(trac2dispstress, els, idx["line"], idx["line"], mu, nu)
-      
+    
+    #! Direct BEM
     dispall = (inv(Tstar + 0.5 * I(size(Tstar)[1]))) * Ustar * interleave(fxscaled, fyscaled)
 
     #! Try Indirect DDM...because it worked for the Brazil test
-    dispall = -Ustar * interleave(fx, fy)
+    # dispall = -Ustar * interleave(fx, fy)
 
     #! Streses from tractions and induced displacements
-    disptrac, stresstrac = constdispstress(trac2dispstress, x, y, els, idx["line"], fxscaled, fyscaled, mu, nu)
-    dispdisp, stressdisp = constdispstress(slip2dispstress, x, y, els, idx["line"], dispall[1:2:end], dispall[2:2:end], mu, nu)
+    Dtrac, Strac = constdispstress(trac2dispstress, x, y, els, idx["line"], fxscaled, fyscaled, mu, nu)
+    Ddisp, Sdisp = constdispstress(slip2dispstress, x, y, els, idx["line"], dispall[1:2:end], dispall[2:2:end], mu, nu)
 
     #! Set everything in the upper half-plane to zero because that is the exterior solution
     to_nan_idx = findall(x -> x > 0.0, y)
-    dispanalytic[to_nan_idx, :] .= NaN
-    stressanalytic[to_nan_idx, :] .= NaN
-    disptrac[to_nan_idx, :] .= NaN
-    dispdisp[to_nan_idx, :] .= NaN
-    stresstrac[to_nan_idx, :] .= NaN
-    stressdisp[to_nan_idx, :] .= NaN
+    Danalytic[to_nan_idx, :] .= NaN
+    Sanalytic[to_nan_idx, :] .= NaN
+    Dtrac[to_nan_idx, :] .= NaN
+    Ddisp[to_nan_idx, :] .= NaN
+    Strac[to_nan_idx, :] .= NaN
+    Sdisp[to_nan_idx, :] .= NaN
 
     #! Multi-panel plot
     close("all")
-    # dispbem = disptrac .- dispdisp # Direct BEM
-    # stressbem = stresstrac .- stressdisp # Direct BEM
-    dispbem = dispdisp # DDM
-    stressbem = stressdisp # DDM
+    # Dbem = @. Dtrac - Ddisp # Direct BEM
+    # Sbem = @. Strac - Sdisp # Direct BEM
+    Dbem = Ddisp # DDM
+    Sbem = Sdisp # DDM
     xplot = reshape(x, npts, npts)
     yplot = reshape(y, npts, npts)
     contoursdisp = 51
@@ -110,39 +111,39 @@ function ex_flamantfinite()
 
     #! Analytic solution
     subplot(nrows, ncols, 1)
-    quiver(x[:], y[:], dispanalytic[:, 1], dispanalytic[:, 2], units="width", color="b")
+    quiver(x[:], y[:], Danalytic[:, 1], Danalytic[:, 2], units="width", color="b")
     stylesubplots_local()
     plotelements(els)
     title("u", fontsize=fontsize)
-    subplotlocal(nrows, ncols, 2, els, xplot, yplot, dispanalytic[:, 1], contoursdisp, "ux (analytic)")
-    subplotlocal(nrows, ncols, 3, els, xplot, yplot, dispanalytic[:, 2], contoursdisp, "uy (analytic)")
-    subplotlocal(nrows, ncols, 4, els, xplot, yplot, stressanalytic[:, 1], contoursdisp, "sxx (analytic)")
-    subplotlocal(nrows, ncols, 5, els, xplot, yplot, stressanalytic[:, 2], contoursdisp, "syy (analytic)")
-    subplotlocal(nrows, ncols, 6, els, xplot, yplot, stressanalytic[:, 3], contoursdisp, "sxy (analytic)")
+    subplotlocal(nrows, ncols, 2, els, xplot, yplot, Danalytic[:, 1], contoursdisp, "ux (analytic)")
+    subplotlocal(nrows, ncols, 3, els, xplot, yplot, Danalytic[:, 2], contoursdisp, "uy (analytic)")
+    subplotlocal(nrows, ncols, 4, els, xplot, yplot, Sanalytic[:, 1], contoursdisp, "sxx (analytic)")
+    subplotlocal(nrows, ncols, 5, els, xplot, yplot, Sanalytic[:, 2], contoursdisp, "syy (analytic)")
+    subplotlocal(nrows, ncols, 6, els, xplot, yplot, Sanalytic[:, 3], contoursdisp, "sxy (analytic)")
 
     # #! Fields from second model
     subplot(nrows, ncols, 7)
-    quiver(x[:], y[:], dispbem[:, 1], dispbem[:, 2], units="width", color="b")
+    quiver(x[:], y[:], Dbem[:, 1], Dbem[:, 2], units="width", color="b")
     stylesubplots_local()
     plotelements(els)
     title("u", fontsize=fontsize)
-    subplotlocal(nrows, ncols, 8, els, xplot, yplot, dispbem[:, 1], contoursdisp, "ux (BEM)")
-    subplotlocal(nrows, ncols, 9, els, xplot, yplot, dispbem[:, 2], contoursdisp, "uy (BEM)")
-    subplotlocal(nrows, ncols, 10, els, xplot, yplot, stressbem[:, 1], contoursdisp, "sxx (BEM)")
-    subplotlocal(nrows, ncols, 11, els, xplot, yplot, stressbem[:, 2], contoursdisp, "syy (BEM)")
-    subplotlocal(nrows, ncols, 12, els, xplot, yplot, stressbem[:, 3], contoursdisp, "sxy (BEM)")
+    subplotlocal(nrows, ncols, 8, els, xplot, yplot, Dbem[:, 1], contoursdisp, "ux (BEM)")
+    subplotlocal(nrows, ncols, 9, els, xplot, yplot, Dbem[:, 2], contoursdisp, "uy (BEM)")
+    subplotlocal(nrows, ncols, 10, els, xplot, yplot, Sbem[:, 1], contoursdisp, "sxx (BEM)")
+    subplotlocal(nrows, ncols, 11, els, xplot, yplot, Sbem[:, 2], contoursdisp, "syy (BEM)")
+    subplotlocal(nrows, ncols, 12, els, xplot, yplot, Sbem[:, 3], contoursdisp, "sxy (BEM)")
 
     # #! Residuals
     subplot(3, 6, 13)
-    quiver(x[:], y[:], dispanalytic[:, 1]-dispbem[:, 1], dispanalytic[:, 2]-dispbem[:, 2], units="width", color="b")
+    quiver(x[:], y[:], Danalytic[:, 1]-Dbem[:, 1], Danalytic[:, 2]-Dbem[:, 2], units="width", color="b")
     stylesubplots_local()
     plotelements(els)
     title("u", fontsize=fontsize)
-    subplotlocal(nrows, ncols, 14, els, xplot, yplot, dispanalytic[:, 1]-dispbem[:, 1], contoursdisp, "ux residuals")
-    subplotlocal(nrows, ncols, 15, els, xplot, yplot, dispanalytic[:, 2]-dispbem[:, 2], contoursdisp, "uy residuals")
-    subplotlocal(nrows, ncols, 16, els, xplot, yplot, stressanalytic[:, 1]-stressbem[:, 1], contoursdisp, "sxx residuals")
-    subplotlocal(nrows, ncols, 17, els, xplot, yplot, stressanalytic[:, 2]-stressbem[:, 2], contoursdisp, "syy residuals")
-    subplotlocal(nrows, ncols, 18, els, xplot, yplot, stressanalytic[:, 3]-stressbem[:, 3], contoursdisp, "sxy residuals")
+    subplotlocal(nrows, ncols, 14, els, xplot, yplot, Danalytic[:, 1]-Dbem[:, 1], contoursdisp, "ux residuals")
+    subplotlocal(nrows, ncols, 15, els, xplot, yplot, Danalytic[:, 2]-Dbem[:, 2], contoursdisp, "uy residuals")
+    subplotlocal(nrows, ncols, 16, els, xplot, yplot, Sanalytic[:, 1]-Sbem[:, 1], contoursdisp, "sxx residuals")
+    subplotlocal(nrows, ncols, 17, els, xplot, yplot, Sanalytic[:, 2]-Sbem[:, 2], contoursdisp, "syy residuals")
+    subplotlocal(nrows, ncols, 18, els, xplot, yplot, Sanalytic[:, 3]-Sbem[:, 3], contoursdisp, "sxy residuals")
 
     tight_layout()
     show()
