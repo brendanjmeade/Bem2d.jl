f8ae066e7155e21ff1d2618660eae21e8d268714
brendanjmeade <brendanjmeade@gmail.com>
2020-04-15 15:30:44 -0400

    Removed unused circle related functions

--- examples/gravitysquare.jl
+++ examples/gravitysquare.jl
@@ -9,66 +9,19 @@ using Bem2d
 
 
 """
-    discretized_arc(θstart, θend, radius, n_pts)
-
-Generate regularly spaced eleemnts along an curved arc.
-"""
-function discretized_arc(θstart, θend, radius, n_pts)
-    # Create geometry of discretized arc
-    θrange = collect(LinRange(θstart, θend, n_pts + 1))
-    x = @. radius * cos(θrange)
-    y = @. radius * sin(θrange)
-    x1 = x[1:1:end-1]
-    x2 = x[2:1:end]
-    y1 = y[1:1:end-1]
-    y2 = y[2:1:end]
-    return x1, y1, x2, y2
-end
-
-
-"""
-    circle_subplot(nrows, ncols, plotidx, x, y, mat, npts, R, theta0, title_string)
-
-Plot field (displacement, stress) within a circular disk and style
-"""
-function circle_subplot(nrows, ncols, plotidx, x, y, mat, npts, R, theta0, title_string)
-    fontsize = 20
-    contour_levels = 200
-    contour_color = "white"
-    contour_linewidth = 0.5
-    color_scale = 1
-
-    subplot(nrows, ncols, plotidx)
-    contourf(reshape(x, npts, npts), reshape(y, npts, npts), reshape(mat, npts, npts), levels=contour_levels)
-    cbar = colorbar(fraction=0.05, pad=0.05, extend = "both")
-    cbar.ax.tick_params(labelsize=fontsize)
-    contour(reshape(x, npts, npts), reshape(y, npts, npts), reshape(mat, npts, npts), levels=contour_levels, colors=contour_color, linewidths=contour_linewidth)
-    xlabel("x (m)", fontsize=fontsize)
-    ylabel("y (m)", fontsize=fontsize)
-    title(title_string, fontsize=fontsize)
-
-    # Draw entire circle and region of applied tractions
-    x1, y1, x2, y2 = discretized_arc(deg2rad(-180), deg2rad(180), R, 360)
-    plot([x1, x2], [y1, y2], "-k", linewidth=2)
-    gca().set_aspect("equal")
-    gca().tick_params(labelsize=fontsize)
-end
-
-
-"""
    kelvinUD(x, y, xoffset, yoffset, fx, fy, mu, nu)
 
    Analytic point source Kelvin solution from Crouch and Starfield
    section 4.2.
 """
 function kelvinUD(x, y, xoffset, yoffset, fx, fy, mu, nu)
-    x = @. x - xoffset
-    y = @. y - yoffset
+    x = x .- xoffset
+    y = y .- yoffset
     Ukelvin = zeros(length(x), 2)
     Skelvin = zeros(length(x), 3)
     C = 1/(4*pi*(1-nu))
-    r = @. sqrt(x^2+y^2)
-    g = @. -C * log(r)
+    r = sqrt.(x.^2 .+ y.^2)
+    g = -C .* log.(r)
     gx = @. -C * x/(x^2+y^2)
     gy = @. -C * y/(x^2+y^2)
     gxy = @. C * 2*x*y/(x^2+y^2)^2
@@ -208,14 +161,14 @@ function gravitydisc()
     #! Loop over each triangle calculate area and contribution to u_eff
     #! For each element centroid calculate the effect of a body force
     ntri = size(mesh.cell)[2]
-    Ugmesh = zeros(els.endidx, 2)
+    Uboundarygravity = zeros(els.endidx, 2)
     for i in 1:ntri
         xtri = mesh.point[1, mesh.cell[:, i]]
         ytri = mesh.point[2, mesh.cell[:, i]]
         triarea = trianglearea(xtri[1], ytri[1], xtri[2], ytri[3], xtri[3], ytri[3])
         tricentroid = [mean(xtri) mean(ytri)]
         Ugi, _ = kelvinUD(els.xcenter[1:1:els.endidx], els.ycenter[1:1:els.endidx], tricentroid[1], tricentroid[2], fx, fy, mu, nu)
-        Ugmesh += @. triarea * Ugi
+        Uboundarygravity += triarea .* Ugi
     end
     
     #! What I need is a full forward evaluation at the bottom coordinates only
@@ -224,22 +177,30 @@ function gravitydisc()
                                        els.ycenter[idx["bottom"]],
                                        els,
                                        collect(1:1:4*nels),
-                                       Ugmesh[:, 1],
-                                       Ugmesh[:, 2],
-                                       mu, nu)
-
+                                       Uboundarygravity[:, 1],
+                                       Uboundarygravity[:, 2],
+                                       mu,
+                                       nu)
 
     #! Forward evaluation
-    Udispg, _ = constdispstress(slip2dispstress, x, y, els,
+    Udispg, _ = constdispstress(slip2dispstress,
+                                x,
+                                y,
+                                els,
                                 collect(1:1:4*nels),
-                                Ugmesh[:, 1],
-                                Ugmesh[:, 2],
-                                mu, nu)
-    Udispgbottom, _ = constdispstress(slip2dispstress, x, y, els,
+                                Uboundarygravity[:, 1],
+                                Uboundarygravity[:, 2],
+                                mu,
+                                nu)
+    Udispgbottom, _ = constdispstress(slip2dispstress,
+                                      x,
+                                      y,
+                                      els,
                                       idx["bottom"],
                                       Udispgbottom2[:, 1],
                                       Udispgbottom2[:, 2],
-                                      mu, nu)
+                                      mu,
+                                      nu)
     
     #! Plot meshes and fields
     figure(figsize=(20,20))
@@ -249,6 +210,7 @@ function gravitydisc()
     contour_levels = 20
     contour_color = "white"
     contour_linewidth = 1.0
+    markersize = 12
 
     subplot(nrows, ncols, 1)
     plotmesh(mesh)
@@ -260,12 +222,27 @@ function gravitydisc()
     gca().tick_params(labelsize=fontsize)
 
     subplot(nrows, ncols, 2)
-    plot(Ugmesh[:, 1], "-r", label="ux gravity")
-    plot(Ugmesh[:, 2], "-b", label="uy gravity")
+    plot(Uboundarygravity[:, 1], "-r", label="ux gravity")
+    plot(Uboundarygravity[:, 2], "-b", label="uy gravity")
     plot(Udispgbottom2[:, 1], "xr", label="ux correction")
     plot(Udispgbottom2[:, 2], ".b", label="uy correction")
     legend()
+    title("components on mesh", fontsize=fontsize)
     gca().tick_params(labelsize=fontsize)
+
+    #! y-components only
+    subplot(nrows, ncols, 3) # Plot slices
+    xmat = reshape(x, npts, npts)
+    uymat = reshape(Udispg[:, 2], npts, npts)
+    # uymat = uymat .- minimum(uymat)
+    uymatdispgbottom = reshape(Udispgbottom[:, 2], npts, npts)
+    plot(xmat[:, 1], uymat[:, 1], "-b", label="gravity at base")
+    plot(xmat[:, 1], uymatdispgbottom[:, 1], "b+", markersize=markersize)
+    plot(els.xcenter[idx["bottom"]], Udispgbottom2[:, 2], "bx", markersize=markersize)
+    gca().tick_params(labelsize=fontsize)
+    title("Translated")
+
+
 
     subplot(nrows, ncols, 4)
     mat = Udispg[:, 1]
@@ -280,7 +257,7 @@ function gravitydisc()
     title("ux", fontsize=fontsize)
 
     subplot(nrows, ncols, 7)
-    mat = Udispg[:, 2]
+    mat = Udispg[:, 2] .- minimum(Udispg[:, 2]) 
     contourf(reshape(x, npts, npts), reshape(y, npts, npts), reshape(mat, npts, npts), levels=contour_levels)
     cbar = colorbar(fraction=0.05, pad=0.05, extend = "both")
     cbar.ax.tick_params(labelsize=fontsize)
@@ -293,7 +270,7 @@ function gravitydisc()
     title("uy", fontsize=fontsize)
 
     subplot(nrows, ncols, 8)
-    mat = Udispgbottom[:, 2]
+    mat = Udispgbottom[:, 2] #.- minimum(Udispgbottom[:, 2])
     contourf(reshape(x, npts, npts), reshape(y, npts, npts), reshape(mat, npts, npts), levels=contour_levels)
     cbar = colorbar(fraction=0.05, pad=0.05, extend = "both")
     cbar.ax.tick_params(labelsize=fontsize)
@@ -304,6 +281,22 @@ function gravitydisc()
     xlabel("x (m)", fontsize=fontsize)
     ylabel("y (m)", fontsize=fontsize)
     title("uy", fontsize=fontsize)
+
+    subplot(nrows, ncols, 9)
+    mat1 = Udispg[:, 2] .- minimum(Udispg[:, 2])
+    mat2 = Udispgbottom[:, 2] .- minimum(Udispgbottom[:, 2])
+    mat = mat1 .- mat2
+    contourf(reshape(x, npts, npts), reshape(y, npts, npts), reshape(mat, npts, npts), levels=contour_levels)
+    cbar = colorbar(fraction=0.05, pad=0.05, extend = "both")
+    cbar.ax.tick_params(labelsize=fontsize)
+    clim([0, 2e-8])
+    contour(reshape(x, npts, npts), reshape(y, npts, npts), reshape(mat, npts, npts), levels=contour_levels, colors=contour_color, linewidths=contour_linewidth)
+    gca().set_aspect("equal")
+    gca().tick_params(labelsize=fontsize)
+    xlabel("x (m)", fontsize=fontsize)
+    ylabel("y (m)", fontsize=fontsize)
+    title("uy", fontsize=fontsize)
+
 
     tight_layout()
     show()
