5f002a727211eedea04c221c72dd9296097c32e1
brendanjmeade <brendanjmeade@gmail.com>
2020-05-05 11:47:41 -0400

    Added labels and returned surface profile plot.

--- examples/okadaindirect.jl
+++ examples/okadaindirect.jl
@@ -12,9 +12,9 @@ ow = pyimport("okada_wrapper") # from okada_wrapper import dc3dwrapper
 Standard plot formatting.
 """
 function plotformat(fontsize)
-    gca().set_xlim([-5, 5]); gca().set_ylim([-1.0, 1.0])
-    gca().set_xticks([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5])
-    gca().set_yticks([-1.00, -0.75, -0.50, -0.25, 0.0, 0.25, 0.50, 0.75, 1.00])
+    # gca().set_xlim([-5, 5]); gca().set_ylim([-1.0, 1.0])
+    # gca().set_xticks([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5])
+    # gca().set_yticks([-1.00, -0.75, -0.50, -0.25, 0.0, 0.25, 0.50, 0.75, 1.00])
     legend(fontsize=fontsize)
     gca().tick_params("both", labelsize=fontsize)
     xlabel(L"$x$ (m)", fontsize=fontsize)
@@ -138,24 +138,27 @@ function okadaindirect()
     #! Hacky BEM
     ufullspacehacky, uhalfspacehacky = hackybem(els, idx, sqrt(2)/2 .* ones(2*nfault), mu, nu)
 
-    #! Formal indirect BEM
+    #! Indirect BEM
     T_psurface_qfault, H_psurface_qfault = PUTC(slip2dispstress, els, idx["surface"], idx["fault"], mu, nu)
     T_psurface_qsurface, H_psurface_qsurface = PUTC(slip2dispstress, els, idx["surface"], idx["surface"], mu, nu)
     bcs = sqrt(2.0)/2.0 * ones(2*nfault)
     ueff = inv(H_psurface_qsurface) * H_psurface_qfault * bcs
     uhalfspaceindirect = ueff
 
-    #! Interior evaluation
-    #TODO: This does not give the right answer at the surface
-    nobs = 50
-    x, y = obsgrid(-50e3, -50e3, 50e3, -1, nobs)
+    Usurface, _ = constdispstress(slip2dispstress, xbem, zeros(length(xbem)), els, idx["surface"], ueff[1:2:end], ueff[2:2:end], mu, nu)
+    Ufault, _ = constdispstress(slip2dispstress, xbem, zeros(length(xbem)), els, idx["fault"], bcs[1:2:end], bcs[2:2:end], mu, nu)
+    uxbemsurface = -Usurface[:, 1] + Ufault[:, 1]
+    uybemsurface = -Usurface[:, 2] + Ufault[:, 2]
 
-    @time Usurface, Ssurface = constdispstress(slip2dispstress, x, y, els, idx["surface"], ueff[1:2:end], ueff[2:2:end], mu, nu)
-    @time Ufault, Sfault = constdispstress(slip2dispstress, x, y, els, idx["fault"], bcs[1:2:end], bcs[2:2:end], mu, nu)
+
+    #! Interior evaluation both BEM and Okada
+    # This does not give the right answer at the surface
+    nobs = 50
+    x, y = obsgrid(-50e3, -30e3, 50e3, 0, nobs)
+    Usurface, Ssurface = constdispstress(slip2dispstress, x, y, els, idx["surface"], ueff[1:2:end], ueff[2:2:end], mu, nu)
+    Ufault, Sfault = constdispstress(slip2dispstress, x, y, els, idx["fault"], bcs[1:2:end], bcs[2:2:end], mu, nu)
     uxbem = -Usurface[:, 1] + Ufault[:, 1]
     uybem = -Usurface[:, 2] + Ufault[:, 2]
-
-    #! Okada interior evaluation
     uxokadainterior, uyokadainterior = okadalocalinterior(x, y)
 
     #! 18 panel plot
@@ -166,7 +169,7 @@ function okadaindirect()
     ncols = 3
     xmat = reshape(x, nobs, nobs)
     ymat = reshape(y, nobs, nobs)
-    contourvec = collect(LinRange(-0.5, 0.5, 20))
+    contourvec = collect(LinRange(-0.5, 0.5, 50))
     figure(figsize=(30, 20))
 
     # Okada results
@@ -174,6 +177,9 @@ function okadaindirect()
     quiver(x, y, uxokadainterior, uyokadainterior)
     gca().set_aspect("equal")
     gca().tick_params(labelsize=fontsize)
+    xlabel("x (m)", fontsize=fontsize)
+    ylabel("y (m)", fontsize=fontsize)
+    title("u (BEM)", fontsize=fontsize)
 
     subplot(nrows, ncols, 2)
     umat = reshape(uxokadainterior, nobs, nobs)
@@ -183,6 +189,9 @@ function okadaindirect()
     contour(xmat, ymat, umat, levels=contourvec, colors="k", linewidths=0.5)
     gca().set_aspect("equal")
     gca().tick_params(labelsize=fontsize)
+    xlabel("x (m)", fontsize=fontsize)
+    ylabel("y (m)", fontsize=fontsize)
+    title("ux (Okada)", fontsize=fontsize)
 
     subplot(nrows, ncols, 3)
     umat = reshape(uyokadainterior, nobs, nobs)
@@ -192,6 +201,9 @@ function okadaindirect()
     contour(xmat, ymat, umat, levels=contourvec, colors="k", linewidths=0.5)
     gca().set_aspect("equal")
     gca().tick_params(labelsize=fontsize)
+    xlabel("x (m)", fontsize=fontsize)
+    ylabel("y (m)", fontsize=fontsize)
+    title("uy (Okada)", fontsize=fontsize)
 
     # IBEM results
     subplot(nrows, ncols, 4)
@@ -200,79 +212,90 @@ function okadaindirect()
     gca().tick_params(labelsize=fontsize)
     xlabel("x (m)", fontsize=fontsize)
     ylabel("y (m)", fontsize=fontsize)
-    title("u_x (fault)", fontsize=fontsize)
+    title("u (BEM)", fontsize=fontsize)
 
     subplot(nrows, ncols, 5)
     umat = reshape(uxbem, nobs, nobs)
     contourf(xmat, ymat, umat, levels=contourvec)
-    cbar = colorbar(fraction=0.05, pad=0.05, extend = "both")
+    cbar = colorbar(fraction=0.05, pad=0.05, extend="both")
     cbar.ax.tick_params(labelsize=fontsize)
     contour(xmat, ymat, umat, levels=contourvec, colors="k", linewidths=0.5)
     gca().set_aspect("equal")
     gca().tick_params(labelsize=fontsize)
+    xlabel("x (m)", fontsize=fontsize)
+    ylabel("y (m)", fontsize=fontsize)
+    title("ux (BEM)", fontsize=fontsize)
 
     subplot(nrows, ncols, 6)
     umat = reshape(uybem, nobs, nobs)
     contourf(xmat, ymat, umat, levels=contourvec)
-    cbar = colorbar(fraction=0.05, pad=0.05, extend = "both")
+    cbar = colorbar(fraction=0.05, pad=0.05, extend="both")
     cbar.ax.tick_params(labelsize=fontsize)
     contour(xmat, ymat, umat, levels=contourvec, colors="k", linewidths=0.5)
     gca().set_aspect("equal")
     gca().tick_params(labelsize=fontsize)
+    xlabel("x (m)", fontsize=fontsize)
+    ylabel("y (m)", fontsize=fontsize)
+    title("uy (BEM)", fontsize=fontsize)
 
     # Residuals
     subplot(nrows, ncols, 7)
     quiver(x, y, uxbem .- uxokadainterior, uybem - uyokadainterior)
     gca().set_aspect("equal")
     gca().tick_params(labelsize=fontsize)
+    xlabel("x (m)", fontsize=fontsize)
+    ylabel("y (m)", fontsize=fontsize)
+    title("u (residual)", fontsize=fontsize)
 
     subplot(nrows, ncols, 8)
     umat = reshape(uxbem .- uxokadainterior, nobs, nobs)
     contourf(xmat, ymat, umat, levels=contourvec)
-    cbar = colorbar(fraction=0.05, pad=0.05, extend = "both")
+    cbar = colorbar(fraction=0.05, pad=0.05, extend="both")
     cbar.ax.tick_params(labelsize=fontsize)
     contour(xmat, ymat, umat, levels=contourvec, colors="k", linewidths=0.5)
     gca().set_aspect("equal")
     gca().tick_params(labelsize=fontsize)
+    xlabel("x (m)", fontsize=fontsize)
+    ylabel("y (m)", fontsize=fontsize)
+    title("ux (residual)", fontsize=fontsize)
 
     subplot(nrows, ncols, 9)
     umat = reshape(uybem .- uyokadainterior, nobs, nobs)
     contourf(xmat, ymat, umat, levels=contourvec)
-    cbar = colorbar(fraction=0.05, pad=0.05, extend = "both")
+    cbar = colorbar(fraction=0.05, pad=0.05, extend="both")
     cbar.ax.tick_params(labelsize=fontsize)
     contour(xmat, ymat, umat, levels=contourvec, colors="k", linewidths=0.5)
     gca().set_aspect("equal")
     gca().tick_params(labelsize=fontsize)
-
-
-
-    @infiltrate
-    return
-
-    #! Okada solution
-    xokada = collect(LinRange(-5, 5, 1000))
-    uxokada, uyokada = okadalocal(xokada)
-
-    #! Plot comparison between BEM and Okada
-    figure(figsize = (20, 20))
-
-    ax = subplot(2, 1, 1)
-    plot(xokada, uxokada, "-k", linewidth=linewidth, label="Okada (halfspace)")
-    plot(xbem, uhalfspacehacky[1:2:end], "bx", markeredgewidth=linewidth, markersize=markersize, label="hacky BEM (halfspace)")
-    plot(xbem, uhalfspaceindirect[1:2:end], "r+", markeredgewidth=linewidth, markersize=markersize, label="indirect BEM (halfspace)")
-    plot(xbem, uinterior[1:2:end], ".g", markeredgewidth=linewidth, markersize=markersize, label = "interior BEM (halfspace)")
-    ylabel(L"$u_x$ (m)", fontsize=fontsize)
-    plotformat(fontsize)
-
-    ax = subplot(2, 1, 2)
-    plot(xokada, uyokada, "-k", linewidth=linewidth, label="Okada (halfspace)")
-    plot(xbem, uhalfspacehacky[2:2:end], "bx", markeredgewidth=linewidth, markersize=markersize, label = "hacky BEM (halfspace)")
-    plot(xbem, uhalfspaceindirect[2:2:end], "r+", markeredgewidth=linewidth, markersize=markersize, label = "indirect BEM (halfspace)")
-    plot(xbem, uinterior[2:2:end], ".g", markeredgewidth=linewidth, markersize=markersize, label = "interior BEM (halfspace)")
-    ylabel(L"$u_y$ (m)", fontsize=fontsize)
-    plotformat(fontsize)
-
+    xlabel("x (m)", fontsize=fontsize)
+    ylabel("y (m)", fontsize=fontsize)
+    title("uy (residual)", fontsize=fontsize)
+    tight_layout()
     show()
-    @infiltrate
+
+
+    # #! Okada solution
+    # xokada = collect(LinRange(-5, 5, 1000))
+    # uxokada, uyokada = okadalocal(xokada)
+
+    # #! Plot comparison between BEM and Okada
+    # figure(figsize = (20, 20))
+
+    # ax = subplot(2, 1, 1)
+    # plot(xokada, uxokada, "-k", linewidth=linewidth, label="Okada (halfspace)")
+    # plot(xbem, uhalfspacehacky[1:2:end], "bx", markeredgewidth=linewidth, markersize=markersize, label="hacky BEM (halfspace)")
+    # plot(xbem, uhalfspaceindirect[1:2:end], "r+", markeredgewidth=linewidth, markersize=markersize, label="indirect BEM (halfspace)")
+    # plot(xbem, uxbemsurface, ".g", markeredgewidth=linewidth, markersize=markersize, label = "interior BEM (halfspace)")
+    # ylabel(L"$u_x$ (m)", fontsize=fontsize)
+    # plotformat(fontsize)
+
+    # ax = subplot(2, 1, 2)
+    # plot(xokada, uyokada, "-k", linewidth=linewidth, label="Okada (halfspace)")
+    # plot(xbem, uhalfspacehacky[2:2:end], "bx", markeredgewidth=linewidth, markersize=markersize, label = "hacky BEM (halfspace)")
+    # plot(xbem, uhalfspaceindirect[2:2:end], "r+", markeredgewidth=linewidth, markersize=markersize, label = "indirect BEM (halfspace)")
+    # plot(xbem, uxbemsurface, ".g", markeredgewidth=linewidth, markersize=markersize, label = "interior BEM (halfspace)")
+    # ylabel(L"$u_y$ (m)", fontsize=fontsize)
+    # plotformat(fontsize)
+    # show()
 end
 okadaindirect()
