12ca53ba048a0ce768bacd4cb0840769a7c0ddb7
brendanjmeade <brendanjmeade@users.noreply.github.com>
2020-06-10 00:12:00 -0400

    Removed unused code and functions

--- examples/gravitysquareparticular3.jl
+++ examples/gravitysquareparticular3.jl
@@ -4,128 +4,7 @@ using LaTeXStrings
 using PyPlot
 using LinearAlgebra
 using Infiltrator
-using TriangleMesh
 using Bem2d
-
-
-"""
-   kelvinUD(x, y, xoffset, yoffset, fx, fy, mu, nu)
-
-   Analytic point source Kelvin solution from Crouch and Starfield
-   section 4.2.
-"""
-function kelvinUS(x, y, xoffset, yoffset, fx, fy, mu, nu)
-    x = @. x - xoffset
-    y = @. y - yoffset
-    Ukelvin = zeros(length(x), 2)
-    Skelvin = zeros(length(x), 3)
-    C = 1/(4*pi*(1-nu))
-    r = @. sqrt(x^2 + y^2)
-    g = @. -C * log(r)
-    gx = @. -C * x/(x^2+y^2)
-    gy = @. -C * y/(x^2+y^2)
-    gxy = @. C * 2*x*y/(x^2+y^2)^2
-    gxx = @. C * (x^2-y^2)/(x^2+y^2)^2
-    gyy = -gxx
-
-    Ukelvin[:, 1] = @. fx/(2*mu)*((3-4*nu)*g-x*gx) + fy/(2*mu)*(-y*gx)
-    Ukelvin[:, 2] = @. fx/(2*mu)*(-x*gy) + fy/(2*mu)*((3-4*nu)*g-y*gy)
-    Skelvin[:, 1] = @. fx*(2*(1-nu)*gx-x*gxx) + fy*(2*nu*gy-y*gxx)
-    Skelvin[:, 2] = @. fx*(2*nu*gx-x*gyy) + fy*(2*(1-nu)*gy-y*gyy)
-    Skelvin[:, 3] = @. fx*((1-2*nu)*gy-x*gxy) + fy*((1-2*nu)*gx-y*gxy)
-    return Ukelvin, Skelvin
-end
-
-
-"""
-    trianglearea(x1, y1, x2, y2, x3, y3)
-
-    It's just half the absolute value of the determinant: 
-    From https://math.stackexchange.com/questions/516219/finding-out-the-area-of-a-triangle-if-the-coordinates-of-the-three-vertices-are
-"""
-function trianglearea(x1, y1, x2, y2, x3, y3)
-    return abs(0.5*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2)))
-end
-
-
-"""
-    trimesh(nodes, connectivity, refinefactor)
-
-Calls triangle binary (https://www.cs.cmu.edu/~quake/triangle.html)
-Based on example at: https://github.com/konsim83/TriangleMesh.jl/blob/master/docs/src/man/examples.md
-"""
-function trimesh(nodes, connectivity, refinefactor)
-    nnodes = size(nodes)[1]
-    poly = Polygon_pslg(nnodes, 1, 2, nnodes, 0)
-    set_polygon_point!(poly, nodes)
-    set_polygon_segment!(poly, connectivity)
-    mesh = create_mesh(poly)
-    if refinefactor > 1
-        # mesh = refine(mesh, divide_cell_into=refinefactor, keep_edges=true)
-        mesh = refine(mesh, divide_cell_into=refinefactor, keep_edges=false)
-    end
-    return mesh
-end
-
-
-"""
-    plotmesh(mesh)
-
-    Plot mesh with shading, mesh edges, and centroids
-"""
-function plotmesh(mesh)
-    for i in 1:size(mesh.cell)[2]
-        x = mesh.point[1, mesh.cell[:, i]]
-        y = mesh.point[2, mesh.cell[:, i]]
-        fill(x, y, "lightgray", edgecolor="k", linewidth=0.25)
-    end
-    return nothing
-end
-
-
-
-"""
-    PUTK(els, obsidx, srcidx, mu, nu)
-
-Kernel relating body forces to boundary displacements and tractions.
-Uses Kelvin kernels.
-"""
-function PUTK(els, obsidx, mesh, mu, nu)
-    ntri = size(mesh.cell)[2]
-    nobs = length(obsidx)
-    Ku = zeros(2*nobs, 2*ntri)
-    Kt = zeros(2*nobs, 2*ntri)
-    _Ku = zeros(2, 2)
-    _Ks = zeros(3, 2)
-    _Kt = zeros(2, 2)
-    for iobs in 1:nobs # Should I loop over obs or src?  src is easier to write.
-        for isrc in 1:ntri
-            # Triangle areas and centroids
-            xtri = mesh.point[1, mesh.cell[:, isrc]]
-            ytri = mesh.point[2, mesh.cell[:, isrc]]
-            triarea = trianglearea(xtri[1], ytri[1], xtri[2], ytri[3], xtri[3], ytri[3])
-            tricentroid = [mean(xtri) mean(ytri)]
-
-            # Calculate displacement and stress kernels
-            _Ku[:, 1], _Ks[:, 1] = kelvinUS([els.xcenter[obsidx[iobs]]], [els.ycenter[obsidx[iobs]]], tricentroid[1], tricentroid[2], 1, 0, mu, nu)
-            _Ku[:, 2], _Ks[:, 2] = kelvinUS([els.xcenter[obsidx[iobs]]], [els.ycenter[obsidx[iobs]]], tricentroid[1], tricentroid[2], 0, 1, mu, nu)
-
-            # TODO This is a really stupid area scaling.  Need to upgrade to Guassian 4-point.
-            # Is there any good way to do this?
-            _Ku *= triarea
-            _Ks *= triarea
-
-            # Calculate traction kernels from stress kernels
-            _Kt[:, 1] = stress2trac(_Ks[:, 1], [els.xnormal[obsidx[iobs]] ; els.ynormal[obsidx[iobs]]])
-            _Kt[:, 2] = stress2trac(_Ks[:, 2], [els.xnormal[obsidx[iobs]] ; els.ynormal[obsidx[iobs]]])
-
-            # Insert 2x2 matrices into system matrices
-            Ku[2 * (iobs - 1) + 1:2 * (iobs - 1) + 2, 2 * (isrc - 1) + 1:2 * (isrc - 1) + 2] = _Ku
-            Kt[2 * (iobs - 1) + 1:2 * (iobs - 1) + 2, 2 * (isrc - 1) + 1:2 * (isrc - 1) + 2] = _Kt
-        end
-    end
-    return Ku, Kt
-end
 
 
 """
@@ -146,14 +25,9 @@ function gravitysquareparticular()
     # x, y = obsgrid(-L+1, -L+1, L-1, L-1, npts)
     x, y = obsgrid(-L+100, -L+100, L-100, L-100, npts)
 
-    fx = 0.0
-    fy = g * rho
-
     #! BEM geometry
     els = Elements(Int(1e5))
     x1, y1, x2, y2 = discretizedline(-L, -L, L, -L, nels)
-    xbottom = x1
-    ybottom = y1
     for i in 1:length(x1)
         els.x1[els.endidx + i] = x1[i]
         els.y1[els.endidx + i] = y1[i]
@@ -164,8 +38,6 @@ function gravitysquareparticular()
     standardize_elements!(els)
 
     x1, y1, x2, y2 = discretizedline(L, -L, L, L, nels)
-    xright = x1
-    yright = y1
     for i in 1:length(x1)
         els.x1[els.endidx + i] = x1[i]
         els.y1[els.endidx + i] = y1[i]
@@ -176,8 +48,6 @@ function gravitysquareparticular()
     standardize_elements!(els)
 
     x1, y1, x2, y2 = discretizedline(L, L, -L, L, nels)
-    xtop = x1
-    ytop = y1
     for i in 1:length(x1)
         els.x1[els.endidx + i] = x1[i]
         els.y1[els.endidx + i] = y1[i]
@@ -188,8 +58,6 @@ function gravitysquareparticular()
     standardize_elements!(els)
 
     x1, y1, x2, y2 = discretizedline(-L, L, -L, -L, nels)
-    xleft = x1
-    yleft = y1
     for i in 1:length(x1)
         els.x1[els.endidx + i] = x1[i]
         els.y1[els.endidx + i] = y1[i]
