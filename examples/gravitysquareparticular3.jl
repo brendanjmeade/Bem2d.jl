1a0a194a296a3c37855e5c4eaf69a39cf496eaf4
brendanjmeade <brendanjmeade@users.noreply.github.com>
2020-06-19 15:40:15 -0400

    Misc cleanup

--- examples/gravitysquareparticular3.jl
+++ examples/gravitysquareparticular3.jl
@@ -1,7 +1,5 @@
 using Revise
 using PyPlot
-using LinearAlgebra
-using Infiltrator
 using Bem2d
 
 
@@ -12,6 +10,7 @@ Experiments with gravity body force.
 """
 function gravitysquareparticular()
     close("all")
+    alpha = 7e-8 # scalar precondtioner for traction terms
     fontsize = 20
     mu = 3e10
     lambda = 3e10
@@ -74,73 +73,43 @@ function gravitysquareparticular()
     T_pB_qBRTL, H_pB_qBRTL = PUTC(slip2dispstress, els, Bidx, BRTLidx, mu, nu)
     T_pRTL_qBRTL, H_pRTL_qBRTL = PUTC(slip2dispstress, els, RTLidx, BRTLidx, mu, nu)
 
-    #! Particular solution technique with modified boundary conditoions
-    # alpha = 7e-8
-    # bcs = zeros(2 * 4 * nels)
-    # bcs[1:2:40] = @. -lambda*rho*g * els.xcenter[idx["B"]]*els.ycenter[idx["B"]] / (4*mu*(lambda + mu)) # Bottom boundary (x-component)
-    # bcs[2:2:40] = @. (rho*g) * (lambda*els.xcenter[idx["B"]]^2 + (lambda+2*mu)*els.ycenter[idx["B"]]^2) / (8*mu*(lambda + mu)) # Bottom boundary (y-component)
-    # bcs[41:2:80] .= 0 # Right boundary (x-component)
-    # bcs[42:2:80] = @. alpha * rho * g * (els.ycenter[idx["R"]]) # Right boundary (y-component)
-    # bcs[81:2:120] .= 0 # Top boundary (x-component)
-    # bcs[82:2:120] .= @. alpha * rho * g * (els.ycenter[idx["T"]]) # Top boundary (y-component)    
-    # bcs[121:2:160] .= 0 # Left boundary (x-component)
-    # bcs[122:2:160] = @. alpha * rho * g * (els.ycenter[idx["L"]])  # Left boundary (y-component)
-    # bcs *= -1 # Flip sign of all bcs?
+    # Particular solution technique with modified boundary conditoions    
+    # bcseff = zeros(2 * 4 * nels)
+    # bcseff[1:2:40] = @. -lambda*rho*g * els.xcenter[idx["B"]]*els.ycenter[idx["B"]] / (4*mu*(lambda + mu)) # Bottom boundary (x-component)
+    # bcseff[2:2:40] = @. (rho*g) * (lambda*els.xcenter[idx["B"]]^2 + (lambda+2*mu)*els.ycenter[idx["B"]]^2) / (8*mu*(lambda + mu)) # Bottom boundary (y-component)
+    # bcseff[41:2:80] .= 0 # Right boundary (x-component)
+    # bcseff[42:2:80] .= 0 # Right boundary (y-component)
+    # bcseff[81:2:120] .= 0 # Top boundary (x-component)
+    # bcseff[82:2:120] = @. alpha * rho * g * (els.ycenter[idx["T"]]) # Top boundary (y-component)    
+    # bcseff[121:2:160] .= 0 # Left boundary (x-component)
+    # bcseff[122:2:160] .= 0 # Left boundary (y-component)
+    # bcseff *= -1 # This is neccesary for the right answer and is consistent with derivation
 
-    #! Particular solution technique with modified boundary conditoions
-    # alpha = 7e-8
-    # bcs = zeros(2 * 4 * nels)
-    # bcs[1:2:40] = @. -lambda*rho*g * els.xcenter[idx["B"]]*els.ycenter[idx["B"]] / (4*mu*(lambda + mu)) # Bottom boundary (x-component)
-    # bcs[2:2:40] = @. (rho*g) * (lambda*els.xcenter[idx["B"]]^2 + (lambda+2*mu)*els.ycenter[idx["B"]]^2) / (8*mu*(lambda + mu)) # Bottom boundary (y-component)
-    # bcs[41:2:80] .= 0 # Right boundary (x-component)
-    # bcs[42:2:80] = @. alpha * rho * g * (els.ycenter[idx["L"]]) # Right boundary (y-component)
-    # bcs[81:2:120] .= 0 # Top boundary (x-component)
-    # bcs[82:2:120] .= 0 # Top boundary (y-component)    
-    # bcs[121:2:160] .= 0 # Left boundary (x-component)
-    # bcs[122:2:160] = @. alpha * rho * g * (els.ycenter[idx["R"]])  # Left boundary (y-component)
-    # bcs *= -1 # This is neccesary for the right answer and consistent with derivation
-
-    #! Particular solution technique with modified boundary conditoions
-    # Now noting (as Ben suggested) that the modified BCs o the side go to zero
-    # because n_2 = 0 for them!!!
-    bcstrue = zeros(2 * 4 * nels)
-    alpha = 7e-8
-    
+    # Tryin a bottom BC only case
     bcseff = zeros(2 * 4 * nels)
-    bcseff[1:2:40] = @. -lambda*rho*g * els.xcenter[idx["B"]]*els.ycenter[idx["B"]] / (4*mu*(lambda + mu)) # Bottom boundary (x-component)
-    bcseff[2:2:40] = @. (rho*g) * (lambda*els.xcenter[idx["B"]]^2 + (lambda+2*mu)*els.ycenter[idx["B"]]^2) / (8*mu*(lambda + mu)) # Bottom boundary (y-component)
+    # bcseff[1:2:40] = @. -lambda*rho*g * els.xcenter[idx["B"]]*els.ycenter[idx["B"]] / (4*mu*(lambda + mu)) # Bottom boundary (x-component)
+    # bcseff[2:2:40] = @. (rho*g) * (lambda*els.xcenter[idx["B"]]^2 + (lambda+2*mu)*els.ycenter[idx["B"]]^2) / (8*mu*(lambda + mu)) # Bottom boundary (y-component)
+    bcseff[1:2:40] = @. -lambda*rho*g * els.xcenter[idx["B"]]*-20000 / (4*mu*(lambda+mu)) # Bottom boundary (x-component)
+    bcseff[2:2:40] = @. (rho*g) * (lambda*els.xcenter[idx["B"]]^2 + (lambda+2*mu)*20000^2) / (8*mu*(lambda+mu)) # Bottom boundary (y-component)
     bcseff[41:2:80] .= 0 # Right boundary (x-component)
     bcseff[42:2:80] .= 0 # Right boundary (y-component)
     bcseff[81:2:120] .= 0 # Top boundary (x-component)
-    bcseff[82:2:120] = @. alpha * rho * g * (els.ycenter[idx["T"]]) # Top boundary (y-component)    
+    # bcseff[82:2:120] = @. alpha * rho * g * (els.ycenter[idx["T"]]) # Top boundary (y-component)    
+    bcseff[82:2:120] .= 0 # Top boundary (y-component)    
     bcseff[121:2:160] .= 0 # Left boundary (x-component)
     bcseff[122:2:160] .= 0 # Left boundary (y-component)
     bcseff *= -1 # This is neccesary for the right answer and is consistent with derivation
 
+
+
     # Solve BEM problem
     TH = [T_pB_qBRTL ; alpha .* H_pRTL_qBRTL]
-    @show cond(TH)
     Ueffparticular = inv(TH) * bcseff
-
-    # Check to see if bcseff are recovered
     
     # Interior displacements from Ueff
     UinteriorBRTL, SinteriorBRTL = constdispstress(slip2dispstress, x, y, els, BRTLidx, Ueffparticular[1:2:end], Ueffparticular[2:2:end], mu, nu)
     
-    # Interior displacements from modifed displacement BCs
-    Uinteriorubcs, Sinteriorubcs = constdispstress(slip2dispstress, x, y, els, BRTLidx, bcseff[1:2:end], bcseff[2:2:end], mu, nu)
- 
-    # Combined solutin
-    # U = @. UinteriorBRTL + Uinteriorubcs
-    # S = @. SinteriorBRTL + Sinteriorubcs
-    U = Uinteriorubcs
-    S = Sinteriorubcs
-
-    U = UinteriorBRTL
-    S = SinteriorBRTL
-
-
-
+    # Plot 6 panel interior evaluation
     plotfields(els, reshape(x, npts, npts), reshape(y, npts, npts), UinteriorBRTL, SinteriorBRTL, "Particular integral method")
 
     # Single quiver plot of interior displacements
@@ -170,11 +139,10 @@ function gravitysquareparticular()
     title("y boundary conditions")
 
     subplot(1, 2, 2)
-    # quiver(x, y, UinteriorBRTL[:, 1], UinteriorBRTL[:, 2])
-    quiver(x, y, U[:, 1], U[:, 2])
+    quiver(x, y, UinteriorBRTL[:, 1], UinteriorBRTL[:, 2])
     xlabel("x (m)")
     ylabel("y (m)")
     gca().set_aspect("equal")
-    title("internal displacements due to ueff")
+    title("internal displacements from ueff")
 end
 gravitysquareparticular()
