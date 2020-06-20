ba4a55ba994d0419ee470cc8df7a57479b1d35db
brendanjmeade <brendanjmeade@users.noreply.github.com>
2020-06-19 23:22:10 -0400

    Renaming of particular gravity files

--- examples/gravitysquareparticular.jl
+++ examples/gravitysquareparticular.jl
@@ -1,130 +1,23 @@
 using Revise
-using Statistics
-using LaTeXStrings
 using PyPlot
-using LinearAlgebra
 using Infiltrator
-using TriangleMesh
 using Bem2d
 
 
 """
-   kelvinUD(x, y, xoffset, yoffset, fx, fy, mu, nu)
+    gravityparticularfunctions()
 
-   Analytic point source Kelvin solution from Crouch and Starfield
-   section 4.2.
+From Pape and Bannerjee 1987
 """
-function kelvinUS(x, y, xoffset, yoffset, fx, fy, mu, nu)
-    x = @. x - xoffset
-    y = @. y - yoffset
-    Ukelvin = zeros(length(x), 2)
-    Skelvin = zeros(length(x), 3)
-    C = 1/(4*pi*(1-nu))
-    r = @. sqrt(x^2 + y^2)
-    g = @. -C * log(r)
-    gx = @. -C * x/(x^2+y^2)
-    gy = @. -C * y/(x^2+y^2)
-    gxy = @. C * 2*x*y/(x^2+y^2)^2
-    gxx = @. C * (x^2-y^2)/(x^2+y^2)^2
-    gyy = -gxx
-
-    Ukelvin[:, 1] = @. fx/(2*mu)*((3-4*nu)*g-x*gx) + fy/(2*mu)*(-y*gx)
-    Ukelvin[:, 2] = @. fx/(2*mu)*(-x*gy) + fy/(2*mu)*((3-4*nu)*g-y*gy)
-    Skelvin[:, 1] = @. fx*(2*(1-nu)*gx-x*gxx) + fy*(2*nu*gy-y*gxx)
-    Skelvin[:, 2] = @. fx*(2*nu*gx-x*gyy) + fy*(2*(1-nu)*gy-y*gyy)
-    Skelvin[:, 3] = @. fx*((1-2*nu)*gy-x*gxy) + fy*((1-2*nu)*gx-y*gxy)
-    return Ukelvin, Skelvin
-end
-
-
-"""
-    trianglearea(x1, y1, x2, y2, x3, y3)
-
-    It's just half the absolute value of the determinant: 
-    From https://math.stackexchange.com/questions/516219/finding-out-the-area-of-a-triangle-if-the-coordinates-of-the-three-vertices-are
-"""
-function trianglearea(x1, y1, x2, y2, x3, y3)
-    return abs(0.5*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2)))
-end
-
-
-"""
-    trimesh(nodes, connectivity, refinefactor)
-
-Calls triangle binary (https://www.cs.cmu.edu/~quake/triangle.html)
-Based on example at: https://github.com/konsim83/TriangleMesh.jl/blob/master/docs/src/man/examples.md
-"""
-function trimesh(nodes, connectivity, refinefactor)
-    nnodes = size(nodes)[1]
-    poly = Polygon_pslg(nnodes, 1, 2, nnodes, 0)
-    set_polygon_point!(poly, nodes)
-    set_polygon_segment!(poly, connectivity)
-    mesh = create_mesh(poly)
-    if refinefactor > 1
-        # mesh = refine(mesh, divide_cell_into=refinefactor, keep_edges=true)
-        mesh = refine(mesh, divide_cell_into=refinefactor, keep_edges=false)
-    end
-    return mesh
-end
-
-
-"""
-    plotmesh(mesh)
-
-    Plot mesh with shading, mesh edges, and centroids
-"""
-function plotmesh(mesh)
-    for i in 1:size(mesh.cell)[2]
-        x = mesh.point[1, mesh.cell[:, i]]
-        y = mesh.point[2, mesh.cell[:, i]]
-        fill(x, y, "lightgray", edgecolor="k", linewidth=0.25)
-    end
-    return nothing
-end
-
-
-
-"""
-    PUTK(els, obsidx, srcidx, mu, nu)
-
-Kernel relating body forces to boundary displacements and tractions.
-Uses Kelvin kernels.
-"""
-function PUTK(els, obsidx, mesh, mu, nu)
-    ntri = size(mesh.cell)[2]
-    nobs = length(obsidx)
-    Ku = zeros(2*nobs, 2*ntri)
-    Kt = zeros(2*nobs, 2*ntri)
-    _Ku = zeros(2, 2)
-    _Ks = zeros(3, 2)
-    _Kt = zeros(2, 2)
-    for iobs in 1:nobs # Should I loop over obs or src?  src is easier to write.
-        for isrc in 1:ntri
-            # Triangle areas and centroids
-            xtri = mesh.point[1, mesh.cell[:, isrc]]
-            ytri = mesh.point[2, mesh.cell[:, isrc]]
-            triarea = trianglearea(xtri[1], ytri[1], xtri[2], ytri[3], xtri[3], ytri[3])
-            tricentroid = [mean(xtri) mean(ytri)]
-
-            # Calculate displacement and stress kernels
-            _Ku[:, 1], _Ks[:, 1] = kelvinUS([els.xcenter[obsidx[iobs]]], [els.ycenter[obsidx[iobs]]], tricentroid[1], tricentroid[2], 1, 0, mu, nu)
-            _Ku[:, 2], _Ks[:, 2] = kelvinUS([els.xcenter[obsidx[iobs]]], [els.ycenter[obsidx[iobs]]], tricentroid[1], tricentroid[2], 0, 1, mu, nu)
-
-            # TODO This is a really stupid area scaling.  Need to upgrade to Guassian 4-point.
-            # Is there any good way to do this?
-            _Ku *= triarea
-            _Ks *= triarea
-
-            # Calculate traction kernels from stress kernels
-            _Kt[:, 1] = stress2trac(_Ks[:, 1], [els.xnormal[obsidx[iobs]] ; els.ynormal[obsidx[iobs]]])
-            _Kt[:, 2] = stress2trac(_Ks[:, 2], [els.xnormal[obsidx[iobs]] ; els.ynormal[obsidx[iobs]]])
-
-            # Insert 2x2 matrices into system matrices
-            Ku[2 * (iobs - 1) + 1:2 * (iobs - 1) + 2, 2 * (isrc - 1) + 1:2 * (isrc - 1) + 2] = _Ku
-            Kt[2 * (iobs - 1) + 1:2 * (iobs - 1) + 2, 2 * (isrc - 1) + 1:2 * (isrc - 1) + 2] = _Kt
-        end
-    end
-    return Ku, Kt
+function gravityparticularfunctions(x, y, g, rho, lambda, mu)
+    U = zeros(length(x), 2)
+    S = zeros(length(x), 3)
+    U[:, 1] = @. -lambda * rho * g / (4 * mu * (lambda + mu)) * x * y  # Pape and Banerjee (1987) equation (6a)
+    U[:, 2] = @. (rho * g) / (8 * mu * (lambda + mu)) * (lambda * x^2 + (lambda + 2 * mu) * y^2) # Pape and Banerjee (1987) equation (6b)
+    S[:, 1] .= 0 # Pape and Banerjee (1987) equation (6c)
+    S[:, 2] = @. rho * g * y  # Pape and Banerjee (1987) equation (6d)
+    S[:, 3] .= 0 # Pape and Banerjee (1987) equation (6e)
+    return U, S
 end
 
 
@@ -134,26 +27,32 @@ end
 Experiments with gravity body force.
 """
 function gravitysquareparticular()
+    # TODO: Quadratic elements
+    # TODO: Move particular solution to Bem2d.jl
+    # TODO: Move generation of modified BCs to function
+    # TODO: Create a convenciece function for simple elements (addelsez?)
+    # TODO: It's strange that the top of the model has to be at zero.  Can we generalize this?
+    # TODO: Rule of thumb for choosing precondtioner value (alpha)?
+    # TODO: This file should be renamed to gravitysquareparticular
+    # TODO: Remove all other gravitysquareparticular functions
+
     close("all")
+    alpha = 7e-8 # scalar preconditioner for traction terms
     fontsize = 20
     mu = 3e10
+    lambda = 3e10
     nu = 0.25
     rho = 2700
     g = 9.81
     nels = 20
     npts = 25
     L = 1e4
-    # x, y = obsgrid(-L+1, -L+1, L-1, L-1, npts)
-    x, y = obsgrid(-L+100, -L+100, L-100, L-100, npts)
+    offset = 1000
+    x, y = obsgrid(-L+offset, -2*L+offset, L-offset, 0-offset, npts) 
 
-    fx = 0.0
-    fy = g * rho
-
-    #! BEM geometry
+    # BEM geometry
     els = Elements(Int(1e5))
-    x1, y1, x2, y2 = discretizedline(-L, -L, L, -L, nels)
-    xbottom = x1
-    ybottom = y1
+    x1, y1, x2, y2 = discretizedline(-L, -2*L, L, -2*L, nels)
     for i in 1:length(x1)
         els.x1[els.endidx + i] = x1[i]
         els.y1[els.endidx + i] = y1[i]
@@ -163,9 +62,7 @@ function gravitysquareparticular()
     end
     standardize_elements!(els)
 
-    x1, y1, x2, y2 = discretizedline(L, -L, L, L, nels)
-    xright = x1
-    yright = y1
+    x1, y1, x2, y2 = discretizedline(L, -2*L, L, 0, nels)
     for i in 1:length(x1)
         els.x1[els.endidx + i] = x1[i]
         els.y1[els.endidx + i] = y1[i]
@@ -175,9 +72,7 @@ function gravitysquareparticular()
     end
     standardize_elements!(els)
 
-    x1, y1, x2, y2 = discretizedline(L, L, -L, L, nels)
-    xtop = x1
-    ytop = y1
+    x1, y1, x2, y2 = discretizedline(L, 0, -L, 0, nels)
     for i in 1:length(x1)
         els.x1[els.endidx + i] = x1[i]
         els.y1[els.endidx + i] = y1[i]
@@ -187,9 +82,7 @@ function gravitysquareparticular()
     end
     standardize_elements!(els)
 
-    x1, y1, x2, y2 = discretizedline(-L, L, -L, -L, nels)
-    xleft = x1
-    yleft = y1
+    x1, y1, x2, y2 = discretizedline(-L, 0, -L, -2*L, nels)
     for i in 1:length(x1)
         els.x1[els.endidx + i] = x1[i]
         els.y1[els.endidx + i] = y1[i]
@@ -198,38 +91,64 @@ function gravitysquareparticular()
         els.name[els.endidx + i] = "L"
     end
     standardize_elements!(els)
-    idx = getidxdict(els)
 
-    #! Kernels matrices (boundary -> boundary)
+    # Common indexing
+    idx = getidxdict(els)
     Bidx = idx["B"]
     RTLidx = [idx["R"] ; idx["T"]; idx["L"]]
     BRTLidx = collect(1:1:els.endidx)
+
+    # Constant kernel matrices
     T_pB_qBRTL, H_pB_qBRTL = PUTC(slip2dispstress, els, Bidx, BRTLidx, mu, nu)
     T_pRTL_qBRTL, H_pRTL_qBRTL = PUTC(slip2dispstress, els, RTLidx, BRTLidx, mu, nu)
 
-    #! Particular solution technique with modified boundary conditoions
-    alpha = 7e-8
-    Tall = [T_pB_qBRTL ; T_pRTL_qBRTL]
-    bcs = zeros(2 * 4 * nels)
-    tempbc = @. alpha * -rho * g * (10000 - els.ycenter[idx["L"]])
-    bcs[42:2:80] = tempbc
-    tempbc = @. alpha * -rho * g * (10000 - els.ycenter[idx["R"]])
-    bcs[122:2:160] = tempbc
+    # Trying a bottom BC only case
+    bcseff = zeros(2 * els.endidx)
+    UB, _ = gravityparticularfunctions(els.xcenter[idx["B"]], els.ycenter[idx["B"]], g, rho, lambda, mu)
+    bcseff[2*idx["B"].-1] = UB[:, 1] # Bottom boundary (x-component)
+    bcseff[2*idx["B"]] = UB[:, 2] # Bottom boundary (y-component)
+    bcseff[2*idx["R"].-1] .= 0 # Right boundary (x-component)
+    bcseff[2*idx["R"]] .= 0 # Right boundary (y-component)
+    bcseff[2*idx["T"].-1] .= 0 # Top boundary (x-component)
+    bcseff[2*idx["T"]] .= 0 # Top boundary (y-component)    
+    bcseff[2*idx["L"].-1] .= 0 # Left boundary (x-component)
+    bcseff[2*idx["L"]] .= 0 # Left boundary (y-component)
+    bcseff *= -1 # This is neccesary for the right answer and is consistent with derivation
+
+    # Solve BEM problem
     TH = [T_pB_qBRTL ; alpha .* H_pRTL_qBRTL]
-    Ueffparticular = inv(TH) * bcs
-    Uboundaryparticular = Tall * Ueffparticular
+    Ueffparticular = inv(TH) * bcseff
     
-    #! Interior displacements from boundaries (Ueff)
+    # Quadratic kernel matrices
+    # T_pB_qBRTL_Q, H_pB_qBRTL_Q = PUTQ(slip2dispstress, els, Bidx, BRTLidx, mu, nu)
+    # T_pRTL_qBRTL_Q, H_pRTL_qBRTL_Q = PUTQ(slip2dispstress, els, RTLidx, BRTLidx, mu, nu)
+    # bcsQ = zeros(6 * els.endidx)
+    # bcsQ[2*idx["B"].-1] = @. -lambda*rho*g * els.xcenter[idx["B"]]*els.ycenter[idx["B"]] / (4*mu*(lambda+mu)) # Bottom boundary (x-component)
+    # bcsQ[2*idx["B"]] = @. (rho*g) * (lambda*els.xcenter[idx["B"]]^2 + (lambda+2*mu)*els.ycenter[idx["B"]]^2) / (8*mu*(lambda+mu)) # Bottom boundary (y-component)
+    # bcsQ[2*idx["R"].-1] .= 0 # Right boundary (x-component)
+    # bcsQ[2*idx["R"]] .= 0 # Right boundary (y-component)
+    # bcsQ[2*idx["T"].-1] .= 0 # Top boundary (x-component)
+    # bcsQ[2*idx["T"]] .= 0 # Top boundary (y-component)    
+    # bcsQ[2*idx["L"].-1] .= 0 # Left boundary (x-component)
+    # bcsQ[2*idx["L"]] .= 0 # Left boundary (y-component)
+    # bcsQ *= -1 # This is neccesary for the right answer and is consistent with derivation
+    # TH = [T_pB_qBRTL_Q ; alpha .* H_pRTL_qBRTL_Q]
+    # UeffparticularQ = inv(TH) * bcsQ
+
+
+    # Complementary solution from Ueff
     UinteriorBRTL, SinteriorBRTL = constdispstress(slip2dispstress, x, y, els, BRTLidx, Ueffparticular[1:2:end], Ueffparticular[2:2:end], mu, nu)
-    plotfields(els, reshape(x, npts, npts), reshape(y, npts, npts), UinteriorBRTL, SinteriorBRTL, "Particular integral method")
+    plotfields(els, reshape(x, npts, npts), reshape(y, npts, npts), UinteriorBRTL, SinteriorBRTL, "ueff solution")
+
+    # Particular solution
+    Uinteriorparticular, Sinteriorparticular = gravityparticularfunctions(x, y, g, rho, lambda, mu)
+    plotfields(els, reshape(x, npts, npts), reshape(y, npts, npts), Uinteriorparticular, Sinteriorparticular, "Particular solution")
     
-    #! Single quiver plot of interior displacements
-    figure(figsize=(8, 8))
-    quiver(x, y, UinteriorBRTL[:, 1], UinteriorBRTL[:, 2])
-    xlabel("x (m)", fontsize=fontsize)
-    ylabel("y (m)", fontsize=fontsize)
-    gca().set_aspect("equal")
-    gca().tick_params(labelsize=fontsize)
-    title("internal displacements", fontsize=fontsize)
+    # Combine solutions following equations 14 in Pape and Bannerjee
+    U = @. UinteriorBRTL + Uinteriorparticular
+    S = @. SinteriorBRTL + Sinteriorparticular
+
+    # Plot total solution particular + complementary
+    plotfields(els, reshape(x, npts, npts), reshape(y, npts, npts), U, S, "uc + up")
 end
 gravitysquareparticular()
