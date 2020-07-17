1416745cc0721e34c60410c7fe48a4013f957ee1
brendanjmeade <brendanjmeade@users.noreply.github.com>
2020-07-16 21:07:32 -0400

    Now includes a nice vector plot of interal displacments.

--- examples/gravitysquareparticular.jl
+++ examples/gravitysquareparticular.jl
@@ -83,21 +83,30 @@ function gravitysquareparticular()
     Ueffparticular = inv(TH) * bcs
 
     # Evaluate and plot interior solution
-    # Uinteriorcomplementary, Sinteriorcomplementary = quaddispstress(slip2dispstress, x, y, els, bcidxall, quadstack(Ueffparticular[1:2:end]), quadstack(Ueffparticular[2:2:end]), mu, nu)
-    # Uinteriorparticular, Sinteriorparticular = gravityparticularfunctions(x, y, g, rho, lambda, mu)
-    # U = @. Uinteriorcomplementary + Uinteriorparticular
-    # S = @. Sinteriorcomplementary + Sinteriorparticular
-    # plotfields(els, reshape(x, npts, npts), reshape(y, npts, npts), Uinteriorcomplementary, Sinteriorcomplementary, "Complementary solution")
-    # plotfields(els, reshape(x, npts, npts), reshape(y, npts, npts), Uinteriorparticular, Sinteriorparticular, "Particular solution")
-    # plotfields(els, reshape(x, npts, npts), reshape(y, npts, npts), U, S, "Complementary + Particular solutions")
+    Uinteriorcomplementary, Sinteriorcomplementary = quaddispstress(slip2dispstress, x, y, els, bcidxall, quadstack(Ueffparticular[1:2:end]), quadstack(Ueffparticular[2:2:end]), mu, nu)
+    Uinteriorparticular, Sinteriorparticular = gravityparticularfunctions(x, y, g, rho, lambda, mu)
+    U = @. Uinteriorcomplementary + Uinteriorparticular
+    S = @. Sinteriorcomplementary + Sinteriorparticular
+    plotfields(els, reshape(x, npts, npts), reshape(y, npts, npts), Uinteriorcomplementary, Sinteriorcomplementary, "Complementary solution")
+    plotfields(els, reshape(x, npts, npts), reshape(y, npts, npts), Uinteriorparticular, Sinteriorparticular, "Particular solution")
+    plotfields(els, reshape(x, npts, npts), reshape(y, npts, npts), U, S, "Complementary + Particular solutions")
 
     # Interior point solution for nice vector plot
-    npts = 30
+    npts = 10
+    offset = 500
     x, y = obsgrid(-L+offset, -2*L+offset, L-offset, 0-offset, npts)
     Uinteriorcomplementary, Sinteriorcomplementary = quaddispstress(slip2dispstress, x, y, els, bcidxall, quadstack(Ueffparticular[1:2:end]), quadstack(Ueffparticular[2:2:end]), mu, nu)
     Uinteriorparticular, Sinteriorparticular = gravityparticularfunctions(x, y, g, rho, lambda, mu)
     U = @. Uinteriorcomplementary + Uinteriorparticular
     S = @. Sinteriorcomplementary + Sinteriorparticular
-    
+
+    # Plot of interior points
+    fig, ax = plt.subplots()
+    fill([-10000, 10000, 10000, -10000], [-20000, -20000, 0, 0] .+ 20000, color="lightgray", zorder=1)
+    q = quiver(x[:], y[:].+20000, U[:, 1], U[:, 2], units="width", color="cyan", edgecolor="k", linewidths=0.25, zorder=10)
+    quiverkey(q, X=0.0, Y=100, U=10000, label="Quiver key, length = 10", labelpos="E")
+    xlabel("x (m)")
+    ylabel("y (m)")
+    gca().set_aspect("equal")
 end
 gravitysquareparticular()
