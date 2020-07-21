67e3c5494385ed210ce2d96358ddf68636578e30
brendanjmeade <brendanjmeade@users.noreply.github.com>
2020-07-21 10:44:54 -0400

    Generalized to fault with higher discretization.  Implmented alternative BEM approach with fault slip embedded in the ueff calculation.

--- examples/dislocationinabox.jl
+++ examples/dislocationinabox.jl
@@ -13,51 +13,92 @@ function dislocationinabox()
     close("all")
     mu = 30e9
     nu = 0.25
+    alpha = 1e-7 # scalar preconditioner
 
-    # Elment geometries and data structures for half space approximation
+    # Element geometries and data structures for half space approximation
     els = Elements(Int(1e5))
-    x1, y1, x2, y2 = discretizedline(-50e3, 0, 50e3, 0, 50) # Free surface
-    addelsez!(els, x1, y1, x2, y2, "surf")
-    x1, y1, x2, y2 = discretizedline(-10e3, -10e3, 0, 0, 1) # 45 degree dipping fault
+    nfault = 20
+    x1, y1, x2, y2 = discretizedline(-10e3, -10e3, 0, 0, nfault) # 45 degree dipping fault
     addelsez!(els, x1, y1, x2, y2, "fault")
+    nsurf = 50
+    x1, y1, x2, y2 = discretizedline(-50e3, 0, 50e3, 0, nsurf) # Free surface
+    addelsez!(els, x1, y1, x2, y2, "surf")
     idx = getidxdict(els)
 
     # Solve the BEM problem
     _, H_surf_fault = PUTC(slip2dispstress, els, idx["surf"], idx["fault"], mu, nu)
     _, H_surf_surf = PUTC(slip2dispstress, els, idx["surf"], idx["surf"], mu, nu)
-    faultslip = sqrt(2) / 2 * [1 ; 1]
+    faultslip = sqrt(2) / 2 * ones(2 * nfault)
+    @infiltrate
     Ueff = inv(H_surf_surf) * (H_surf_fault * faultslip)
-    
-    # Evaluate in a volume
-    Ufault, _ = constdispstress(slip2dispstress, els.xcenter[idx["surf"]], els.ycenter[idx["surf"]], els, idx["fault"], 1, 1, mu, nu)
-    Usurf, _ = constdispstress(slip2dispstress, els.xcenter[idx["surf"]], els.ycenter[idx["surf"]], els, idx["surf"], Ueff[1:2:end], Ueff[2:2:end], mu, nu)
+
+    # Forward evaluation
+    Ufault, _ = constdispstress(
+        slip2dispstress,
+        els.xcenter[idx["surf"]],
+        els.ycenter[idx["surf"]],
+        els,
+        idx["fault"],
+        ones(nfault),
+        ones(nfault),
+        mu,
+        nu,
+    )
+    Usurf, _ = constdispstress(
+        slip2dispstress,
+        els.xcenter[idx["surf"]],
+        els.ycenter[idx["surf"]],
+        els,
+        idx["surf"],
+        Ueff[1:2:end],
+        Ueff[2:2:end],
+        mu,
+        nu,
+    )
     Utotal = @. Ufault - Usurf
-    
+
+    # Alternative BEM solution
+    T_fault_all, _ =
+        PUTC(slip2dispstress, els, idx["fault"], collect(1:1:els.endidx), mu, nu)
+    _, H_surf_all = PUTC(slip2dispstress, els, idx["surf"], collect(1:1:els.endidx), mu, nu)
+    bcs = zeros(2 * els.endidx)
+    bcs[1:2*nfault] .= -1
+    Ueffalt = inv([T_fault_all; H_surf_all]) * bcs
+
+    # Alternative forward evaluation
+    Ualt, _ = constdispstress(
+        slip2dispstress,
+        els.xcenter[idx["surf"]],
+        els.ycenter[idx["surf"]],
+        els,
+        collect(1:1:els.endidx),
+        Ueffalt[1:2:end],
+        Ueffalt[2:2:end],
+        mu,
+        nu,
+    )
+
     # Plot surface displacements
     fontsize = 20
     markersize = 12
     linewidth = 2.0
     figure(figsize = (12, 12))
-    ax = subplot(2, 1, 1)
-    plot(els.xcenter[idx["surf"]], Ueff[1:2:end], "b.", label="Ueff")
-    plot(els.xcenter[idx["surf"]], Ufault[:, 1], "r+", label="fault only")
-    plot(els.xcenter[idx["surf"]], Usurf[:, 1], "rx", label="fault only")
-    plot(els.xcenter[idx["surf"]], Utotal[:, 1], "r.", label="total")
-    
-    gca().set_xlim([-50e3, 50e3])
-    gca().set_ylim([-1.0, 1.0])
-    legend(fontsize=fontsize)
-    ax.tick_params("both", labelsize = fontsize)
-    ylabel(L"$u_x$ (m)", fontsize=fontsize)
+    ax = subplot(2, 1, 1) # x-displacements
+    plot(els.xcenter[idx["surf"]], Ueff[1:2:end], "b.", label = "Ueff")
+    plot(els.xcenter[idx["surf"]], Ueffalt[2*nfault+1:2:end], "g.", label = "Ueffalt")
+    plot(els.xcenter[idx["surf"]], Ualt[1:2:end], "c.", label = "Ualt")
+    plot(els.xcenter[idx["surf"]], Utotal[:, 1], "r.", label = "total")
+    legend()
+    xlabel(L"$x$ (m)")
+    ylabel(L"$u_x$ (m)")
 
-    ax = subplot(2, 1, 2)
-    plot(els.xcenter[idx["surf"]], Ueff[2:2:end], "b.", label="halfspace")
-    gca().set_xlim([-50e3, 50e3])
-    gca().set_ylim([-1.0, 1.0])
-    legend(fontsize=fontsize)
-    ax.tick_params("both", labelsize = fontsize)
-    xlabel(L"$x$ (m)", fontsize=fontsize)
-    ylabel(L"$u_y$ (m)", fontsize=fontsize)
-    show()
+    ax = subplot(2, 1, 2) # y-displacements
+    plot(els.xcenter[idx["surf"]], Ueff[2:2:end], "b.", label = "Ueff")
+    plot(els.xcenter[idx["surf"]], Ueffalt[2*nfault+2:2:end], "g.", label = "Ueffalt")
+    plot(els.xcenter[idx["surf"]], Ualt[2:2:end], "c.", label = "Ualt")
+    plot(els.xcenter[idx["surf"]], Utotal[:, 2], "r.", label = "total")
+    legend()
+    xlabel(L"$x$ (m)")
+    ylabel(L"$u_y$ (m)")
 end
 dislocationinabox()
