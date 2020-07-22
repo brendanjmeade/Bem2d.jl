8f32448f1a99aebba34e150ef28cb87ef3b52ddb
brendanjmeade <brendanjmeade@gmail.com>
2020-07-21 22:44:32 -0400

    Now with higher resolution Alternative and islinux guard for okada_wrapper.  Now Alternative appears to agree better with Okada specifically at the free surface where the stress residuals are smaller.

--- examples/dislocationinabox.jl
+++ examples/dislocationinabox.jl
@@ -1,6 +1,7 @@
 using Revise
 using PyPlot
 using PyCall
+using Statistics
 using LinearAlgebra
 using Infiltrator
 using Bem2d
@@ -22,10 +23,10 @@ function dislocationinabox()
 
     # Element geometries and data structures for half space approximation
     els = Elements(Int(1e5))
-    nfault = 20
+    nfault = 1
     x1, y1, x2, y2 = discretizedline(-10e3, -10e3, 0, 0, nfault) # 45 degree dipping fault
     addelsez!(els, x1, y1, x2, y2, "fault")
-    nsurf = 200
+    nsurf = 800
     x1, y1, x2, y2 = discretizedline(-100e3, 0, 100e3, 0, nsurf) # Free surface
     addelsez!(els, x1, y1, x2, y2, "surf")
     idx = getidxdict(els)
@@ -54,7 +55,7 @@ function dislocationinabox()
     _, H_surf_all = PUTC(slip2dispstress, els, idx["surf"],
                          collect(1:1:els.endidx), mu, nu)
     bcs = zeros(2 * els.endidx)
-    bcs[1:2*nfault] .= 1 * sqrt(2) / 2
+    bcs[1:2*nfault] .= 0.5
     Ueffalt = inv([T_fault_all; alpha .* H_surf_all]) * bcs
     
     # Alternative forward evaluation
@@ -62,43 +63,49 @@ function dislocationinabox()
         collect(1:1:els.endidx), Ueffalt[1:2:end], Ueffalt[2:2:end], mu, nu)
     plotfields(els, reshape(xgrid, npts, npts), reshape(ygrid, npts, npts),
                Ualt, Salt, "Alternative")
-               
-    # Okada solution
-    ow = pyimport("okada_wrapper") # from okada_wrapper import dc3dwrapper
-    Uokada = zeros(length(xgrid), 2)
-    Sokada = zeros(length(xgrid), 3)
-    # dispokadats = zeros(length(x), 2)
-    # stressokadats = zeros(length(x), 3)
-    for i in 1:length(xgrid)
-        # Strike-slip
-        _, u, gradu = ow.dc3dwrapper((2.0/3.0),
-                                     [0.0, xgrid[i]+5e3, ygrid[i]],
-                                     5e3,
-                                     45,
-                                     [-1000000, 1000000],
-                                     [-5e3*sqrt(2), 5e3*sqrt(2)],
-                                     [0.0, 1.0, 0.0])
-        strain = @. 0.5 * (gradu' + gradu)
-        stress = mu*LinearAlgebra.I(3)*tr(strain) + 2.0*mu*strain
-        Uokada[i, 1] = u[2]
-        Uokada[i, 2] = u[3]
-        Sokada[i, 1] = stress[2, 2]
-        Sokada[i, 2] = stress[3, 3]
-        Sokada[i, 3] = stress[2, 3]
 
-        # # Tensile-slip
-        # _, u, gradu = ow.dc3dwrapper((2.0/3.0), [0.0, x[i], y[i]-deep], 0.0 + deep, 0.0, [-1000000, 1000000], [-0.5, 0.5], [0.0, 0.0, 1.0])
-        # strain = @. 0.5 * (gradu' + gradu)
-        # stress = mu*LinearAlgebra.I(3)*tr(strain) + 2.0*mu*strain
-        # dispokadats[i, 1] = u[2]
-        # dispokadats[i, 2] = u[3]
-        # stressokadats[i, 1] = stress[2, 2]
-        # stressokadats[i, 2] = stress[3, 3]
-        # stressokadats[i, 3] = stress[2, 3]
-    end
+    # Plot difference
+    Udiff = @. Ualt - Utotal
+    Sdiff = @. Salt - Stotal
     plotfields(els, reshape(xgrid, npts, npts), reshape(ygrid, npts, npts),
-               Uokada, Sokada, "Okada")
+               Udiff, Sdiff, "Alternative - Total")
 
+    # Okada comparision if on Linux (I can only install okada wrapper there)
+    if Sys.islinux()
+        # Okada solution
+        ow = pyimport("okada_wrapper") # from okada_wrapper import dc3dwrapper
+        Uokada = zeros(length(xgrid), 2)
+        Sokada = zeros(length(xgrid), 3)
+        for i in 1:length(xgrid)
+            _, u, gradu = ow.dc3dwrapper((2.0/3.0),
+                                         [0.0, xgrid[i]+5e3, ygrid[i]],
+                                         5e3,
+                                         45,
+                                         [-1000000, 1000000],
+                                         [-5e3*sqrt(2), 5e3*sqrt(2)],
+                                         [0.0, 1.0, 0.0])
+            strain = @. 0.5 * (gradu' + gradu)
+            stress = mu*LinearAlgebra.I(3)*tr(strain) + 2.0*mu*strain
+            Uokada[i, 1] = u[2]
+            Uokada[i, 2] = u[3]
+            Sokada[i, 1] = stress[2, 2]
+            Sokada[i, 2] = stress[3, 3]
+            Sokada[i, 3] = stress[2, 3]
+        end
+        plotfields(els, reshape(xgrid, npts, npts), reshape(ygrid, npts, npts),
+                   Uokada, Sokada, "Okada")
+
+        # Let's look at some Residuals
+        Utotalres = @. Uokada - Utotal
+        Stotalres = @. Sokada - Stotal
+        Ualtres = @. Uokada - Ualt
+        Saltres = @. Sokada - Salt
+        plotfields(els, reshape(xgrid, npts, npts), reshape(ygrid, npts, npts),
+               Utotalres, Stotalres, "Okada - Total")
+        plotfields(els, reshape(xgrid, npts, npts), reshape(ygrid, npts, npts),
+               Ualtres, Saltres, "Okada - Alternative")
+    end
+    
     
     # # Plot surface displacements
     # fontsize = 20
