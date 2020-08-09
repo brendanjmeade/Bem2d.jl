2fa787e891345c45eec6a236d56ca16c108aeb09
brendanjmeade <brendanjmeade@users.noreply.github.com>
2020-08-08 23:18:03 -0400

    Working on calculating and visualizating internal displacments.
    Just a check point.  Quiver plots on same mesh are working.

--- examples/weldedcircle.jl
+++ examples/weldedcircle.jl
@@ -74,7 +74,7 @@ function discmaterialother()
     PLOTGEOMETRY = true
     mu = 3e10
     nu = 0.25
-    npts = 50
+    npts = 30
     offset = 100 # meters
     
     # Element geometries and data structures for the homogeneous circle case
@@ -98,10 +98,9 @@ function discmaterialother()
     xgrid1, ygrid1 = obsgrid(-r, -r, r, r, npts)
     U1, S1 = constdispstress(slip2dispstress, xgrid1, ygrid1, els1, 1:els1.endidx,
                              Ueff1[1:2:end], Ueff1[2:2:end], mu, nu)
-    plotfields(els1, reshape(xgrid1, npts, npts), reshape(ygrid1, npts, npts),
-               U1, S1, "homogeneous circle")
+    # plotfields(els1, reshape(xgrid1, npts, npts), reshape(ygrid1, npts, npts),
+    #            U1, S1, "homogeneous circle")
   
-
     # Element geometries and data structures for the welded circle case
     els2 = Elements(Int(1e5))
     nels = 20
@@ -115,9 +114,8 @@ function discmaterialother()
     x1, y1, x2, y2 = discretizedline(-r, 0, r, 0, nels) # Peculiar
     addelsez!(els2, x2, y2, x1, y1, "midB")
     idx2 = getidxdict(els2)
-    PLOTGEOMETRY && plotgeometry(els2, "Welded circle boundaries and normals")
+    # PLOTGEOMETRY && plotgeometry(els2, "Welded circle boundaries and normals")
     
-    # Interface ueff = [T, midT, B, midB]
     TH = zeros(2 * els2.endidx, 2 * els2.endidx)
     T_midT_TmidT, H_midT_TmidT = PUTC(slip2dispstress, els2, idx2["midT"], [idx2["T"]; idx2["midT"]], mu, nu)
     T_midB_BmidB, H_midB_BmidB = PUTC(slip2dispstress, els2, idx2["midB"], [idx2["B"]; idx2["midB"]], mu, nu)
@@ -138,28 +136,54 @@ function discmaterialother()
     bcs2[2*51] = 1.0
     
     Ueff2 = TH \ bcs2
-    plotbcsUeff(els2, bcs2, Ueff2, "welded circle")
-
     UeffT2 = Ueff2[1:80]
     UeffB2 = Ueff2[81:160]
+    # plotbcsUeff(els2, bcs2, Ueff2, "welded circle")
 
-    xgridT2, ygridT2 = obsgrid(-r, 1000, r, r, npts)
-    # U1, S1 = constdispstress(slip2dispstress, xgrid1, ygrid1, els1, 1:els1.endidx,
-    #                          Ueff1[1:2:end], Ueff1[2:2:end], mu, nu)
-
-    UT2, ST2 = constdispstress(slip2dispstress, xgridT2, ygridT2, els2,
+    # Internal evaluation
+    Tidx = findall(x -> x > 0, ygrid1)
+    Bidx = findall(x -> x < 0, ygrid1)
+    Tx = xgrid1[Tidx]
+    Ty = ygrid1[Tidx]
+    Bx = xgrid1[Bidx]
+    By = ygrid1[Bidx]
+    UT2, ST2 = constdispstress(slip2dispstress, Tx, Ty, els2,
                                [idx2["T"]; idx2["midT"]],
-                               UeffT2[1:2:end],
-                               UeffT2[2:2:end], mu, nu)
-    plotfields(els1, reshape(xgridT2, npts, npts), reshape(ygridT2, npts, npts),
-               UT2, ST2, "homogeneous circle (top)")
+                               UeffT2[1:2:end], UeffT2[2:2:end], mu, nu)
+    UB2, SB2 = constdispstress(slip2dispstress, Bx, By, els2,
+                               [idx2["B"]; idx2["midB"]],
+                               UeffB2[1:2:end], UeffB2[2:2:end], mu, nu)
+
     
-    # UT2, ST2 = constdispstress(slip2dispstress, xgrid1, ygrid1, els2,
-    #                            1:els2.endidx,
-    #                            Ueff2[1:2:end],
-    #                            Ueff2[2:2:end], mu, nu)
-    # plotfields(els1, reshape(xgrid1, npts, npts), reshape(ygrid1, npts, npts),
-    #            UT2, ST2, "homogeneous circle (top)")
-    return
+    # xgridT2, ygridT2 = obsgrid(-r, 344.827, r, r, npts)
+    # UT2, ST2 = constdispstress(slip2dispstress, xgridT2, ygridT2, els2,
+    #                            [idx2["T"]; idx2["midT"]],
+    #                            UeffT2[1:2:end], UeffT2[2:2:end], mu, nu)
+    # # plotfields(els2, reshape(xgridT2, npts, npts), reshape(ygridT2, npts, npts),
+    # #            UT2, ST2, "homogeneous circle (top)")
+
+    # xgridB2, ygridB2 = obsgrid(-r, -r, r, -344.827, npts)
+    # UB2, SB2 = constdispstress(slip2dispstress, xgridB2, ygridB2, els2,
+    #                            [idx2["B"]; idx2["midB"]],
+    #                            UeffB2[1:2:end], UeffB2[2:2:end], mu, nu)
+    # plotfields(els2, reshape(xgridB2, npts, npts), reshape(ygridB2, npts, npts),
+    #            UB2, SB2, "homogeneous circle (bottom)")
+
+    # Quick and dirty reshaping
+    figure(figsize=(20, 10))
+    subplot(1, 2, 1)
+    quiver(xgrid1[:], ygrid1[:], U1[:, 1], U1[:, 2],
+           width=1e-3, scale=1e-6)
+    gca().set_aspect("equal")
+
+    subplot(1, 2, 2)
+    quiver(Tx[:], Ty[:], UT2[:, 1], UT2[:, 2],
+           width=1e-3, scale=1e-6)
+    quiver(Bx[:], By[:], UB2[:, 1], UB2[:, 2],
+           width=1e-3, scale=1e-6)    
+    gca().set_aspect("equal")
+
+    @infiltrate
+    
 end
 discmaterialother()
