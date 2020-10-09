28138ff806127091557667c22191b526ccd6ae1d
brendanjmeade <brendanjmeade@users.noreply.github.com>
2020-10-08 22:43:37 -0400

    Cleaning up.  Need to move everything to functions

--- examples/femtest2d.jl
+++ examples/femtest2d.jl
@@ -3,23 +3,19 @@ using PyPlot
 
 mu = 1
 rho = 1
-# gamma = 0.4*delta^2
-# beta = 1.25
 lambda = 1
-# g = gamma
 g = 9.81
-
 
 # Create mesh and define function space
 mesh = UnitSquareMesh(20, 20)
 V = VectorFunctionSpace(mesh, "P", 1)
 c = Constant((0, 0))
-bc = DirichletBC(V, c, "on_boundary && x[1]<1E-14")
+bc = DirichletBC(V, c, "on_boundary && x[1]<1E-14") # What BCs are bing set???
 tol = 1E-14
 
 # Define strain
 function epsilon(u)
-     0.5*(nabla_grad(u)+Transpose(nabla_grad(u)))
+     0.5 * (nabla_grad(u) + Transpose(nabla_grad(u)))
 end
 
 # Define stress
@@ -33,13 +29,13 @@ v = TestFunction(V)
 f = Constant((0, -rho*g)) # Vector of uniform body force
 T = Constant((0, 0))
 a = inner(sigma(u), epsilon(v))*dx
-L = dot(f, v)*dx + dot(T, v)*ds
+L = dot(f, v) * dx + dot(T, v) * ds
 u = FeFunction(V)
 lvsolve(a, L, u, bc)
 
-s = sigma(u) - (1.0/3.0) * tr(sigma(u)) * Identity(d)
+# s = sigma(u) - (1.0/3.0) * tr(sigma(u)) * Identity(d)
 V = FunctionSpace(mesh, "P", 1)
 u_magnitude = sqrt(dot(u, u))
 u_magnitude = project(u_magnitude, V)
-
-FEniCS.Plot(u_magnitude)
+cmap = FEniCS.Plot(u_magnitude)
+colorbar(cmap)
