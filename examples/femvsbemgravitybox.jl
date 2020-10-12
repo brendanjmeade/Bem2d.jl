46023f91f7d8accea338017151bce0fac59da4a1
brendanjmeade <brendanjmeade@users.noreply.github.com>
2020-10-11 23:46:33 -0400

    FEM and BEM displacment magnitude same figure.
    Still can't figure out how to extract the FEM solution from the
    pyobject.  I really want this so that I can calculate residuals from
    BEM solution.

--- examples/femvsbemgravitybox.jl
+++ examples/femvsbemgravitybox.jl
@@ -1,5 +1,7 @@
+using Revise
 using FEniCS
 using PyPlot
+using Bem2d
 
 # Calculate strain
 function strain(u)
@@ -18,13 +20,16 @@ function femvsbemgravitybox()
     lambda = mu
     rho = 2700
     g = 9.81
-    L = 10e3
+    width = 10e3
 
+    ###
+    ### FEM solution
+    ###
     # Create mesh and define function space
-    mesh = RectangleMesh(Point((-L, 0)), Point((L, 2 * L)), 100, 100)
+    mesh = RectangleMesh(Point((-width, 0)), Point((width, 2 * width)), 100, 100)
     V = VectorFunctionSpace(mesh, "P", 1)
     bc = DirichletBC(V, Constant((0, 0)), "on_boundary && x[1]<1E-14") # What BCs are bing set???
-
+    
     # Solve
     u = TrialFunction(V)
     d = geometric_dimension(u) # space dimension
@@ -41,7 +46,69 @@ function femvsbemgravitybox()
     u_magnitude = sqrt(dot(u, u))
     u_magnitude = project(u_magnitude, V)
     close("all")
+    figure(figsize=(10, 5))
+    subplot(1, 2, 1)
     plothandle = FEniCS.Plot(u_magnitude)
     colorbar(plothandle)
+    title("FEM solution")
+
+
+    ###
+    ### BEM solution
+    ### 
+    alpha = 7e-8 # scalar preconditioner for traction terms
+    nu = 0.25
+    nels = 20
+    npts = 100
+    offset = 1
+    x, y = obsgrid(-width+offset, -2*width+offset, width-offset, 0-offset, npts)
+
+    # Define BEM geometry
+    els = Elements(Int(1e5))
+    x1, y1, x2, y2 = discretizedline(-width, -2*width, width, -2*width, nels) # Bottom
+    addelsez!(els, x1, y1, x2, y2, "B")
+    x1, y1, x2, y2 = discretizedline(width, -2*width, width, 0, nels) # Right hand side
+    addelsez!(els, x1, y1, x2, y2, "R")
+    x1, y1, x2, y2 = discretizedline(width, 0, -width, 0, nels) # Top
+    addelsez!(els, x1, y1, x2, y2, "T")
+    x1, y1, x2, y2 = discretizedline(-width, 0, -width, -2*width, nels) # Left hand side
+    addelsez!(els, x1, y1, x2, y2, "L")
+    
+    # Common indexing
+    idx = getidxdict(els) # Should this return "all" - YES TODO
+    bcidxU = idx["B"] # Boundaries with *displacement* BCs
+    bcidxT = [idx["R"] ; idx["T"]; idx["L"]] # Boundaries with *traction* BCs
+    bcidxall = collect(1:1:els.endidx) # All boundaries
+
+    # Gravity square problem with quadratic elements
+    T_pU_qall, _ = PUTQ(slip2dispstress, els, bcidxU, bcidxall, mu, nu)
+    _, H_pT_qall = PUTQ(slip2dispstress, els, bcidxT, bcidxall, mu, nu)
+    TH = [T_pU_qall ; alpha .* H_pT_qall] # Assemble combined linear operator
+
+    # Particular solution and effective boundary conditions
+    xnodes = transpose(els.xnodes[idx["B"], :])[:]
+    ynodes = transpose(els.ynodes[idx["B"], :])[:]
+    UB, _ = gravityparticularfunctions(xnodes, ynodes, g, rho, lambda, mu)    
+    bcs = zeros(6 * els.endidx)
+    bcs[1:2:6*nels] = UB[:, 1] # Bottom boundary (x-component)
+    bcs[2:2:6*nels] = UB[:, 2] # Bottom boundary (y-component)
+    bcs *= -1 # This is neccesary for the right answer and is consistent with derivation
+    
+    # BEM solve to get particular solution
+    Ueffparticular = inv(TH) * bcs
+
+    Uinteriorcomplementary, Sinteriorcomplementary = quaddispstress(slip2dispstress, x, y, els, bcidxall, quadstack(Ueffparticular[1:2:end]), quadstack(Ueffparticular[2:2:end]), mu, nu)
+    Uinteriorparticular, Sinteriorparticular = gravityparticularfunctions(x, y, g, rho, lambda, mu)
+    U = @. Uinteriorcomplementary + Uinteriorparticular
+    S = @. Sinteriorcomplementary + Sinteriorparticular
+    Umag = sqrt.(U[:, 1].^2 + U[:, 2].^2)
+    
+    subplot(1, 2, 2)
+    contourf(reshape(x, npts, npts), reshape(y, npts, npts).+2*width, reshape(Umag, npts, npts), 50)
+    colorbar()
+    gca().set_xlim([-width, width])
+    gca().set_ylim([0, 2*width])
+    gca().set_aspect("equal")
+    title("BEM solution")
 end
 femvsbemgravitybox()
