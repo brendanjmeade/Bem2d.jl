de01bb79bc1f2c40c86e73f677a50d17997347b0
brendanjmeade <brendanjmeade@users.noreply.github.com>
2020-11-01 22:59:35 -0500

    Added partiular solution visualziation

--- examples/debugbox.jl
+++ examples/debugbox.jl
@@ -5,6 +5,43 @@ using LinearAlgebra
 using Bem2d
 
 
+"""
+    plotparticular(g, rho, lambda, mu)
+
+Plot fundamental particular fields
+"""
+function plotparticular(g, rho, lambda, mu)
+    npts = 100
+    x, y = obsgrid(-20000, -20000, 20000, 20000, npts) 
+    Up, Sp = gravityparticularfunctions(x, y, g, rho, lambda, mu)
+    figure(figsize=(20, 10))
+    subplot(2, 3, 1)
+    contourf(reshape(x, npts, npts), reshape(y, npts, npts), reshape(Up[:, 1], npts, npts), 50)
+    colorbar()
+    gca().set_aspect("equal")
+    title("ux")
+    subplot(2, 3, 2)
+    contourf(reshape(x, npts, npts), reshape(y, npts, npts), reshape(Up[:, 2], npts, npts), 50)
+    colorbar()
+    gca().set_aspect("equal")
+    title("uy")
+    subplot(2, 3, 4)
+    contourf(reshape(x, npts, npts), reshape(y, npts, npts), reshape(Sp[:, 1], npts, npts), 50)
+    colorbar()
+    gca().set_aspect("equal")
+    title("Sxx")
+    subplot(2, 3, 5)
+    contourf(reshape(x, npts, npts), reshape(y, npts, npts), reshape(Sp[:, 2], npts, npts), 50)
+    colorbar()
+    gca().set_aspect("equal")
+    title("Syy")
+    subplot(2, 3, 6)
+    contourf(reshape(x, npts, npts), reshape(y, npts, npts), reshape(Sp[:, 3], npts, npts), 50)
+    colorbar()
+    gca().set_aspect("equal")
+    title("Sxy")
+end
+
 
 """
     gravitysquareparticular()
@@ -12,12 +49,12 @@ using Bem2d
 Experiments with gravity body force.
 """
 function gravitysquareparticular()
-    # TODO: Move particular solution to Bem2d.jl
     # TODO: It's strange that the top of the model has to be at zero.  Can we generalize this?
     # TODO: Rule of thumb for choosing precondtioner value (alpha)?
     # TODO: Make a version of gravityparticularfunctions() for BC generation
 
     close("all")
+
     alpha = 7e-8 # scalar preconditioner for traction terms
     fontsize = 20
     mu = 3e10
@@ -29,7 +66,10 @@ function gravitysquareparticular()
     npts = 100
     L = 1e4
     offset = 10
-    x, y = obsgrid(-L+offset, -2*L+offset, L-offset, 0-offset, npts) 
+    # x, y = obsgrid(-L+offset, -2*L+offset, L-offset, 0-offset, npts) 
+
+    plotparticular(g, rho, lambda, mu)
+    return
 
     # Define BEM geometry
     els = Elements(Int(1e5))
@@ -86,16 +126,28 @@ function gravitysquareparticular()
     x, y = obsgrid(-L+offset, 0, L-offset, 2*L-offset, npts)
 
     # Define BEM geometry
+    # elsbox = Elements(Int(1e5))
+    # x1, y1, x2, y2 = discretizedline(-L, 0, L, 0, nels) # Bottom
+    # addelsez!(elsbox, x1, y1, x2, y2, "B")
+    # x1, y1, x2, y2 = discretizedline(L, 0, L, 2*L, nels) # Right hand side
+    # addelsez!(elsbox, x1, y1, x2, y2, "R")
+    # x1, y1, x2, y2 = discretizedline(L, 2*L, -L, 2*L, nels) # Top
+    # addelsez!(elsbox, x1, y1, x2, y2, "T")
+    # x1, y1, x2, y2 = discretizedline(-L, 2*L, -L, 0, nels) # Left hand side
+    # addelsez!(elsbox, x1, y1, x2, y2, "L")
+    # Define BEM geometry
+    num = -2*L
+    y = y .+ num
     elsbox = Elements(Int(1e5))
-    x1, y1, x2, y2 = discretizedline(-L, 0, L, 0, nels) # Bottom
+    x1, y1, x2, y2 = discretizedline(-L, 0+num, L, 0+num, nels) # Bottom
     addelsez!(elsbox, x1, y1, x2, y2, "B")
-    x1, y1, x2, y2 = discretizedline(L, 0, L, 2*L, nels) # Right hand side
+    x1, y1, x2, y2 = discretizedline(L, 0+num, L, 2*L+num, nels) # Right hand side
     addelsez!(elsbox, x1, y1, x2, y2, "R")
-    x1, y1, x2, y2 = discretizedline(L, 2*L, -L, 2*L, nels) # Top
+    x1, y1, x2, y2 = discretizedline(L, 2*L+num, -L, 2*L+num, nels) # Top
     addelsez!(elsbox, x1, y1, x2, y2, "T")
-    x1, y1, x2, y2 = discretizedline(-L, 2*L, -L, 0, nels) # Left hand side
+    x1, y1, x2, y2 = discretizedline(-L, 2*L+num, -L, 0+num, nels) # Left hand side
     addelsez!(elsbox, x1, y1, x2, y2, "L")
-    
+
     # Common indexing
     idxbox = getidxdict(elsbox) # Should this return "all" - YES TODO
     bcidxU = idxbox["B"] # Boundaries with *displacement* BCs
@@ -117,27 +169,27 @@ function gravitysquareparticular()
     THbox = [T_B_BRTL ; H_R_BRTL; H_T_BRTL ; H_L_BRTL]
     
     # Displacement BCs for bottom
-    Ug, Sg = gravityparticularfunctions(elsbox.xcenter[idxbox["B"]],
-                                        elsbox.ycenter[idxbox["B"]],
-                                        g, rho, lambda, mu)
+    Ugabove, Sgabove = gravityparticularfunctions(elsbox.xcenter[idxbox["B"]],
+                                                  elsbox.ycenter[idxbox["B"]],
+                                                  g, rho, lambda, mu)
     bcsboxgravity = zeros(2 * elsbox.endidx)
-    bcsboxgravity[1:2:2*nels] = Ug[:, 1]
-    bcsboxgravity[2:2:2*nels] = Ug[:, 2]
-    
+    bcsboxgravity[1:2:2*nels] = Ugabove[:, 1]
+    bcsboxgravity[2:2:2*nels] = Ugabove[:, 2]
+
     # Traction BCs for top and sides
-    Ug, Sg = gravityparticularfunctions(elsbox.xcenter[idxbox["T"]],
-                                        elsbox.ycenter[idxbox["T"]],
-                                        g, rho, lambda, mu)  
-    Ttx = zeros(nels)
-    Tty = zeros(nels)
-    for i in 1:length(Sg[:, 1])
-        nvec = [elsbox.xnormal[idxbox["T"][i]] ; elsbox.ynormal[idxbox["T"][i]]]
-        temp = [Sg[i, 1] Sg[i, 3] ; Sg[i, 3] Sg[i, 2]] * nvec
-        Ttx[i] = temp[1]
-        Tty[i] = temp[2]
-    end
-    bcsboxgravity[4*nels+1:2:6*nels] = Ttx
-    bcsboxgravity[4*nels+2:2:6*nels] = Tty
+    # Ug, Sg = gravityparticularfunctions(elsbox.xcenter[idxbox["T"]],
+    #                                     elsbox.ycenter[idxbox["T"]],
+    #                                     g, rho, lambda, mu)  
+    # Ttx = zeros(nels)
+    # Tty = zeros(nels)
+    # for i in 1:length(Sg[:, 1])
+    #     nvec = [elsbox.xnormal[idxbox["T"][i]] ; elsbox.ynormal[idxbox["T"][i]]]
+    #     temp = [Sg[i, 1] Sg[i, 3] ; Sg[i, 3] Sg[i, 2]] * nvec
+    #     Ttx[i] = temp[1]
+    #     Tty[i] = temp[2]
+    # end
+    # bcsboxgravity[4*nels+1:2:6*nels] = Ttx
+    # bcsboxgravity[4*nels+2:2:6*nels] = Tty
     bcsboxgravity *= -1
     Ueffboxparticular = THbox \ bcsboxgravity
     
@@ -154,11 +206,7 @@ function gravitysquareparticular()
     contourf(reshape(x, npts, npts), reshape(y, npts, npts),
              reshape(bemaboveumag, npts, npts), 50)
     colorbar()
-    # gca().set_xlim([-L, L])
-    # gca().set_ylim([0, 2*L])
     gca().set_aspect("equal")
     title("BEM - above ground")
-
-    
 end
 gravitysquareparticular()
