4f47dee0ef793444be984a96e126b837c4f9acf1
Ben Thompson <t.ben.thompson@gmail.com>
2020-11-19 21:17:27 -0500

    Fix debugbox2.jl by multiplying traction BC by els.ycenter.

--- examples/debugbox2.jl
+++ examples/debugbox2.jl
@@ -20,12 +20,11 @@ function bemsolve(els, nels, g, rho, lambda, mu, nu, x, y)
     # Modified displacment BCs for bottom
     Upart, _ = gravityparticularfunctions(els.xcenter[idx["B"]], els.ycenter[idx["B"]], g, rho, lambda, mu)
     bcs = zeros(2 * els.endidx)
-    bcs[1:2:2*nels] = Upart[:, 1]
-    bcs[2:2:2*nels] = Upart[:, 2]
+    bcs[1:2:2*nels] = -Upart[:, 1]
+    bcs[2:2:2*nels] = -Upart[:, 2]
 
     # Modified traction BCs for top
-    bcs[4*nels+2:2:6*nels] = rho * g * els.ynormal[idx["T"]]
-    bcs *= -1
+    bcs[4*nels+2:2:6*nels] = -rho * g * (els.ycenter[idx["T"]] .* els.ynormal[idx["T"]])
     Ueff = TH \ bcs
     
     # Forward solution on grid
@@ -51,22 +50,19 @@ function gravitysquareparticular()
     nu = 0.25
     rho = 2700
     g = 9.81
-    nels = 20
-    npts = 100
+    nels = 100
+    npts = 200
     L = 1e4
-    offset = 10
+    offset = 100
 
     ##### Below ground box
-    x, y = obsgrid(-L+offset, -2*L+offset, L-offset, 0-offset, npts) 
+    top = 0
+    x, y = obsgrid(-L+offset, top-2*L+offset, L-offset, top-offset, npts) 
     els = Elements(Int(1e5))
-    x1, y1, x2, y2 = discretizedline(-L, -2*L, L, -2*L, nels) # Bottom
-    addelsez!(els, x1, y1, x2, y2, "B")
-    x1, y1, x2, y2 = discretizedline(L, -2*L, L, 0, nels) # Right hand side
-    addelsez!(els, x1, y1, x2, y2, "R")
-    x1, y1, x2, y2 = discretizedline(L, 0, -L, 0, nels) # Top
-    addelsez!(els, x1, y1, x2, y2, "T")
-    x1, y1, x2, y2 = discretizedline(-L, 0, -L, -2*L, nels) # Left hand side
-    addelsez!(els, x1, y1, x2, y2, "L")
+    addelsez!(els, discretizedline(-L, top-2*L, L, top-2*L, nels)..., "B") # Bottom
+    addelsez!(els, discretizedline(L, top-2*L, L, top, nels)... , "R") # Right hand side
+    addelsez!(els, discretizedline(L, top, -L, top, nels)..., "T") # Top
+    addelsez!(els, discretizedline(-L, top, -L, top-2*L, nels)..., "L") # Left hand side
 
     Umag = bemsolve(els, nels, g, rho, lambda, mu, nu, x, y)
     figure(figsize=(15, 5))
@@ -77,16 +73,13 @@ function gravitysquareparticular()
     title("y(top) = 0")
 
     ##### Now with top of box not at zero
-    xoffset = 0 # Horizontal box translation...works fine
-    yoffset = 2 * L
-    x .+= xoffset
-    els.x1 .+= xoffset
-    els.x2 .+= xoffset
-    els.xcenter .+= xoffset
-    y .+= yoffset
-    els.y1 .+= yoffset
-    els.y2 .+= yoffset
-    els.ycenter .+= yoffset
+    top = 2*L
+    x, y = obsgrid(-L+offset, top-2*L+offset, L-offset, top-offset, npts) 
+    els = Elements(Int(1e5))
+    addelsez!(els, discretizedline(-L, top-2*L, L, top-2*L, nels)..., "B") # Bottom
+    addelsez!(els, discretizedline(L, top-2*L, L, top, nels)... , "R") # Right hand side
+    addelsez!(els, discretizedline(L, top, -L, top, nels)..., "T") # Top
+    addelsez!(els, discretizedline(-L, top, -L, top-2*L, nels)..., "L") # Left hand side
 
     Umagoffset = bemsolve(els, nels, g, rho, lambda, mu, nu, x, y)
     subplot(1, 3, 2)
